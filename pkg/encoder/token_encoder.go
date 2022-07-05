df8a984fe6ca590f7934837dca2fc71bc2a9d4a6
Craig Pastro <craig.pastro@auth0.com>
2022-07-05 12:16:08 -0700

    Update comments

--- pkg/encoder/token_encoder.go
+++ pkg/encoder/token_encoder.go
@@ -11,8 +11,7 @@ type TokenEncoder struct {
 
 var _ Encoder = (*TokenEncoder)(nil)
 
-// NewTokenEncoder constructs an Encoder that implements a base64 encoding as specified
-// by the encoding/base64 package.
+// NewTokenEncoder constructs an Encoder that includes an encrypter and encoder.
 func NewTokenEncoder(encrypter encrypter.Encrypter, encoder Encoder) *TokenEncoder {
 	return &TokenEncoder{
 		encrypter: encrypter,
@@ -20,7 +19,7 @@ func NewTokenEncoder(encrypter encrypter.Encrypter, encoder Encoder) *TokenEncod
 	}
 }
 
-// Decode base64 URL decodes the provided string.
+// Decode will first decode the given string with its decoder, then decrypt the result with its decrypter.
 func (e *TokenEncoder) Decode(s string) ([]byte, error) {
 	decoded, err := e.encoder.Decode(s)
 	if err != nil {
@@ -30,7 +29,7 @@ func (e *TokenEncoder) Decode(s string) ([]byte, error) {
 	return e.encrypter.Decrypt(decoded)
 }
 
-// Encode base64 URL encodes the provided byte slice and returns the encoded value as a string.
+// Encode will first encrypt the given data with its encrypter, then encode the result with its encoder.
 func (e *TokenEncoder) Encode(data []byte) (string, error) {
 	encrypted, err := e.encrypter.Encrypt(data)
 	if err != nil {
