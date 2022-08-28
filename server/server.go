28d98ae0f30186db35dc435c1f8121ade5d0d02a
Maria Ines Parnisari <maineparnisari@gmail.com>
2022-08-28 12:08:37 -0700

    replace logging field

--- server/server.go
+++ server/server.go
@@ -137,10 +137,10 @@ func New(dependencies *Dependencies, config *Config) (*Server, error) {
 		defaultServeMuxOpts: []runtime.ServeMuxOption{
 			runtime.WithForwardResponseOption(httpmiddleware.HTTPResponseModifier),
 			runtime.WithErrorHandler(func(c context.Context, sr *runtime.ServeMux, mm runtime.Marshaler, w http.ResponseWriter, r *http.Request, e error) {
-				dependencies.Logger.ErrorWithContext(c, "grpc error", logger.Error(e), logger.String("request_url", r.URL.String()))
 				if internalError, ok := e.(serverErrors.InternalError); ok {
-					dependencies.Logger.ErrorWithContext(c, "internal error", logger.Error(internalError.Internal()))
+					e = internalError.Internal()
 				}
+				dependencies.Logger.ErrorWithContext(c, "grpc_error", logger.Error(e), logger.String("request_url", r.URL.String()))
 				actualCode := serverErrors.ConvertToEncodedErrorCode(status.Convert(e))
 				httpmiddleware.CustomHTTPErrorHandler(c, w, r, serverErrors.NewEncodedError(actualCode, e.Error()))
 			}),
