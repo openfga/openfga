b2892e2eb1f697a98de387bc42f1069797255f2a
Craig Pastro <28915792+craigpastro@users.noreply.github.com>
2022-08-29 10:52:29 -0700

    Add test

--- server/test/create_store.go
+++ server/test/create_store.go
@@ -4,68 +4,43 @@ import (
 	"context"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/openfga/openfga/pkg/logger"
 	"github.com/openfga/openfga/pkg/testutils"
 	"github.com/openfga/openfga/server/commands"
 	"github.com/openfga/openfga/storage"
+	"github.com/stretchr/testify/require"
 	openfgapb "go.buf.build/openfga/go/openfga/api/openfga/v1"
 )
 
 func TestCreateStore(t *testing.T, datastore storage.OpenFGADatastore) {
-
 	type createStoreTestSettings struct {
-		_name    string
-		request  *openfgapb.CreateStoreRequest
-		response *openfgapb.CreateStoreResponse
-		err      error
+		name    string
+		request *openfgapb.CreateStoreRequest
 	}
-
-	name := testutils.CreateRandomString(10)
 
 	var tests = []createStoreTestSettings{
 		{
-			_name: "CreateStoreSucceeds",
+			name: "CreateStoreSucceeds",
 			request: &openfgapb.CreateStoreRequest{
-				Name: name,
-			},
-			response: &openfgapb.CreateStoreResponse{
-				Name: name,
+				Name: testutils.CreateRandomString(10),
 			},
 		},
 	}
-
-	ignoreStateOpts := cmpopts.IgnoreUnexported(openfgapb.CreateStoreResponse{})
-	ignoreStoreFields := cmpopts.IgnoreFields(openfgapb.CreateStoreResponse{}, "CreatedAt", "UpdatedAt", "Id")
 
 	ctx := context.Background()
 	logger := logger.NewNoopLogger()
 
 	for _, test := range tests {
-		t.Run(test._name, func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {
 
-			actualResponse, actualError := commands.NewCreateStoreCommand(datastore, logger).Execute(ctx, test.request)
+			resp, err := commands.NewCreateStoreCommand(datastore, logger).Execute(ctx, test.request)
+			require.NoError(t, err)
 
-			if test.err != nil {
-				if actualError == nil {
-					t.Errorf("[%s] Expected error '%s', but got none", test._name, test.err)
-				}
-				if test.err.Error() != actualError.Error() {
-					t.Errorf("[%s] Expected error '%s', actual '%s'", test._name, test.err, actualError)
-				}
-			}
-			if test.err == nil && actualError != nil {
-				t.Errorf("[%s] Did not expect an error but got one: %v", test._name, actualError)
-			}
-
-			if actualResponse == nil {
-				t.Error("Expected non nil response, got nil")
-			} else {
-				if diff := cmp.Diff(actualResponse, test.response, ignoreStateOpts, ignoreStoreFields, cmpopts.EquateEmpty()); diff != "" {
-					t.Errorf("[%s] store mismatch (-got +want):\n%s", test._name, diff)
-				}
-			}
+			require.Equal(t, test.request.Name, resp.Name)
+			require.NotEmpty(t, resp.Id)
+			require.NotEmpty(t, resp.CreatedAt)
+			require.NotEmpty(t, resp.UpdatedAt)
+			require.Equal(t, resp.CreatedAt, resp.UpdatedAt)
 		})
 	}
 }
