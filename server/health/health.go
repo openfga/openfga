d0120307f9140504b82592d8c6b88327b68e8b9f
Maria Ines Parnisari <maineparnisari@gmail.com>
2022-09-01 17:29:01 -0700

    chore: move health checker to its own package

---
+++ server/health/health.go
@@ -0,0 +1,50 @@
+package health
+
+import (
+	"context"
+
+	grpc_auth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+	"google.golang.org/grpc/codes"
+	healthv1pb "google.golang.org/grpc/health/grpc_health_v1"
+	"google.golang.org/grpc/status"
+)
+
+// TargetService defines an interface that services can implement for server health checks.
+type TargetService interface {
+	IsReady(ctx context.Context) (bool, error)
+}
+
+type Checker struct {
+	healthv1pb.UnimplementedHealthServer
+	TargetService
+	TargetServiceName string
+}
+
+var _ grpc_auth.ServiceAuthFuncOverride = (*Checker)(nil)
+
+// AuthFuncOverride implements the grpc_auth.ServiceAuthFuncOverride interface by bypassing authn middleware.
+func (o *Checker) AuthFuncOverride(ctx context.Context, fullMethodName string) (context.Context, error) {
+	return ctx, nil
+}
+
+func (o *Checker) Check(ctx context.Context, req *healthv1pb.HealthCheckRequest) (*healthv1pb.HealthCheckResponse, error) {
+	requestedService := req.GetService()
+	if requestedService == "" || requestedService == o.TargetServiceName {
+		ready, err := o.TargetService.IsReady(ctx)
+		if err != nil {
+			return &healthv1pb.HealthCheckResponse{Status: healthv1pb.HealthCheckResponse_NOT_SERVING}, err
+		}
+
+		if !ready {
+			return &healthv1pb.HealthCheckResponse{Status: healthv1pb.HealthCheckResponse_NOT_SERVING}, nil
+		}
+
+		return &healthv1pb.HealthCheckResponse{Status: healthv1pb.HealthCheckResponse_SERVING}, nil
+	}
+
+	return nil, status.Errorf(codes.NotFound, "service '%s' is not registered with the Health server", requestedService)
+}
+
+func (o *Checker) Watch(req *healthv1pb.HealthCheckRequest, server healthv1pb.Health_WatchServer) error {
+	return status.Error(codes.Unimplemented, "unimplemented streaming endpoint")
+}
