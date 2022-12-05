14a5aca628c3fbdb298d2d114fca373da5ec4bad
Maria Ines Parnisari <maineparnisari@gmail.com>
2022-12-05 14:57:02 -0800

    add tests

--- server/server_test.go
+++ server/server_test.go
@@ -7,12 +7,15 @@ import (
 	"path"
 	"runtime"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/oklog/ulid/v2"
 	"github.com/openfga/openfga/pkg/logger"
 	"github.com/openfga/openfga/pkg/telemetry"
 	storagefixtures "github.com/openfga/openfga/pkg/testfixtures/storage"
+	"github.com/openfga/openfga/pkg/tuple"
+	"github.com/openfga/openfga/pkg/typesystem"
 	serverErrors "github.com/openfga/openfga/server/errors"
 	"github.com/openfga/openfga/server/gateway"
 	"github.com/openfga/openfga/server/test"
@@ -22,6 +25,8 @@ import (
 	"github.com/openfga/openfga/storage/mysql"
 	"github.com/openfga/openfga/storage/postgres"
 	"github.com/stretchr/testify/require"
+	openfgapb "go.buf.build/openfga/go/openfga/api/openfga/v1"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func init() {
@@ -155,5 +160,154 @@ func TestResolveAuthorizationModel(t *testing.T) {
 		if _, err := s.resolveAuthorizationModelID(ctx, store, modelID); err.Error() != want.Error() {
 			t.Fatalf("got '%v', want '%v'", err, want)
 		}
+	})
+}
+func TestListObjects_Unoptimized_UnhappyPaths(t *testing.T) {
+	ctx := context.Background()
+	tracer := telemetry.NewNoopTracer()
+	logger := logger.NewNoopLogger()
+	transport := gateway.NewNoopTransport()
+	meter := telemetry.NewNoopMeter()
+	store := ulid.Make().String()
+	modelID := ulid.Make().String()
+
+	mockController := gomock.NewController(t)
+	defer mockController.Finish()
+
+	data, err := os.ReadFile("testdata/github.json")
+	require.NoError(t, err)
+
+	var gitHubTypeDefinitions openfgapb.WriteAuthorizationModelRequest
+	err = protojson.Unmarshal(data, &gitHubTypeDefinitions)
+	require.NoError(t, err)
+
+	mockDatastore := mockstorage.NewMockOpenFGADatastore(mockController)
+
+	mockDatastore.EXPECT().ReadAuthorizationModel(gomock.Any(), store, modelID).AnyTimes().Return(&openfgapb.AuthorizationModel{
+		SchemaVersion:   typesystem.SchemaVersion1_0,
+		TypeDefinitions: gitHubTypeDefinitions.GetTypeDefinitions(),
+	}, nil)
+	mockDatastore.EXPECT().ListObjectsByType(gomock.Any(), store, "repo").AnyTimes().Return(nil, errors.New("error reading from storage"))
+
+	s := Server{
+		datastore: mockDatastore,
+		tracer:    tracer,
+		transport: transport,
+		logger:    logger,
+		meter:     meter,
+		config: &Config{
+			ResolveNodeLimit:      25,
+			ListObjectsDeadline:   5 * time.Second,
+			ListObjectsMaxResults: 1000,
+		},
+	}
+
+	t.Run("error listing objects from storage in non-streaming version", func(t *testing.T) {
+		res, err := s.ListObjects(ctx, &openfgapb.ListObjectsRequest{
+			StoreId:              store,
+			AuthorizationModelId: modelID,
+			Type:                 "repo",
+			Relation:             "owner",
+			User:                 "bob",
+		})
+
+		require.Nil(t, res)
+		require.ErrorIs(t, err, serverErrors.NewInternalError("", errors.New("error reading from storage")))
+	})
+
+	t.Run("error listing objects from storage in streaming version", func(t *testing.T) {
+		err = s.StreamedListObjects(&openfgapb.StreamedListObjectsRequest{
+			StoreId:              store,
+			AuthorizationModelId: modelID,
+			Type:                 "repo",
+			Relation:             "owner",
+			User:                 "bob",
+		}, nil)
+
+		require.ErrorIs(t, err, serverErrors.NewInternalError("", errors.New("error reading from storage")))
+	})
+}
+
+func TestListObjects_Optimized_UnhappyPaths(t *testing.T) {
+	ctx := context.Background()
+	tracer := telemetry.NewNoopTracer()
+	logger := logger.NewNoopLogger()
+	transport := gateway.NewNoopTransport()
+	meter := telemetry.NewNoopMeter()
+	store := ulid.Make().String()
+	modelID := ulid.Make().String()
+
+	mockController := gomock.NewController(t)
+	defer mockController.Finish()
+
+	mockDatastore := mockstorage.NewMockOpenFGADatastore(mockController)
+
+	mockDatastore.EXPECT().ReadAuthorizationModel(gomock.Any(), store, modelID).AnyTimes().Return(&openfgapb.AuthorizationModel{
+		SchemaVersion: typesystem.SchemaVersion1_1,
+		TypeDefinitions: []*openfgapb.TypeDefinition{
+			{
+				Type: "user",
+			},
+			{
+				Type: "document",
+				Relations: map[string]*openfgapb.Userset{
+					"viewer": typesystem.This(),
+				},
+				Metadata: &openfgapb.Metadata{
+					Relations: map[string]*openfgapb.RelationMetadata{
+						"viewer": {
+							DirectlyRelatedUserTypes: []*openfgapb.RelationReference{
+								{Type: "user"},
+							},
+						},
+					},
+				},
+			},
+		},
+	}, nil)
+	mockDatastore.EXPECT().ReadStartingWithUser(gomock.Any(), store, storage.ReadStartingWithUserFilter{
+		ObjectType: "document",
+		Relation:   "viewer",
+		UserFilter: []*openfgapb.ObjectRelation{
+			{Object: "user:bob"},
+			{Object: tuple.Wildcard},
+		}}).AnyTimes().Return(nil, errors.New("error reading from storage"))
+
+	s := Server{
+		datastore: mockDatastore,
+		tracer:    tracer,
+		transport: transport,
+		logger:    logger,
+		meter:     meter,
+		config: &Config{
+			ResolveNodeLimit:      25,
+			ListObjectsDeadline:   5 * time.Second,
+			ListObjectsMaxResults: 1000,
+		},
+	}
+
+	t.Run("error listing objects from storage in non-streaming version", func(t *testing.T) {
+		res, err := s.ListObjects(ctx, &openfgapb.ListObjectsRequest{
+			StoreId:              store,
+			AuthorizationModelId: modelID,
+			Type:                 "document",
+			Relation:             "viewer",
+			User:                 "user:bob",
+		})
+
+		require.Nil(t, res)
+		require.ErrorIs(t, err, serverErrors.NewInternalError("", errors.New("error reading from storage")))
+	})
+
+	t.Run("error listing objects from storage in streaming version", func(t *testing.T) {
+		err := s.StreamedListObjects(&openfgapb.StreamedListObjectsRequest{
+			StoreId:              store,
+			AuthorizationModelId: modelID,
+			Type:                 "document",
+			Relation:             "viewer",
+			User:                 "user:bob",
+		}, nil)
+
+		require.ErrorIs(t, err, serverErrors.NewInternalError("", errors.New("error reading from storage")))
 	})
 }
