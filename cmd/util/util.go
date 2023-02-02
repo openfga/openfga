95bf17b7835be0722f3121839da10f1990472fd3
Craig Pastro <craig.pastro@okta.com>
2023-02-02 10:18:51 -0800

    move it out

--- cmd/util/util.go
+++ cmd/util/util.go
@@ -1,25 +1,4 @@
-// Package util provides common utilities for spf13/cobra CLI utilities
-// that can be used for various commands within this project.
 package util
-
-import (
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
-)
-
-// MustBindPFlag attempts to bind a specific key to a pflag (as used by cobra) and panics
-// if the binding fails with a non-nil error.
-func MustBindPFlag(key string, flag *pflag.Flag) {
-	if err := viper.BindPFlag(key, flag); err != nil {
-		panic("failed to bind pflag: " + err.Error())
-	}
-}
-
-func MustBindEnv(input ...string) {
-	if err := viper.BindEnv(input...); err != nil {
-		panic("failed to bind env key: " + err.Error())
-	}
-}
 
 func Contains[E comparable](s []E, v E) bool {
 	return Index(s, v) >= 0
