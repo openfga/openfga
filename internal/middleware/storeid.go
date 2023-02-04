7f7d53dbc8622ff941a2bc85fa8652a57f3b777c
Craig Pastro <craig.pastro@okta.com>
2023-02-03 17:23:51 -0800

    Update storeid.go

--- internal/middleware/storeid.go
+++ internal/middleware/storeid.go
@@ -3,10 +3,15 @@ package middleware
 import (
 	"context"
 
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 )
 
-const storeIDCtxKey ctxKey = "store-id-context-key"
+const (
+	storeIDCtxKey   ctxKey = "store-id-context-key"
+	storeIDTraceKey        = "store_id"
+)
 
 func StoreIDFromContext(ctx context.Context) (string, bool) {
 	storeID, ok := ctx.Value(storeIDCtxKey).(string)
@@ -17,47 +22,54 @@ type hasGetStoreId interface {
 	GetStoreId() string
 }
 
+// NewStoreIDInterceptor must come after the trace interceptor
 func NewStoreIDInterceptor() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		if r, ok := req.(hasGetStoreId); ok {
-			ctx = context.WithValue(ctx, storeIDCtxKey, r.GetStoreId())
+			storeID := r.GetStoreId()
+
+			// Add the storeID to the context
+			ctx = context.WithValue(ctx, storeIDCtxKey, storeID)
+
+			// Add the storeID to the span
+			trace.SpanFromContext(ctx).SetAttributes(attribute.String(storeIDTraceKey, storeID))
 		}
 
 		return handler(ctx, req)
 	}
 }
 
-type WrappedServerStream struct {
+type wrappedStoreIDServerStream struct {
 	grpc.ServerStream
 	WrappedContext context.Context
 }
 
-func WrapServerStream(stream grpc.ServerStream) *WrappedServerStream {
-	if existing, ok := stream.(*WrappedServerStream); ok {
-		return existing
-	}
-	return &WrappedServerStream{ServerStream: stream, WrappedContext: stream.Context()}
-}
-
-func (w *WrappedServerStream) Context() context.Context {
+func (w *wrappedStoreIDServerStream) Context() context.Context {
 	return w.WrappedContext
 }
 
-func (w *WrappedServerStream) RecvMsg(m interface{}) error {
+func (w *wrappedStoreIDServerStream) RecvMsg(m interface{}) error {
 	if err := w.ServerStream.RecvMsg(m); err != nil {
 		return nil
 	}
 
 	if r, ok := m.(hasGetStoreId); ok {
-		w.WrappedContext = context.WithValue(w.Context(), storeIDCtxKey, r.GetStoreId())
+		storeID := r.GetStoreId()
+
+		// Add the storeID to the context
+		w.WrappedContext = context.WithValue(w.Context(), storeIDCtxKey, storeID)
+
+		// Add the storeID to the span
+		trace.SpanFromContext(w.Context()).SetAttributes(attribute.String(storeIDTraceKey, storeID))
 	}
 
 	return nil
 }
 
+// NewStreamingStoreIDInterceptor must come after the trace interceptor
 func NewStreamingStoreIDInterceptor() grpc.StreamServerInterceptor {
 	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		ss := WrapServerStream(stream)
+		ss := &wrappedStoreIDServerStream{ServerStream: stream, WrappedContext: stream.Context()}
 
 		return handler(srv, ss)
 	}
