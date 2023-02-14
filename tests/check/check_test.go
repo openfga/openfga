487872fe0b5139e941c19577ca2780b8bd3f84ea
Adrian Tam <adrian.tam@okta.com>
2023-02-14 12:39:13 -0500

    fix: error handling for check in auth model id not found
    When auth model id is not found in check, it should return error
    auth model id not found instead of internal error.
    
    Close https://github.com/openfga/openfga/issues/540

--- tests/check/check_test.go
+++ tests/check/check_test.go
@@ -7,6 +7,7 @@ import (
 
 	parser "github.com/craigpastro/openfga-dsl-parser/v2"
 	"github.com/openfga/openfga/cmd/run"
+	"github.com/openfga/openfga/pkg/tuple"
 	"github.com/openfga/openfga/pkg/typesystem"
 	"github.com/openfga/openfga/tests"
 	"github.com/stretchr/testify/require"
@@ -41,14 +42,19 @@ type assertion struct {
 
 func TestCheckMemory(t *testing.T) {
 	testCheck(t, "memory")
+	testBadAuthModelID(t, "memory")
 }
 
 func TestCheckPostgres(t *testing.T) {
 	testCheck(t, "postgres")
+	testBadAuthModelID(t, "postgres")
+
 }
 
 func TestCheckMySQL(t *testing.T) {
 	testCheck(t, "mysql")
+	testBadAuthModelID(t, "mysql")
+
 }
 
 func testCheck(t *testing.T, engine string) {
@@ -125,4 +131,65 @@ func runTests(t *testing.T, client pb.OpenFGAServiceClient, tests checkTests) {
 			}
 		})
 	}
+}
+
+func testBadAuthModelID(t *testing.T, engine string) {
+
+	cfg := run.MustDefaultConfigWithRandomPorts()
+	cfg.Log.Level = "none"
+	cfg.Datastore.Engine = engine
+
+	cancel := tests.StartServer(t, cfg)
+	defer cancel()
+
+	conn := tests.Connect(t, cfg.GRPC.Addr)
+	defer conn.Close()
+	client := pb.NewOpenFGAServiceClient(conn)
+
+	ctx := context.Background()
+	resp, err := client.CreateStore(ctx, &pb.CreateStoreRequest{Name: "bad auth id"})
+	require.NoError(t, err)
+
+	storeID := resp.GetId()
+	_, err = client.WriteAuthorizationModel(ctx, &pb.WriteAuthorizationModelRequest{
+		StoreId:       storeID,
+		SchemaVersion: typesystem.SchemaVersion1_1,
+		TypeDefinitions: []*pb.TypeDefinition{
+			{
+				Type:      "user",
+				Relations: map[string]*pb.Userset{},
+			},
+			{
+				Type: "doc",
+				Relations: map[string]*pb.Userset{
+					"viewer":   typesystem.This(),
+					"can_view": typesystem.ComputedUserset("viewer"),
+				},
+				Metadata: &pb.Metadata{
+					Relations: map[string]*pb.RelationMetadata{
+						"viewer": {
+							DirectlyRelatedUserTypes: []*pb.RelationReference{
+								{
+									Type: "user",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	})
+	require.NoError(t, err)
+	_, err = client.Check(ctx, &pb.CheckRequest{
+		StoreId:              storeID,
+		TupleKey:             tuple.NewTupleKey("doc:x", "viewer", "user:y"),
+		AuthorizationModelId: "01GS89AJC3R3PFQ9BNY5ZF6Q97",
+	})
+
+	require.Error(t, err)
+	e, ok := status.FromError(err)
+	require.True(t, ok)
+	require.Equal(t, int(pb.ErrorCode_authorization_model_not_found), int(e.Code()))
+
+	cancel()
 }
