92fca9f43c53a7d59855727793d3664d931045ba
Craig Pastro <craig.pastro@okta.com>
2023-02-14 18:55:31 -0800

    stuff

---
+++ tests/check/check.go
@@ -0,0 +1,108 @@
+package check
+
+import (
+	"context"
+	"testing"
+
+	parser "github.com/craigpastro/openfga-dsl-parser/v2"
+	"github.com/openfga/openfga/assets"
+	"github.com/openfga/openfga/pkg/typesystem"
+	"github.com/stretchr/testify/require"
+	pb "go.buf.build/openfga/go/openfga/api/openfga/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+	"gopkg.in/yaml.v3"
+)
+
+type checkTests struct {
+	Tests []*checkTest
+}
+
+type checkTest struct {
+	Name   string
+	Stages []*stage
+}
+
+// stage is a stage of a test. All stages will be run in a single store.
+type stage struct {
+	Model      string
+	Tuples     []*pb.TupleKey
+	Assertions []*assertion
+}
+
+type assertion struct {
+	Tuple            *pb.TupleKey
+	ContextualTuples []*pb.TupleKey `yaml:"contextualTuples"`
+	Expectation      bool
+	ErrorCode        int `yaml:"errorCode"` // If ErrorCode is non-zero then we expect that the check call failed.
+	Trace            string
+}
+
+type ClientInterface interface {
+	CreateStore(ctx context.Context, in *pb.CreateStoreRequest, opts ...grpc.CallOption) (*pb.CreateStoreResponse, error)
+	WriteAuthorizationModel(ctx context.Context, in *pb.WriteAuthorizationModelRequest, opts ...grpc.CallOption) (*pb.WriteAuthorizationModelResponse, error)
+	Write(ctx context.Context, in *pb.WriteRequest, opts ...grpc.CallOption) (*pb.WriteResponse, error)
+	Check(ctx context.Context, in *pb.CheckRequest, opts ...grpc.CallOption) (*pb.CheckResponse, error)
+}
+
+// RunTests is public so can be run when OpenFGA is used as a library.
+// An OpenFGA server needs to be running and client is a client for
+// the server.
+func RunTests(t *testing.T, client ClientInterface) {
+	data, err := assets.EmbedTests.ReadFile("tests/check_tests.yaml")
+	require.NoError(t, err)
+
+	var testCases checkTests
+	err = yaml.Unmarshal(data, &testCases)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	for _, test := range testCases.Tests {
+		resp, err := client.CreateStore(ctx, &pb.CreateStoreRequest{Name: test.Name})
+		require.NoError(t, err)
+
+		storeID := resp.GetId()
+
+		t.Run(test.Name, func(t *testing.T) {
+			for _, stage := range test.Stages {
+				_, err = client.WriteAuthorizationModel(ctx, &pb.WriteAuthorizationModelRequest{
+					StoreId:         storeID,
+					SchemaVersion:   typesystem.SchemaVersion1_1,
+					TypeDefinitions: parser.MustParse(stage.Model),
+				})
+				require.NoError(t, err)
+
+				if len(stage.Tuples) > 0 {
+					_, err = client.Write(ctx, &pb.WriteRequest{
+						StoreId: storeID,
+						Writes:  &pb.TupleKeys{TupleKeys: stage.Tuples},
+					})
+					require.NoError(t, err)
+				}
+
+				for _, assertion := range stage.Assertions {
+					resp, err := client.Check(ctx, &pb.CheckRequest{
+						StoreId:  storeID,
+						TupleKey: assertion.Tuple,
+						ContextualTuples: &pb.ContextualTupleKeys{
+							TupleKeys: assertion.ContextualTuples,
+						},
+						Trace: true,
+					})
+
+					if assertion.ErrorCode == 0 {
+						require.NoError(t, err)
+						require.Equal(t, assertion.Expectation, resp.Allowed, assertion)
+					} else {
+						require.Error(t, err)
+						e, ok := status.FromError(err)
+						require.True(t, ok)
+						require.Equal(t, assertion.ErrorCode, int(e.Code()))
+					}
+
+				}
+			}
+		})
+	}
+}
--- tests/check/check_test.go
+++ tests/check/check_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	parser "github.com/craigpastro/openfga-dsl-parser/v2"
-	"github.com/openfga/openfga/assets"
 	"github.com/openfga/openfga/cmd/run"
 	"github.com/openfga/openfga/pkg/tuple"
 	"github.com/openfga/openfga/pkg/typesystem"
@@ -13,32 +12,7 @@ import (
 	"github.com/stretchr/testify/require"
 	pb "go.buf.build/openfga/go/openfga/api/openfga/v1"
 	"google.golang.org/grpc/status"
-	"gopkg.in/yaml.v3"
 )
-
-type checkTests struct {
-	Tests []*checkTest
-}
-
-type checkTest struct {
-	Name   string
-	Stages []*stage
-}
-
-// stage is a stage of a test. All stages will be run in a single store.
-type stage struct {
-	Model      string
-	Tuples     []*pb.TupleKey
-	Assertions []*assertion
-}
-
-type assertion struct {
-	Tuple            *pb.TupleKey
-	ContextualTuples []*pb.TupleKey `yaml:"contextualTuples"`
-	Expectation      bool
-	ErrorCode        int `yaml:"errorCode"` // If ErrorCode is non-zero then we expect that the check call failed.
-	Trace            string
-}
 
 func TestCheckMemory(t *testing.T) {
 	testRunAll(t, "memory")
@@ -58,13 +32,6 @@ func testRunAll(t *testing.T, engine string) {
 }
 
 func testCheck(t *testing.T, engine string) {
-	data, err := assets.EmbedTests.ReadFile("tests/check_tests.yaml")
-	require.NoError(t, err)
-
-	var testCases checkTests
-	err = yaml.Unmarshal(data, &testCases)
-	require.NoError(t, err)
-
 	cfg := run.MustDefaultConfigWithRandomPorts()
 	cfg.Log.Level = "none"
 	cfg.Datastore.Engine = engine
@@ -75,62 +42,7 @@ func testCheck(t *testing.T, engine string) {
 	conn := tests.Connect(t, cfg.GRPC.Addr)
 	defer conn.Close()
 
-	runTests(t, pb.NewOpenFGAServiceClient(conn), testCases)
-
-	// Shutdown the server.
-	cancel()
-}
-
-func runTests(t *testing.T, client pb.OpenFGAServiceClient, tests checkTests) {
-	ctx := context.Background()
-
-	for _, test := range tests.Tests {
-		resp, err := client.CreateStore(ctx, &pb.CreateStoreRequest{Name: test.Name})
-		require.NoError(t, err)
-
-		storeID := resp.GetId()
-
-		t.Run(test.Name, func(t *testing.T) {
-			for _, stage := range test.Stages {
-				_, err = client.WriteAuthorizationModel(ctx, &pb.WriteAuthorizationModelRequest{
-					StoreId:         storeID,
-					SchemaVersion:   typesystem.SchemaVersion1_1,
-					TypeDefinitions: parser.MustParse(stage.Model),
-				})
-				require.NoError(t, err)
-
-				if len(stage.Tuples) > 0 {
-					_, err = client.Write(ctx, &pb.WriteRequest{
-						StoreId: storeID,
-						Writes:  &pb.TupleKeys{TupleKeys: stage.Tuples},
-					})
-					require.NoError(t, err)
-				}
-
-				for _, assertion := range stage.Assertions {
-					resp, err := client.Check(ctx, &pb.CheckRequest{
-						StoreId:  storeID,
-						TupleKey: assertion.Tuple,
-						ContextualTuples: &pb.ContextualTupleKeys{
-							TupleKeys: assertion.ContextualTuples,
-						},
-						Trace: true,
-					})
-
-					if assertion.ErrorCode == 0 {
-						require.NoError(t, err)
-						require.Equal(t, assertion.Expectation, resp.Allowed, assertion)
-					} else {
-						require.Error(t, err)
-						e, ok := status.FromError(err)
-						require.True(t, ok)
-						require.Equal(t, assertion.ErrorCode, int(e.Code()))
-					}
-
-				}
-			}
-		})
-	}
+	RunTests(t, pb.NewOpenFGAServiceClient(conn))
 }
 
 func testBadAuthModelID(t *testing.T, engine string) {
