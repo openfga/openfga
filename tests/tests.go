cc5b4873ba623bdf658ae1918e8873a01cad8641
Craig Pastro <craig.pastro@okta.com>
2023-02-15 10:04:28 -0800

    remove unused

--- tests/tests.go
+++ tests/tests.go
@@ -2,18 +2,11 @@ package tests
 
 import (
 	"context"
-	"fmt"
 	"testing"
-	"time"
 
-	"github.com/cenkalti/backoff/v4"
 	"github.com/openfga/openfga/cmd/run"
 	"github.com/openfga/openfga/pkg/testfixtures/storage"
 	"github.com/stretchr/testify/require"
-	pb "go.buf.build/openfga/go/openfga/api/openfga/v1"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	healthv1pb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func StartServer(t *testing.T, cfg *run.Config) context.CancelFunc {
@@ -28,31 +21,4 @@ func StartServer(t *testing.T, cfg *run.Config) context.CancelFunc {
 	}()
 
 	return cancel
-}
-
-func Connect(t *testing.T, addr string) *grpc.ClientConn {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	require.NoError(t, err)
-
-	// Ensure the service is up before continuing.
-	client := healthv1pb.NewHealthClient(conn)
-	policy := backoff.NewExponentialBackOff()
-	policy.MaxElapsedTime = 5 * time.Second
-	err = backoff.Retry(func() error {
-		resp, err := client.Check(context.Background(), &healthv1pb.HealthCheckRequest{
-			Service: pb.OpenFGAService_ServiceDesc.ServiceName,
-		})
-		if err != nil {
-			return err
-		}
-
-		if resp.GetStatus() != healthv1pb.HealthCheckResponse_SERVING {
-			return fmt.Errorf("not serving")
-		}
-
-		return nil
-	}, policy)
-	require.NoError(t, err)
-
-	return conn
 }
