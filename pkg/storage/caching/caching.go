f3c97f1d8868dcc66b9f23c81c7aa73d318445ca
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2023-04-24 14:04:49 -0700

    chore: remove storage.ReadTypeDefinition because it's unused (#713)

--- pkg/storage/caching/caching.go
+++ pkg/storage/caching/caching.go
@@ -19,29 +19,13 @@ type cachedOpenFGADatastore struct {
 	cache *ccache.Cache
 }
 
+// NewCachedOpenFGADatastore returns a wrapper over a datastore that caches *openfgapb.AuthorizationModel
+// on every call to storage.ReadAuthorizationModel.
 func NewCachedOpenFGADatastore(inner storage.OpenFGADatastore, maxSize int) *cachedOpenFGADatastore {
 	return &cachedOpenFGADatastore{
 		OpenFGADatastore: inner,
 		cache:            ccache.New(ccache.Configure().MaxSize(int64(maxSize))),
 	}
-}
-
-func (c *cachedOpenFGADatastore) ReadTypeDefinition(ctx context.Context, storeID, modelID, name string) (*openfgapb.TypeDefinition, error) {
-	cacheKey := fmt.Sprintf("%s:%s:%s", storeID, modelID, name)
-	cachedEntry := c.cache.Get(cacheKey)
-
-	if cachedEntry != nil {
-		return cachedEntry.Value().(*openfgapb.TypeDefinition), nil
-	}
-
-	td, err := c.OpenFGADatastore.ReadTypeDefinition(ctx, storeID, modelID, name)
-	if err != nil {
-		return nil, err
-	}
-
-	c.cache.Set(cacheKey, td, ttl) // these are immutable, once created, there cannot be edits, therefore they can be cached without ttl
-
-	return td, nil
 }
 
 func (c *cachedOpenFGADatastore) ReadAuthorizationModel(ctx context.Context, storeID, modelID string) (*openfgapb.AuthorizationModel, error) {
--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -150,12 +150,6 @@ type AuthorizationModelReadBackend interface {
 	FindLatestAuthorizationModelID(ctx context.Context, store string) (string, error)
 }
 
-// TypeDefinitionReadBackend Provides a Read interface for managing type definitions.
-type TypeDefinitionReadBackend interface {
-	// ReadTypeDefinition Read the store authorization model corresponding to `id` + `objectType`.
-	ReadTypeDefinition(ctx context.Context, store, id string, objectType string) (*openfgapb.TypeDefinition, error)
-}
-
 // TypeDefinitionWriteBackend Provides a write interface for managing typed definition.
 type TypeDefinitionWriteBackend interface {
 	// MaxTypesPerAuthorizationModel returns the maximum number of items allowed for type definitions
@@ -169,7 +163,6 @@ type TypeDefinitionWriteBackend interface {
 // AuthorizationModelBackend provides an R/W interface for managing type definition.
 type AuthorizationModelBackend interface {
 	AuthorizationModelReadBackend
-	TypeDefinitionReadBackend
 	TypeDefinitionWriteBackend
 }
 
