openfga/openfga

commit 8f634158d9bf147cc76fd6f8b0383bc33945fb5b

docs: if OPENFGA_LIST_OBJECTS_MAX_RESULTS=0, no limit (#757)
Co-authored-by: Jonathan Whitaker <jon.b.whitaker@gmail.com>

pkg/server/commands/list_objects.go
@@ -93,6 +93,7 @@ func (q *ListObjectsQuery) evaluate(
 		err = q.performChecks(ctx, req, resultsChan, maxResults)
 		if err != nil {
 			errChan <- err
+			close(errChan)
 		}
 
 		close(resultsChan)
@@ -157,6 +158,7 @@ func (q *ListObjectsQuery) evaluate(
 			}, resultsChan)
 			if err != nil {
 				errChan <- err
+				close(errChan)
 			}
 
 			close(resultsChan)
@@ -175,10 +177,14 @@ func (q *ListObjectsQuery) Execute(
 	req *openfgapb.ListObjectsRequest,
 ) (*openfgapb.ListObjectsResponse, error) {
 
+	resultsChan := make(chan string, 1)
 	maxResults := q.ListObjectsMaxResults
-	resultsChan := make(chan string, maxResults)
+	if maxResults > 0 {
+		resultsChan = make(chan string, maxResults)
+	}
 
-	errChan := make(chan error)
+	// make a buffered channel so that writer goroutines aren't blocked when attempting to send an error
+	errChan := make(chan error, 1)
 
 	timeoutCtx := ctx
 	if q.ListObjectsDeadline != 0 {
@@ -234,7 +240,8 @@ func (q *ListObjectsQuery) ExecuteStreamed(
 	// make a buffered channel so that writer goroutines aren't blocked when attempting to send a result
 	resultsChan := make(chan string, streamedBufferSize)
 
-	errChan := make(chan error)
+	// make a buffered channel so that writer goroutines aren't blocked when attempting to send an error
+	errChan := make(chan error, 1)
 
 	timeoutCtx := ctx
 	if q.ListObjectsDeadline != 0 {

cmd/run/flags.go
@@ -191,7 +191,7 @@ func bindRunFlags(command *cobra.Command) {
 	util.MustBindPFlag("listObjectsDeadline", flags.Lookup("listObjects-deadline"))
 	util.MustBindEnv("listObjectsDeadline", "OPENFGA_LIST_OBJECTS_DEADLINE", "OPENFGA_LISTOBJECTSDEADLINE")
 
-	flags.Uint32("listObjects-max-results", defaultConfig.ListObjectsMaxResults, "the maximum results to return in non-streaming ListObjects API responses")
+	flags.Uint32("listObjects-max-results", defaultConfig.ListObjectsMaxResults, "the maximum results to return in non-streaming ListObjects API responses. If 0, all results can be returned")
 	util.MustBindPFlag("listObjectsMaxResults", flags.Lookup("listObjects-max-results"))
 	util.MustBindEnv("listObjectsMaxResults", "OPENFGA_LIST_OBJECTS_MAX_RESULTS", "OPENFGA_LISTOBJECTSMAXRESULTS")
 

commit 8a2e182eb64c519e63477cd46b8957235251afd6

fix: regression in #757

pkg/server/commands/list_objects.go
@@ -94,9 +94,9 @@ func (q *ListObjectsQuery) evaluate(
 		if err != nil {
 			errChan <- err
 			close(errChan)
+		} else {
+			close(resultsChan)
 		}
-
-		close(resultsChan)
 	}
 
 	hasTypeInfo, err := typesys.HasTypeInfo(targetObjectType, targetRelation)
@@ -159,9 +159,9 @@ func (q *ListObjectsQuery) evaluate(
 			if err != nil {
 				errChan <- err
 				close(errChan)
+			} else {
+				close(resultsChan)
 			}
-
-			close(resultsChan)
 		}
 	}
 
@@ -213,14 +213,14 @@ func (q *ListObjectsQuery) Execute(
 			}, nil
 
 		case objectID, ok := <-resultsChan:
-			if !ok {
+			if !ok { //channel was closed and no new values are available
 				return &openfgapb.ListObjectsResponse{
 					Objects: objects,
 				}, nil
 			}
 			objects = append(objects, objectID)
 		case genericError, ok := <-errChan:
-			if ok {
+			if ok { //a value is available
 				return nil, serverErrors.NewInternalError("", genericError)
 			}
 		}