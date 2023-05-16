openfga/openfga

commit 8f634158d9bf147cc76fd6f8b0383bc33945fb5b

docs: if OPENFGA_LIST_OBJECTS_MAX_RESULTS=0, no limit (#757)
Co-authored-by: Jonathan Whitaker <jon.b.whitaker@gmail.com>

pkg/server/commands/list_objects.go
@@ -93,6 +93,7 @@ func (q *ListObjectsQuery) evaluate(
 		err = q.performChecks(ctx, req, resultsChan, maxResults)
 		if err != nil {
 			errChan <- err
+			close(errChan)
 		}
 
 		close(resultsChan)
@@ -157,6 +158,7 @@ func (q *ListObjectsQuery) evaluate(
 			}, resultsChan)
 			if err != nil {
 				errChan <- err
+				close(errChan)
 			}
 
 			close(resultsChan)
@@ -175,10 +177,14 @@ func (q *ListObjectsQuery) Execute(
 	req *openfgapb.ListObjectsRequest,
 ) (*openfgapb.ListObjectsResponse, error) {
 
+	resultsChan := make(chan string, 1)
 	maxResults := q.ListObjectsMaxResults
-	resultsChan := make(chan string, maxResults)
+	if maxResults > 0 {
+		resultsChan = make(chan string, maxResults)
+	}
 
-	errChan := make(chan error)
+	// make a buffered channel so that writer goroutines aren't blocked when attempting to send an error
+	errChan := make(chan error, 1)
 
 	timeoutCtx := ctx
 	if q.ListObjectsDeadline != 0 {
@@ -234,7 +240,8 @@ func (q *ListObjectsQuery) ExecuteStreamed(
 	// make a buffered channel so that writer goroutines aren't blocked when attempting to send a result
 	resultsChan := make(chan string, streamedBufferSize)
 
-	errChan := make(chan error)
+	// make a buffered channel so that writer goroutines aren't blocked when attempting to send an error
+	errChan := make(chan error, 1)
 
 	timeoutCtx := ctx
 	if q.ListObjectsDeadline != 0 {

cmd/run/flags.go
@@ -191,7 +191,7 @@ func bindRunFlags(command *cobra.Command) {
 	util.MustBindPFlag("listObjectsDeadline", flags.Lookup("listObjects-deadline"))
 	util.MustBindEnv("listObjectsDeadline", "OPENFGA_LIST_OBJECTS_DEADLINE", "OPENFGA_LISTOBJECTSDEADLINE")
 
-	flags.Uint32("listObjects-max-results", defaultConfig.ListObjectsMaxResults, "the maximum results to return in non-streaming ListObjects API responses")
+	flags.Uint32("listObjects-max-results", defaultConfig.ListObjectsMaxResults, "the maximum results to return in non-streaming ListObjects API responses. If 0, all results can be returned")
 	util.MustBindPFlag("listObjectsMaxResults", flags.Lookup("listObjects-max-results"))
 	util.MustBindEnv("listObjectsMaxResults", "OPENFGA_LIST_OBJECTS_MAX_RESULTS", "OPENFGA_LISTOBJECTSMAXRESULTS")
 

commit 0f1a5d06073efe0fbd33823cda56a6202864f816

fix: race regression in ListObjects introduced in #757 (#762)

pkg/server/commands/list_objects.go
@@ -94,8 +94,8 @@ func (q *ListObjectsQuery) evaluate(
 		if err != nil {
 			errChan <- err
 			close(errChan)
+			return
 		}
-
 		close(resultsChan)
 	}
 
@@ -159,8 +159,8 @@ func (q *ListObjectsQuery) evaluate(
 			if err != nil {
 				errChan <- err
 				close(errChan)
+				return
 			}
-
 			close(resultsChan)
 		}
 	}
@@ -212,15 +212,15 @@ func (q *ListObjectsQuery) Execute(
 				Objects: objects,
 			}, nil
 
-		case objectID, ok := <-resultsChan:
-			if !ok {
+		case objectID, channelOpen := <-resultsChan:
+			if !channelOpen {
 				return &openfgapb.ListObjectsResponse{
 					Objects: objects,
 				}, nil
 			}
 			objects = append(objects, objectID)
-		case genericError, ok := <-errChan:
-			if ok {
+		case genericError, valueAvailable := <-errChan:
+			if valueAvailable {
 				return nil, serverErrors.NewInternalError("", genericError)
 			}
 		}

pkg/server/server_test.go
@@ -10,6 +10,7 @@ import (
 	"testing"
 	"time"
 
+	parser "github.com/craigpastro/openfga-dsl-parser/v2"
 	"github.com/golang/mock/gomock"
 	"github.com/oklog/ulid/v2"
 	"github.com/openfga/openfga/internal/gateway"
@@ -347,6 +348,71 @@ func (m *mockStreamServer) Send(*openfgapb.StreamedListObjectsResponse) error {
 	return nil
 }
 
+// This runs TestListObjects_Unoptimized_UnhappyPaths many times over to ensure no race conditions (see https://github.com/openfga/openfga/pull/762)
+func BenchmarkListObjectsNoRaceCondition(b *testing.B) {
+	ctx := context.Background()
+	logger := logger.NewNoopLogger()
+	transport := gateway.NewNoopTransport()
+	store := ulid.Make().String()
+	modelID := ulid.Make().String()
+
+	mockController := gomock.NewController(b)
+	defer mockController.Finish()
+
+	typedefs := parser.MustParse(`
+    type user
+    type org
+
+    type repo
+        relations
+            define blocked: [user] as self
+            define owner: [org] as self but not blocked
+    `)
+
+	mockDatastore := mockstorage.NewMockOpenFGADatastore(mockController)
+
+	mockDatastore.EXPECT().ReadAuthorizationModel(gomock.Any(), store, modelID).AnyTimes().Return(&openfgapb.AuthorizationModel{
+		SchemaVersion:   typesystem.SchemaVersion1_1,
+		TypeDefinitions: typedefs,
+	}, nil)
+	mockDatastore.EXPECT().ListObjectsByType(gomock.Any(), store, "repo").AnyTimes().Return(nil, errors.New("error reading from storage"))
+
+	s := New(&Dependencies{
+		Datastore: mockDatastore,
+		Transport: transport,
+		Logger:    logger,
+	}, &Config{
+		ResolveNodeLimit:         25,
+		ListObjectsDeadline:      5 * time.Second,
+		ListObjectsMaxResults:    1000,
+		AllowEvaluating1_0Models: true,
+	})
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := s.ListObjects(ctx, &openfgapb.ListObjectsRequest{
+			StoreId:              store,
+			AuthorizationModelId: modelID,
+			Type:                 "repo",
+			Relation:             "owner",
+			User:                 "user:bob",
+		})
+
+		require.ErrorIs(b, err, serverErrors.NewInternalError("", errors.New("error reading from storage")))
+
+		err = s.StreamedListObjects(&openfgapb.StreamedListObjectsRequest{
+			StoreId:              store,
+			AuthorizationModelId: modelID,
+			Type:                 "repo",
+			Relation:             "owner",
+			User:                 "user:bob",
+		}, NewMockStreamServer())
+
+		require.ErrorIs(b, err, serverErrors.NewInternalError("", errors.New("error reading from storage")))
+	}
+}
+
+// This test ensures that when the data storage fails, ListObjects v0 throws an error
 func TestListObjects_Unoptimized_UnhappyPaths(t *testing.T) {
 	ctx := context.Background()
 	logger := logger.NewNoopLogger()
@@ -409,6 +475,7 @@ func TestListObjects_Unoptimized_UnhappyPaths(t *testing.T) {
 	})
 }
 
+// This test ensures that when the data storage fails, ListObjects v1 throws an error
 func TestListObjects_UnhappyPaths(t *testing.T) {
 	ctx := context.Background()
 	logger := logger.NewNoopLogger()