d1ff4a53a1472f785f871af6a887d36fc2b0d2f9
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2023-06-07 10:00:04 -0600

    Revert "fix: validate models with non-zero entrypoints properly (#802)" (#807)

--- pkg/server/test/write_authzmodel.go
+++ pkg/server/test/write_authzmodel.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"testing"
 
-	parser "github.com/craigpastro/openfga-dsl-parser/v2"
 	"github.com/oklog/ulid/v2"
 	"github.com/openfga/openfga/pkg/logger"
 	"github.com/openfga/openfga/pkg/server/commands"
@@ -135,77 +134,6 @@ func WriteAuthorizationModelTest(t *testing.T, datastore storage.OpenFGADatastor
 					},
 				},
 			},
-		},
-		{
-			name: "self_referencing_type_restriction_with_entrypoint",
-			request: &openfgapb.WriteAuthorizationModelRequest{
-				StoreId: storeID,
-				TypeDefinitions: parser.MustParse(`
-				type user
-
-				type document
-				  relations
-				    define editor: [user] as self
-				    define viewer: [document#viewer] as self or editor
-				`),
-				SchemaVersion: typesystem.SchemaVersion1_1,
-			},
-		},
-		{
-			name: "self_referencing_type_restriction_without_entrypoint_1",
-			request: &openfgapb.WriteAuthorizationModelRequest{
-				StoreId: storeID,
-				TypeDefinitions: parser.MustParse(`
-				type user
-				type document
-				  relations
-				    define viewer: [document#viewer] as self
-				`),
-				SchemaVersion: typesystem.SchemaVersion1_1,
-			},
-			err: serverErrors.InvalidAuthorizationModelInput(&typesystem.InvalidRelationError{
-				ObjectType: "document",
-				Relation:   "viewer",
-				Cause:      typesystem.ErrCycle,
-			}),
-		},
-		{
-			name: "self_referencing_type_restriction_without_entrypoint_2",
-			request: &openfgapb.WriteAuthorizationModelRequest{
-				StoreId: storeID,
-				TypeDefinitions: parser.MustParse(`
-				type user
-				type document
-				  relations
-				    define editor: [user] as self
-				    define viewer: [document#viewer] as self and editor
-				`),
-				SchemaVersion: typesystem.SchemaVersion1_1,
-			},
-			err: serverErrors.InvalidAuthorizationModelInput(&typesystem.InvalidRelationError{
-				ObjectType: "document",
-				Relation:   "viewer",
-				Cause:      typesystem.ErrCycle,
-			}),
-		},
-		{
-			name: "self_referencing_type_restriction_without_entrypoint_3",
-			request: &openfgapb.WriteAuthorizationModelRequest{
-				StoreId: storeID,
-				TypeDefinitions: parser.MustParse(`
-				type user
-				type document
-				  relations
-				    define restricted: [user] as self
-				    define viewer: [document#viewer] as self but not restricted
-				`),
-				SchemaVersion: typesystem.SchemaVersion1_1,
-			},
-			err: serverErrors.InvalidAuthorizationModelInput(&typesystem.InvalidRelationError{
-				ObjectType: "document",
-				Relation:   "viewer",
-				Cause:      typesystem.ErrCycle,
-			}),
 		},
 	}
 
