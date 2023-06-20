b428fcecdffe0e28e8cfd3145a2956caa96c2ddd
Liam G <liam.gallear@gmail.com>
2023-06-20 08:24:33 +0100

    Removed residual commented line

--- pkg/typesystem/typesystem.go
+++ pkg/typesystem/typesystem.go
@@ -723,8 +723,6 @@ func (t *TypeSystem) isUsersetRewriteValid(objectType, relation string, rewrite 
 					return nil
 				}
 			}
-			// ErrRes := ErrUserNotRelatedToType{computedUserset, userTypes}
-
 			return fmt.Errorf("%w: %s does not appear as a relation in any of the directly related user types %s", ErrRelationUndefined, computedUserset, userTypes)
 		} else {
 			// for 1.0 models, relation `computedUserset` has to be defined _somewhere_ in the model
