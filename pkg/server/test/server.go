6fd0ab4f77f58c4600d0cc9a91b3b670194e880c
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2023-07-12 21:48:40 -0700

    uncomment code

--- pkg/server/test/server.go
+++ pkg/server/test/server.go
@@ -12,43 +12,43 @@ const (
 
 func RunAllTests(t *testing.T, ds storage.OpenFGADatastore) {
 	RunQueryTests(t, ds)
-	//RunCommandTests(t, ds)
+	RunCommandTests(t, ds)
 }
 
 func RunQueryTests(t *testing.T, ds storage.OpenFGADatastore) {
-	//t.Run("TestReadAuthorizationModelQueryErrors", func(t *testing.T) { TestReadAuthorizationModelQueryErrors(t, ds) })
-	//t.Run("TestSuccessfulReadAuthorizationModelQuery", func(t *testing.T) { TestSuccessfulReadAuthorizationModelQuery(t, ds) })
-	//t.Run("TestReadAuthorizationModel", func(t *testing.T) { ReadAuthorizationModelTest(t, ds) })
-	//t.Run("TestExpandQuery", func(t *testing.T) { TestExpandQuery(t, ds) })
-	//t.Run("TestExpandQueryErrors", func(t *testing.T) { TestExpandQueryErrors(t, ds) })
-	//
-	//t.Run("TestGetStoreQuery", func(t *testing.T) { TestGetStoreQuery(t, ds) })
-	//t.Run("TestGetStoreSucceeds", func(t *testing.T) { TestGetStoreSucceeds(t, ds) })
-	//t.Run("TestListStores", func(t *testing.T) { TestListStores(t, ds) })
-	//
-	//t.Run("TestReadAssertionQuery", func(t *testing.T) { TestReadAssertionQuery(t, ds) })
-	//
-	//t.Run("TestReadQuerySuccess", func(t *testing.T) { ReadQuerySuccessTest(t, ds) })
-	//t.Run("TestReadQueryError", func(t *testing.T) { ReadQueryErrorTest(t, ds) })
-	//t.Run("TestReadAllTuples", func(t *testing.T) { ReadAllTuplesTest(t, ds) })
-	//t.Run("TestReadAllTuplesInvalidContinuationToken", func(t *testing.T) { ReadAllTuplesInvalidContinuationTokenTest(t, ds) })
-	//
-	//t.Run("TestReadAuthorizationModelsWithoutPaging",
-	//	func(t *testing.T) { TestReadAuthorizationModelsWithoutPaging(t, ds) },
-	//)
-	//
-	//t.Run("TestReadAuthorizationModelsWithPaging",
-	//	func(t *testing.T) { TestReadAuthorizationModelsWithPaging(t, ds) },
-	//)
-	//
-	//t.Run("TestReadAuthorizationModelsInvalidContinuationToken",
-	//	func(t *testing.T) { TestReadAuthorizationModelsInvalidContinuationToken(t, ds) },
-	//)
-	//
-	//t.Run("TestReadChanges", func(t *testing.T) { TestReadChanges(t, ds) })
-	//t.Run("TestReadChangesReturnsSameContTokenWhenNoChanges",
-	//	func(t *testing.T) { TestReadChangesReturnsSameContTokenWhenNoChanges(t, ds) },
-	//)
+	t.Run("TestReadAuthorizationModelQueryErrors", func(t *testing.T) { TestReadAuthorizationModelQueryErrors(t, ds) })
+	t.Run("TestSuccessfulReadAuthorizationModelQuery", func(t *testing.T) { TestSuccessfulReadAuthorizationModelQuery(t, ds) })
+	t.Run("TestReadAuthorizationModel", func(t *testing.T) { ReadAuthorizationModelTest(t, ds) })
+	t.Run("TestExpandQuery", func(t *testing.T) { TestExpandQuery(t, ds) })
+	t.Run("TestExpandQueryErrors", func(t *testing.T) { TestExpandQueryErrors(t, ds) })
+
+	t.Run("TestGetStoreQuery", func(t *testing.T) { TestGetStoreQuery(t, ds) })
+	t.Run("TestGetStoreSucceeds", func(t *testing.T) { TestGetStoreSucceeds(t, ds) })
+	t.Run("TestListStores", func(t *testing.T) { TestListStores(t, ds) })
+
+	t.Run("TestReadAssertionQuery", func(t *testing.T) { TestReadAssertionQuery(t, ds) })
+
+	t.Run("TestReadQuerySuccess", func(t *testing.T) { ReadQuerySuccessTest(t, ds) })
+	t.Run("TestReadQueryError", func(t *testing.T) { ReadQueryErrorTest(t, ds) })
+	t.Run("TestReadAllTuples", func(t *testing.T) { ReadAllTuplesTest(t, ds) })
+	t.Run("TestReadAllTuplesInvalidContinuationToken", func(t *testing.T) { ReadAllTuplesInvalidContinuationTokenTest(t, ds) })
+
+	t.Run("TestReadAuthorizationModelsWithoutPaging",
+		func(t *testing.T) { TestReadAuthorizationModelsWithoutPaging(t, ds) },
+	)
+
+	t.Run("TestReadAuthorizationModelsWithPaging",
+		func(t *testing.T) { TestReadAuthorizationModelsWithPaging(t, ds) },
+	)
+
+	t.Run("TestReadAuthorizationModelsInvalidContinuationToken",
+		func(t *testing.T) { TestReadAuthorizationModelsInvalidContinuationToken(t, ds) },
+	)
+
+	t.Run("TestReadChanges", func(t *testing.T) { TestReadChanges(t, ds) })
+	t.Run("TestReadChangesReturnsSameContTokenWhenNoChanges",
+		func(t *testing.T) { TestReadChangesReturnsSameContTokenWhenNoChanges(t, ds) },
+	)
 
 	t.Run("TestListObjectsRespectsMaxResults", func(t *testing.T) { TestListObjectsRespectsMaxResults(t, ds) })
 }
