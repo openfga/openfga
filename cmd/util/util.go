8b64ae11985d5d93616e202dee7b300bf21d0141
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2023-10-27 15:44:43 -0700

    leave Contains and Index

--- cmd/util/util.go
+++ cmd/util/util.go
@@ -32,6 +32,19 @@ func MustBindEnv(input ...string) {
 	}
 }
 
+func Contains[E comparable](s []E, v E) bool {
+	return Index(s, v) >= 0
+}
+
+func Index[E comparable](s []E, v E) int {
+	for i, vs := range s {
+		if v == vs {
+			return i
+		}
+	}
+	return -1
+}
+
 func MustBootstrapDatastore(t testing.TB, engine string) (storagefixtures.DatastoreTestContainer, storage.OpenFGADatastore, string, error) {
 	container := storagefixtures.RunDatastoreTestContainer(t, engine)
 
