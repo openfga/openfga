40288a46e5f026161d53effac2926bc10f30a7ce
Jonathan Whitaker <jon.whitaker@auth0.com>
2023-11-20 16:42:52 -0700

    chore(abac): add context size limit validation middleware

---
+++ pkg/middleware/validator/rpc_with_context.go
@@ -0,0 +1,40 @@
+package validator
+
+import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+const (
+	maxRequestContextSizeBytes = 131072 // 128KiB
+)
+
+type requestWithContext interface {
+	GetContext() *structpb.Struct
+}
+
+func requestContextWithinLimits(
+	req interface{},
+) error {
+
+	reqWithContext, ok := req.(requestWithContext)
+	if !ok {
+		return nil
+	}
+
+	reqContext := reqWithContext.GetContext()
+
+	bytes, err := proto.Marshal(reqContext)
+	if err != nil {
+		return err
+	}
+
+	contextSize := len(bytes)
+	if contextSize > maxRequestContextSizeBytes {
+		return status.Errorf(codes.InvalidArgument, "request 'context' size limit exceeded - %d (bytes) exceeds limit of %d (bytes)", contextSize, maxRequestContextSizeBytes)
+	}
+
+	return nil
+}
--- pkg/middleware/validator/validator.go
+++ pkg/middleware/validator/validator.go
@@ -25,10 +25,15 @@ func RequestIsValidatedFromContext(ctx context.Context) bool {
 
 // UnaryServerInterceptor returns a new unary server interceptor that runs request validations and injects a bool in the context indicating that validation has been run.
 func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
-	validator := grpcvalidator.UnaryServerInterceptor()
+	requestValidator := grpcvalidator.UnaryServerInterceptor()
 
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		return validator(ctx, req, info, func(ctx context.Context, req interface{}) (interface{}, error) {
+		return requestValidator(ctx, req, info, func(ctx context.Context, req interface{}) (interface{}, error) {
+
+			if err := requestContextWithinLimits(req); err != nil {
+				return nil, err
+			}
+
 			return handler(contextWithRequestIsValidated(ctx), req)
 		})
 	}
@@ -36,10 +41,11 @@ func UnaryServerInterceptor() grpc.UnaryServerInterceptor {
 
 // StreamServerInterceptor returns a new streaming server interceptor that runs request validations and injects a bool in the context indicating that validation has been run.
 func StreamServerInterceptor() grpc.StreamServerInterceptor {
-	validator := grpcvalidator.StreamServerInterceptor()
+	requestValidator := grpcvalidator.StreamServerInterceptor()
 
 	return func(srv interface{}, stream grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
-		return validator(srv, stream, info, func(srv interface{}, ss grpc.ServerStream) error {
+		return requestValidator(srv, stream, info, func(srv interface{}, ss grpc.ServerStream) error {
+
 			return handler(srv, &recvWrapper{
 				ctx:          contextWithRequestIsValidated(stream.Context()),
 				ServerStream: ss,
@@ -51,6 +57,18 @@ func StreamServerInterceptor() grpc.StreamServerInterceptor {
 type recvWrapper struct {
 	ctx context.Context
 	grpc.ServerStream
+}
+
+func (s *recvWrapper) RecvMsg(m any) error {
+	if err := s.ServerStream.RecvMsg(m); err != nil {
+		return err
+	}
+
+	if err := requestContextWithinLimits(m); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (r *recvWrapper) Context() context.Context {
