d4cfbd9f2160938b027f3ebd91c6774c1bf65e78
José Padilla <jose.padilla@okta.com>
2023-12-05 12:07:58 -0500

    add more test

--- pkg/storage/postgres/postgres_test.go
+++ pkg/storage/postgres/postgres_test.go
@@ -156,7 +156,9 @@ func TestReadAuthorizationModelUnmarshallError(t *testing.T) {
 	require.Contains(t, err.Error(), "cannot parse invalid wire-format data")
 }
 
-func TestReadUserTupleNullCondition(t *testing.T) {
+// TestAllowNullCondition tests that tuples existing before
+// migration 005_add_conditions_to_tuples can be successfully read.
+func TestAllowNullCondition(t *testing.T) {
 	ctx := context.Background()
 	testDatastore := storagefixtures.RunDatastoreTestContainer(t, "postgres")
 
@@ -171,11 +173,51 @@ func TestReadUserTupleNullCondition(t *testing.T) {
 			condition_name, condition_context, inserted_at
 		) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW());
 	`
-	ulid := ulid.Make().String()
-	_, err = ds.db.ExecContext(ctx, stmt, "store", "folder", "2021-budget", "owner", "user:anne", "user", ulid, nil, nil)
+	_, err = ds.db.ExecContext(
+		ctx, stmt, "store", "folder", "2021-budget", "owner", "user:anne", "user",
+		ulid.Make().String(), nil, nil,
+	)
 	require.NoError(t, err)
 
 	tk := tuple.NewTupleKey("folder:2021-budget", "owner", "user:anne")
-	_, err = ds.ReadUserTuple(ctx, "store", tk)
+	iter, err := ds.Read(ctx, "store", tk)
 	require.NoError(t, err)
+	defer iter.Stop()
+
+	curTuple, err := iter.Next(ctx)
+	require.NoError(t, err)
+	require.Equal(t, tk, curTuple.Key)
+
+	userTuple, err := ds.ReadUserTuple(ctx, "store", tk)
+	require.NoError(t, err)
+	require.Equal(t, tk, userTuple.Key)
+
+	tk2 := tuple.NewTupleKey("folder:2022-budget", "viewer", "user:anne")
+	_, err = ds.db.ExecContext(
+		ctx, stmt, "store", "folder", "2022-budget", "viewer", "user:anne", "userset",
+		ulid.Make().String(), nil, nil,
+	)
+
+	require.NoError(t, err)
+	iter, err = ds.ReadUsersetTuples(ctx, "store", storage.ReadUsersetTuplesFilter{Object: "folder:2022-budget"})
+	require.NoError(t, err)
+	defer iter.Stop()
+
+	curTuple, err = iter.Next(ctx)
+	require.NoError(t, err)
+	require.Equal(t, tk2, curTuple.Key)
+
+	iter, err = ds.ReadStartingWithUser(ctx, "store", storage.ReadStartingWithUserFilter{
+		ObjectType: "folder",
+		Relation:   "owner",
+		UserFilter: []*openfgav1.ObjectRelation{
+			{Object: "user:anne"},
+		},
+	})
+	require.NoError(t, err)
+	defer iter.Stop()
+
+	curTuple, err = iter.Next(ctx)
+	require.NoError(t, err)
+	require.Equal(t, tk, curTuple.Key)
 }
