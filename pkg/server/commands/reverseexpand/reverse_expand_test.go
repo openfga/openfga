c26f6d2f74fc5d7ccff9d0b04cd80b18bcc9985c
Adrian Tam <adrian.tam@okta.com>
2024-01-08 14:11:42 -0500

    fix: ensure ut close channel properly

--- pkg/server/commands/reverseexpand/reverse_expand_test.go
+++ pkg/server/commands/reverseexpand/reverse_expand_test.go
@@ -144,6 +144,14 @@ type document
 	case <-done:
 		// OK!
 	}
+
+	select {
+	case <-done:
+		t.Log("OK!")
+		return
+	case <-time.After(30 * time.Millisecond):
+		require.FailNow(t, "timed out")
+	}
 }
 
 // errorIterator is a mock iterator that returns error when calling next on the second Next call
