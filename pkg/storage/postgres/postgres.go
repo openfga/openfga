00a05d6841c84168cf477fb437f4d763d554d236
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-01-30 17:09:35 -0300

    undo whitespace changes

--- pkg/storage/postgres/postgres.go
+++ pkg/storage/postgres/postgres.go
@@ -172,7 +172,6 @@ func (p *Postgres) read(ctx context.Context, store string, tupleKey *openfgav1.T
 	if opts != nil {
 		sb = sb.OrderBy("ulid")
 	}
-
 	objectType, objectID := tupleUtils.SplitObject(tupleKey.GetObject())
 	if objectType != "" {
 		sb = sb.Where(sq.Eq{"object_type": objectType})
