93fefa548929b9101d043b4f551666be27ac3ae0
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-02-01 19:26:49 -0300

    chore: support more error codes from storage

--- pkg/server/errors/errors_test.go
+++ pkg/server/errors/errors_test.go
@@ -4,7 +4,10 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/openfga/openfga/pkg/storage"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func TestInternalErrorDontLeakInternals(t *testing.T) {
@@ -18,4 +21,41 @@ func TestInternalErrorsWithNoMessageReturnsInternalServiceError(t *testing.T) {
 
 	expected := InternalServerErrorMsg
 	require.Contains(t, err.Error(), expected)
+}
+
+func TestHandleStorageErrors(t *testing.T) {
+	tests := map[string]struct {
+		storageErr              error
+		expectedTranslatedError error
+	}{
+		`invalid_token`: {
+			storageErr:              storage.ErrInvalidContinuationToken,
+			expectedTranslatedError: InvalidContinuationToken,
+		},
+		`invalid_token_for_read_changes_api`: {
+			storageErr:              storage.ErrMismatchObjectType,
+			expectedTranslatedError: MismatchObjectType,
+		},
+		`context_cancelled`: {
+			storageErr:              storage.ErrCancelled,
+			expectedTranslatedError: RequestCancelled,
+		},
+		`context_deadline_exceeeded`: {
+			storageErr:              storage.ErrDeadlineExceeded,
+			expectedTranslatedError: RequestDeadlineExceeded,
+		},
+		`invalid_write_input`: {
+			storageErr:              storage.ErrInvalidWriteInput,
+			expectedTranslatedError: WriteFailedDueToInvalidInput(storage.ErrInvalidWriteInput),
+		},
+		`transaction_failed`: {
+			storageErr:              storage.ErrTransactionalWriteFailed,
+			expectedTranslatedError: status.Error(codes.Aborted, storage.ErrTransactionalWriteFailed.Error()),
+		},
+	}
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			require.ErrorIs(t, HandleError("", test.storageErr), test.expectedTranslatedError)
+		})
+	}
 }
