7aa2ce9ece2d403902d061b047e3c82959003b63
Adrian Tam <adrian.tam@okta.com>
2024-02-23 17:21:30 -0500

    update unit test as suggested in code review

--- pkg/storage/storagewrappers/caching_test.go
+++ pkg/storage/storagewrappers/caching_test.go
@@ -3,22 +3,25 @@ package storagewrappers
 import (
 	"context"
 	"fmt"
+
 	"testing"
 
 	"github.com/oklog/ulid/v2"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 
-	"github.com/openfga/openfga/pkg/storage/memory"
+	"github.com/openfga/openfga/internal/mocks"
 	"github.com/openfga/openfga/pkg/typesystem"
 )
 
 func TestReadAuthorizationModel(t *testing.T) {
 	ctx := context.Background()
-	memoryBackend := memory.New()
-	cachingBackend := NewCachedOpenFGADatastore(memoryBackend, 5)
-	defer cachingBackend.Close()
+	mockController := gomock.NewController(t)
+	mockController.Finish()
 
+	mockDatastore := mocks.NewMockOpenFGADatastore(mockController)
+	cachingBackend := NewCachedOpenFGADatastore(mockDatastore, 5)
 	model := &openfgav1.AuthorizationModel{
 		Id:            ulid.Make().String(),
 		SchemaVersion: typesystem.SchemaVersion1_1,
@@ -32,8 +35,13 @@ func TestReadAuthorizationModel(t *testing.T) {
 		},
 	}
 	storeID := ulid.Make().String()
+	gomock.InOrder(
+		mockDatastore.EXPECT().WriteAuthorizationModel(gomock.Any(), storeID, gomock.Any()).Times(1).Return(nil),
+		mockDatastore.EXPECT().ReadAuthorizationModel(gomock.Any(), storeID, model.GetId()).Times(1).Return(model, nil),
+		mockDatastore.EXPECT().FindLatestAuthorizationModel(gomock.Any(), storeID).Times(1).Return(model, nil),
+	)
 
-	err := memoryBackend.WriteAuthorizationModel(ctx, storeID, model)
+	err := cachingBackend.WriteAuthorizationModel(ctx, storeID, model)
 	require.NoError(t, err)
 
 	// Check that first hit to cache -> miss.
