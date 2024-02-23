f2ec2958c36b5848c6158b6bc3fa1a0c04b179f5
Will Vedder <willvedd@gmail.com>
2024-02-23 17:38:00 -0500

    Adding tracing + attributes to cycle detect resolver

--- internal/graph/cycle_check_resolver.go
+++ internal/graph/cycle_check_resolver.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/openfga/openfga/pkg/tuple"
+	"go.opentelemetry.io/otel/attribute"
 )
 
 type CycleDetectionCheckResolver struct {
@@ -27,13 +28,20 @@ func (c *CycleDetectionCheckResolver) ResolveCheck(
 	ctx context.Context,
 	req *ResolveCheckRequest,
 ) (*ResolveCheckResponse, error) {
+	ctx, span := tracer.Start(ctx, "ResolveCheck")
+	defer span.End()
+	span.SetAttributes(attribute.String("resolver_type", "CycleDetectionCheckResolver"))
+	span.SetAttributes(attribute.String("tuple_key", req.GetTupleKey().String()))
+
 	key := tuple.TupleKeyToString(req.GetTupleKey())
 
 	if req.VisitedPaths == nil {
 		req.VisitedPaths = map[string]struct{}{}
 	}
 
-	if _, ok := req.VisitedPaths[key]; ok {
+	_, cycleDetected := req.VisitedPaths[key]
+	span.SetAttributes(attribute.Bool("cycle_detected", cycleDetected))
+	if cycleDetected {
 		return nil, ErrCycleDetected
 	}
 
