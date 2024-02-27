d9e666c81516b39bd987426a2bae03678a1b193c
Will Vedder <willvedd@gmail.com>
2024-02-27 18:50:08 -0500

    Simplifying dependencies

--- internal/graph/cycle_check_resolver_test.go
+++ internal/graph/cycle_check_resolver_test.go
@@ -4,16 +4,12 @@ import (
 	"context"
 	"testing"
 
-	"github.com/oklog/ulid/v2"
-	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
+	"go.uber.org/mock/gomock"
 
-	"github.com/openfga/openfga/pkg/storage"
-	"github.com/openfga/openfga/pkg/storage/memory"
-	"github.com/openfga/openfga/pkg/testutils"
 	"github.com/openfga/openfga/pkg/tuple"
-	"github.com/openfga/openfga/pkg/typesystem"
 )
 
 func TestCycleDetectionCheckResolver(t *testing.T) {
@@ -21,60 +17,42 @@ func TestCycleDetectionCheckResolver(t *testing.T) {
 		goleak.VerifyNone(t)
 	})
 
-	ds := memory.New()
-	t.Cleanup(ds.Close)
-	storeID := ulid.Make().String()
-
-	err := ds.Write(context.Background(), storeID, nil, []*openfgav1.TupleKey{
-		tuple.NewTupleKey("document:1", "viewer3", "document:1#viewer3"),
-		tuple.NewTupleKey("document:1", "viewer1", "user:1"),
-	})
-	require.NoError(t, err)
-
-	model := testutils.MustTransformDSLToProtoWithID(`model
-	schema 1.1
-  
-  type user
-  
-  type document
-	relations
-		define viewer1: [user, document#viewer1]
-		define viewer2: viewer1 or viewer2
-		define viewer3: viewer1 or viewer2`)
-
-	ctx := typesystem.ContextWithTypesystem(context.Background(), typesystem.New(model))
-
-	ctx = storage.ContextWithRelationshipTupleReader(ctx, ds)
+	ctx := context.Background()
 
 	cycleDetectionCheckResolver := NewCycleDetectionCheckResolver()
-	t.Cleanup(cycleDetectionCheckResolver.Close)
-	localCheckResolver := NewLocalChecker()
-	t.Cleanup(localCheckResolver.Close)
 
-	t.Run("detects_cycle_and_returns_no_error", func(t *testing.T) {
-		cycleDetectionCheckResolver.SetDelegate(localCheckResolver)
-		localCheckResolver.SetDelegate(cycleDetectionCheckResolver)
+	t.Run("detects_cycle_and_cycle_detected_error", func(t *testing.T) {
+		cyclicalTuple := tuple.NewTupleKey("document:1", "viewer", "user:will")
+
+		visitedPaths := make(map[string]struct{}, 0)
+		visitedPaths[tuple.TupleKeyToString(cyclicalTuple)] = struct{}{}
 
 		resp, err := cycleDetectionCheckResolver.ResolveCheck(ctx, &ResolveCheckRequest{
-			StoreID:            storeID,
-			TupleKey:           tuple.NewTupleKey("document:1", "viewer3", "user:jon"),
-			ContextualTuples:   nil,
+			StoreID:            uuid.NewString(),
+			TupleKey:           cyclicalTuple,
 			ResolutionMetadata: &ResolutionMetadata{Depth: defaultResolveNodeLimit},
+			VisitedPaths:       visitedPaths,
 		})
 
-		require.NoError(t, err)
+		require.ErrorIs(t, err, ErrCycleDetected)
 		require.False(t, resp.GetAllowed())
 	})
 
 	t.Run("correctly_undetects_cycle_and_returns_no_error", func(t *testing.T) {
-		cycleDetectionCheckResolver.SetDelegate(localCheckResolver)
-		localCheckResolver.SetDelegate(cycleDetectionCheckResolver)
+		ctrl := gomock.NewController(t)
+		t.Cleanup(ctrl.Finish)
+
+		mockLocalChecker := NewMockCheckResolver(ctrl)
+		mockLocalChecker.EXPECT().ResolveCheck(ctx, gomock.Any()).Return(&ResolveCheckResponse{
+			Allowed: true,
+		}, nil).Times(1)
+		cycleDetectionCheckResolver.SetDelegate(mockLocalChecker)
 
 		resp, err := cycleDetectionCheckResolver.ResolveCheck(ctx, &ResolveCheckRequest{
-			StoreID:            storeID,
-			TupleKey:           tuple.NewTupleKey("document:1", "viewer1", "user:1"),
-			ContextualTuples:   nil,
+			StoreID:            uuid.NewString(),
+			TupleKey:           tuple.NewTupleKey("document:1", "viewer", "user:will"),
 			ResolutionMetadata: &ResolutionMetadata{Depth: defaultResolveNodeLimit},
+			VisitedPaths:       map[string]struct{}{},
 		})
 
 		require.NoError(t, err)
