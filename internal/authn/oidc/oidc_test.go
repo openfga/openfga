b68626a09c6bac2236bb96f8df4bbea4571b7f94
Edwin Marrima <edwinmarrima@gmail.com>
2024-03-04 20:17:04 +0200

    refactor: test implementation

--- internal/authn/oidc/oidc_test.go
+++ internal/authn/oidc/oidc_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/metadata"
 	"log"
+	"strings"
 	"testing"
 )
 
@@ -22,22 +23,121 @@ func TestRemoteOidcAuthenticator_Authenticate(t *testing.T) {
 		////then
 		require.Equal(t, authn.ErrMissingBearerToken, err)
 	})
-	t.Run("When JWT and JWK kid don't match, returns 'invalid bearer token'", func(t *testing.T) {
-		//given
-		oidc, requestContext, err := quickConfigSetup("kid_1", "kid_2", "", "", jwt.MapClaims{}, nil)
-		//when
-		_, err = oidc.Authenticate(requestContext)
-		//then
-		require.Contains(t, err.Error(), "invalid bearer token")
-	})
-	t.Run("When token is signed using different public/private key pairs, returns  'invalid bearer token'", func(t *testing.T) {
-		//given
-		privateKey, _ := generateJWTSignatureKeys()
-		oidc, requestContext, err := quickConfigSetup("kid_1", "kid_1", "", "", jwt.MapClaims{}, privateKey)
-		//when
-		_, err = oidc.Authenticate(requestContext)
-		//then
-		require.Contains(t, err.Error(), "invalid bearer token")
+	testCases := []struct {
+		testDescription string
+		testSetup       func() (*RemoteOidcAuthenticator, context.Context, error)
+		expectedError   string
+	}{
+		{
+			testDescription: "When JWT and JWK kid don't match, returns 'invalid bearer token'",
+			testSetup: func() (*RemoteOidcAuthenticator, context.Context, error) {
+				return quickConfigSetup("kid_1", "kid_2", "", "", jwt.MapClaims{}, nil)
+			},
+			expectedError: "invalid bearer token",
+		},
+		{
+			testDescription: "When token is signed using different public/private key pairs, returns  'invalid bearer token'",
+			testSetup: func() (*RemoteOidcAuthenticator, context.Context, error) {
+				privateKey, _ := generateJWTSignatureKeys()
+				return quickConfigSetup("kid_1", "kid_1", "", "", jwt.MapClaims{}, privateKey)
+			},
+			expectedError: "invalid bearer token",
+		},
+		{
+			testDescription: "When token's issuer does not match the one provided in the server configuration, MUST return 'invalid issuer' error.",
+			testSetup: func() (*RemoteOidcAuthenticator, context.Context, error) {
+				return quickConfigSetup(
+					"kid_1",
+					"kid_1",
+					"right_issuer",
+					"",
+					jwt.MapClaims{
+						"iss": "wrong_issuer",
+					},
+					nil,
+				)
+			},
+			expectedError: "invalid issuer",
+		},
+		{
+			testDescription: "When token's audience does not match the one provided in the server configuration, MUST return 'invalid audience' error.",
+			testSetup: func() (*RemoteOidcAuthenticator, context.Context, error) {
+				return quickConfigSetup(
+					"kid_1",
+					"kid_1",
+					"right_issuer",
+					"right_audience",
+					jwt.MapClaims{
+						"iss": "right_issuer",
+						"aud": "wrong_audience",
+					},
+					nil,
+				)
+			},
+		},
+		{
+			testDescription: "When the subject of the token is not a string, MUST return 'invalid subject' error.",
+			testSetup: func() (*RemoteOidcAuthenticator, context.Context, error) {
+				return quickConfigSetup(
+					"kid_1",
+					"kid_1",
+					"right_issuer",
+					"right_audience",
+					jwt.MapClaims{
+						"iss": "right_issuer",
+						"aud": "right_audience",
+						"sub": 12,
+					},
+					nil,
+				)
+			},
+			expectedError: "invalid subject",
+		},
+	}
+
+	for _, testC := range testCases {
+		t.Run(testC.testDescription, func(t *testing.T) {
+			//given
+			oidc, requestContext, err := testC.testSetup()
+			//when
+			_, err = oidc.Authenticate(requestContext)
+			//then
+			require.Contains(t, err.Error(), testC.expectedError)
+		})
+	}
+
+	// Success testcases
+	t.Run("When the token is valid, it MUST return the token subject and its associated scopes.", func(t *testing.T) {
+		// given
+		scopes := "offline_access read write delete"
+		oidc, requestContext, err := quickConfigSetup(
+			"kid_1",
+			"kid_1",
+			"right_issuer",
+			"right_audience",
+			jwt.MapClaims{
+				"iss":   "right_issuer",
+				"aud":   "right_audience",
+				"sub":   "openfga client",
+				"scope": scopes,
+			},
+			nil,
+		)
+		if err != nil {
+			t.Fatal(err)
+		}
+		// when
+		authClaims, err := oidc.Authenticate(requestContext)
+		// then
+		require.Nil(t, err)
+		require.Equal(t, "openfga client", authClaims.Subject)
+		scopesList := strings.Split(scopes, " ")
+		require.Equal(t, len(scopesList), len(authClaims.Scopes))
+		for _, scope := range scopesList {
+			_, ok := authClaims.Scopes[scope]
+			require.True(t, ok)
+		}
+
 	})
 }
 
