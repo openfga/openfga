fce511329fcfe6f581ff2993354491754668f703
Poovamraj T T <poovamraj@gmail.com>
2024-03-07 22:04:08 +0100

    Testing list objects dispatch count

--- pkg/server/commands/list_objects_test.go
+++ pkg/server/commands/list_objects_test.go
@@ -1,6 +1,13 @@
 package commands
 
 import (
+	"context"
+	"github.com/oklog/ulid/v2"
+	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+	parser "github.com/openfga/language/pkg/go/transformer"
+	"github.com/openfga/openfga/pkg/storage"
+	"github.com/openfga/openfga/pkg/tuple"
+	"github.com/openfga/openfga/pkg/typesystem"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -21,4 +28,59 @@ func TestNewListObjectsQuery(t *testing.T) {
 		require.Nil(t, q)
 		require.Error(t, err)
 	})
+}
+
+func TestCheckDispatchCount(t *testing.T) {
+	ds := memory.New()
+	ctx := storage.ContextWithRelationshipTupleReader(context.Background(), ds)
+
+	t.Run("dispatch_count_ttu", func(t *testing.T) {
+		storeID := ulid.Make().String()
+		model := parser.MustTransformDSLToProto(`model
+  schema 1.1
+
+type user
+
+type folder
+  	relations
+		define viewer: [user] 
+`)
+
+		err := ds.Write(ctx, storeID, nil, []*openfgav1.TupleKey{
+			tuple.NewTupleKey("folder:C", "viewer", "user:jon"),
+			tuple.NewTupleKey("folder:B", "viewer", "user:jon"),
+			tuple.NewTupleKey("folder:A", "viewer", "user:jon"),
+		})
+		require.NoError(t, err)
+
+		typesys, err := typesystem.NewAndValidate(
+			context.Background(),
+			model,
+		)
+		require.NoError(t, err)
+
+		ctx = typesystem.ContextWithTypesystem(ctx, typesys)
+
+		checker := graph.NewLocalCheckerWithCycleDetection(
+			graph.WithMaxConcurrentReads(1),
+		)
+
+		q, err := NewListObjectsQuery(
+			ds,
+			checker,
+		)
+
+		resp, err := q.Execute(ctx, &openfgav1.ListObjectsRequest{
+			Type:     "folder",
+			Relation: "viewer",
+			User:     "user:jon",
+		})
+
+		require.NoError(t, err)
+		t.Log(resp.Objects)
+		t.Log(*resp.ResolutionMetadata.DispatchCount)
+
+		//require.Equal(t, uint32(3), resp.ResolutionMetadata.DispatchCount)
+	})
+
 }
