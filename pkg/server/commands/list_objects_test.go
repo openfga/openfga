5829facd667fa76f8f57700e99478d5d2d9bae8e
Poovamraj T T <poovamraj@gmail.com>
2024-03-07 23:02:44 +0100

    Implement tests for ListObjects

--- pkg/server/commands/list_objects_test.go
+++ pkg/server/commands/list_objects_test.go
@@ -34,53 +34,120 @@ func TestCheckDispatchCount(t *testing.T) {
 	ds := memory.New()
 	ctx := storage.ContextWithRelationshipTupleReader(context.Background(), ds)
 
-	t.Run("dispatch_count_ttu", func(t *testing.T) {
-		storeID := ulid.Make().String()
-		model := parser.MustTransformDSLToProto(`model
-  schema 1.1
+	tests := []struct {
+		name                  string
+		model                 string
+		tuples                []*openfgav1.TupleKey
+		objectType            string
+		relation              string
+		user                  string
+		expectedDispatchCount uint32
+	}{
+		{
+			name: "Test direct relation",
+			model: `model
+schema 1.1
 
 type user
 
 type folder
   	relations
 		define viewer: [user] 
-`)
+`,
+			tuples: []*openfgav1.TupleKey{
+				tuple.NewTupleKey("folder:C", "viewer", "user:jon"),
+				tuple.NewTupleKey("folder:B", "viewer", "user:jon"),
+				tuple.NewTupleKey("folder:A", "viewer", "user:jon"),
+			},
+			objectType:            "folder",
+			relation:              "viewer",
+			user:                  "user:jon",
+			expectedDispatchCount: 3,
+		},
+		{
+			name: "Test union relation",
+			model: `model
+schema 1.1
 
-		err := ds.Write(ctx, storeID, nil, []*openfgav1.TupleKey{
-			tuple.NewTupleKey("folder:C", "viewer", "user:jon"),
-			tuple.NewTupleKey("folder:B", "viewer", "user:jon"),
-			tuple.NewTupleKey("folder:A", "viewer", "user:jon"),
+type user
+
+type folder
+  	relations
+		define editor: [user]
+		define viewer: [user] or editor 
+`,
+			tuples: []*openfgav1.TupleKey{
+				tuple.NewTupleKey("folder:C", "editor", "user:jon"),
+				tuple.NewTupleKey("folder:B", "viewer", "user:jon"),
+				tuple.NewTupleKey("folder:A", "viewer", "user:jon"),
+			},
+			objectType:            "folder",
+			relation:              "viewer",
+			user:                  "user:jon",
+			expectedDispatchCount: 4,
+		},
+		{
+			name: "Test intersection relation",
+			model: `model
+schema 1.1
+
+type user
+
+type folder
+  	relations
+		define editor: [user]
+		define can_delete: [user] and editor 
+`,
+			tuples: []*openfgav1.TupleKey{
+				tuple.NewTupleKey("folder:C", "can_delete", "user:jon"),
+				tuple.NewTupleKey("folder:B", "viewer", "user:jon"),
+				tuple.NewTupleKey("folder:A", "viewer", "user:jon"),
+			},
+			objectType:            "folder",
+			relation:              "can_delete",
+			user:                  "user:jon",
+			expectedDispatchCount: 2,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+			storeID := ulid.Make().String()
+			model := parser.MustTransformDSLToProto(test.model)
+
+			err := ds.Write(ctx, storeID, nil, test.tuples)
+			require.NoError(t, err)
+
+			typesys, err := typesystem.NewAndValidate(
+				context.Background(),
+				model,
+			)
+			require.NoError(t, err)
+
+			ctx = typesystem.ContextWithTypesystem(ctx, typesys)
+
+			checker := graph.NewLocalCheckerWithCycleDetection(
+				graph.WithMaxConcurrentReads(1),
+			)
+
+			q, err := NewListObjectsQuery(
+				ds,
+				checker,
+			)
+
+			resp, err := q.Execute(ctx, &openfgav1.ListObjectsRequest{
+				StoreId:  storeID,
+				Type:     test.objectType,
+				Relation: test.relation,
+				User:     test.user,
+			})
+
+			require.NoError(t, err)
+			t.Log(resp.Objects)
+			t.Log(*resp.ResolutionMetadata.DispatchCount)
+
+			require.Equal(t, test.expectedDispatchCount, *resp.ResolutionMetadata.DispatchCount)
 		})
-		require.NoError(t, err)
-
-		typesys, err := typesystem.NewAndValidate(
-			context.Background(),
-			model,
-		)
-		require.NoError(t, err)
-
-		ctx = typesystem.ContextWithTypesystem(ctx, typesys)
-
-		checker := graph.NewLocalCheckerWithCycleDetection(
-			graph.WithMaxConcurrentReads(1),
-		)
-
-		q, err := NewListObjectsQuery(
-			ds,
-			checker,
-		)
-
-		resp, err := q.Execute(ctx, &openfgav1.ListObjectsRequest{
-			Type:     "folder",
-			Relation: "viewer",
-			User:     "user:jon",
-		})
-
-		require.NoError(t, err)
-		t.Log(resp.Objects)
-		t.Log(*resp.ResolutionMetadata.DispatchCount)
-
-		//require.Equal(t, uint32(3), resp.ResolutionMetadata.DispatchCount)
-	})
-
+	}
 }
