7f21ca9fde539db1616c607913679a9e9e09c15f
Poovamraj T T <poovamraj@gmail.com>
2024-03-08 19:59:13 +0100

    Update pkg/server/commands/list_objects.go
    Co-authored-by: Maria Ines Parnisari <maria.inesparnisari@okta.com>

--- pkg/server/commands/list_objects.go
+++ pkg/server/commands/list_objects.go
@@ -61,7 +61,7 @@ type ResolutionMetadata struct {
 	// The total number of database reads from reverse_expand and Check (if any) to complete the ListObjects request
 	QueryCount *uint32
 
-	// The total number of dispatch counts from reverse_expand and check resolution to complete the ListObjects request
+	// The total number of dispatch counts from reverse_expand and check resolutions (if any) to complete the ListObjects request
 	DispatchCount *uint32
 }
 
