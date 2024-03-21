d5ed93444c346d3805e9e2a6b18d1fe2f06a1a4c
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-03-21 09:40:18 -0700

    test: attempt at preventing 'bind: address already in use' (#1468)

--- tests/tests.go
+++ tests/tests.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
@@ -32,13 +33,22 @@ func StartServer(t testing.TB, cfg *serverconfig.Config) {
 	StartServerWithContext(t, cfg, serverCtx)
 }
 
-// StartServerWithContext starts a server with a specific ServerContext and waits until it is healthy.
+// StartServerWithContext starts a server in random ports and with a specific ServerContext and waits until it is healthy.
 // When the test ends, all resources are cleaned.
 func StartServerWithContext(t testing.TB, cfg *serverconfig.Config, serverCtx *run.ServerContext) {
 	container := storage.RunDatastoreTestContainer(t, cfg.Datastore.Engine)
 	cfg.Datastore.URI = container.GetConnectionURI(true)
 
 	ctx, cancel := context.WithCancel(context.Background())
+
+	httpPort, httpPortReleaser := serverconfig.TCPRandomPort()
+	cfg.HTTP.Addr = fmt.Sprintf("0.0.0.0:%d", httpPort)
+	grpcPort, grpcPortReleaser := serverconfig.TCPRandomPort()
+	cfg.GRPC.Addr = fmt.Sprintf("0.0.0.0:%d", grpcPort)
+
+	// these two functions release the ports so that the server can start listening on them
+	httpPortReleaser()
+	grpcPortReleaser()
 
 	serverDone := make(chan error)
 	go func() {
