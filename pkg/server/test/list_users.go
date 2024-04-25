1803733e8c0f20f7ac208060aefe7cf40fb056a6
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-04-25 16:37:30 -0700

    use test tables

--- pkg/server/test/list_users.go
+++ pkg/server/test/list_users.go
@@ -3,8 +3,6 @@ package test
 import (
 	"context"
 	"fmt"
-	"math"
-	"strconv"
 	"testing"
 
 	"github.com/oklog/ulid/v2"
@@ -18,128 +16,187 @@ import (
 	"github.com/openfga/openfga/pkg/typesystem"
 )
 
-// setupListUsersBenchmark writes the model and lots of tuples
-func setupListUsersBenchmark(b *testing.B, ds storage.OpenFGADatastore, storeID string) (*typesystem.TypeSystem, int) {
-	b.Helper()
-	model := testutils.MustTransformDSLToProtoWithID(
-		`model
-			schema 1.1
-		type user
-		type folder
-			relations
-				define viewer: [user]
-		type document
-			relations
-				define viewer: [user, user with condTrue]
-				define parent: [folder]
-				define can_view: viewer or viewer from parent
-				define can_view_conditional: viewer or viewer from parent
-				define blocked: [user]
-				define public_but_not: [user:*] but not blocked
-
-		condition condTrue(param: bool) {
-			param == true
-		}`)
-	typeSystem, err := typesystem.NewAndValidate(context.Background(), model)
-	require.NoError(b, err)
-	err = ds.WriteAuthorizationModel(context.Background(), storeID, model)
-	require.NoError(b, err)
-
-	numberOfUsersWithAccess := 0
-	for i := 0; i < 100; i++ {
-		var tuples []*openfgav1.TupleKey
-
-		for j := 0; j < ds.MaxTuplesPerWrite(); j++ {
-			user := fmt.Sprintf("user:%s", strconv.Itoa(numberOfUsersWithAccess))
-
-			// one document viewable by many users
-			tuples = append(tuples, tuple.NewTupleKey("document:1", "viewer", user))
-			// one document viewable by many users with a condition
-			tuples = append(tuples, tuple.NewTupleKeyWithCondition("document:2", "viewer", user, "condTrue", nil))
-
-			numberOfUsersWithAccess += 1
-		}
-
-		err = ds.Write(context.Background(), storeID, nil, tuples)
-		require.NoError(b, err)
-	}
-	// and one public document
-	err = ds.Write(context.Background(), storeID, nil, []*openfgav1.TupleKey{
-		tuple.NewTupleKey("document:3", "public_but_not", "user:*"),
-	})
-	require.NoError(b, err)
-
-	return typeSystem, numberOfUsersWithAccess
-}
-
 func BenchmarkListUsers(b *testing.B, ds storage.OpenFGADatastore) {
-	ctx := context.Background()
-	storeID := ulid.Make().String()
-
-	ts, numberOfUsersWithAccess := setupListUsersBenchmark(b, ds, storeID)
-	ctx = typesystem.ContextWithTypesystem(ctx, ts)
-
 	var oneResultIterations, allResultsIterations int
 
-	b.Run("oneResult_without_conditions", func(b *testing.B) {
-		oneResultIterations = int(math.Inf(1))
-		// TODO add benchmark for when max_results = 1, use the same model and tuples as "allResults"
-		// so that we assert that we don't waste time computing extra results in that case
-		b.Skip("unimplemented")
-	})
+	benchmarkScenarios := map[string]struct {
+		inputModel            string
+		tupleGenerator        func() []*openfgav1.TupleKey
+		inputConfigMaxResults uint32
+		inputRequest          *openfgav1.ListUsersRequest
+		expectedResults       int
+	}{
+		`one_result_without_conditions`: {
+			inputModel: `model
+							schema 1.1
+						type user
+						type folder
+							relations
+								define viewer: [user]
+						type document
+							relations
+								define viewer: [user]
+								define parent: [folder]
+								define can_view: viewer or viewer from parent`,
+			tupleGenerator: func() []*openfgav1.TupleKey {
+				// same as the next benchmark, so that later we can compare times.
+				var tuples []*openfgav1.TupleKey
+				for i := 0; i < 100; i++ {
+					for j := 0; j < 50; j++ {
+						user := fmt.Sprintf("user:%s", ulid.Make().String())
+						// one document accessible by many users
+						tuples = append(tuples, tuple.NewTupleKey("document:1", "viewer", user))
+					}
+				}
+				return tuples
+			},
+			inputRequest: &openfgav1.ListUsersRequest{
+				Object:      &openfgav1.Object{Type: "document", Id: "1"},
+				Relation:    "can_view",
+				UserFilters: []*openfgav1.UserTypeFilter{{Type: "user"}},
+			},
+			inputConfigMaxResults: 100 * 50, // TODO change to 1
+			expectedResults:       100 * 50, // TODO change to 1
+		},
+		`all_results_without_conditions`: {
+			inputModel: `model
+							schema 1.1
+						type user
+						type folder
+							relations
+								define viewer: [user]
+						type document
+							relations
+								define viewer: [user]
+								define parent: [folder]
+								define can_view: viewer or viewer from parent`,
+			tupleGenerator: func() []*openfgav1.TupleKey {
+				// same as the previous benchmark, so that later we can compare times.
+				var tuples []*openfgav1.TupleKey
+				for i := 0; i < 100; i++ {
+					for j := 0; j < 50; j++ {
+						user := fmt.Sprintf("user:%s", ulid.Make().String())
+						// one document accessible by many users
+						tuples = append(tuples, tuple.NewTupleKey("document:1", "viewer", user))
+					}
+				}
+				return tuples
+			},
+			inputRequest: &openfgav1.ListUsersRequest{
+				Object:      &openfgav1.Object{Type: "document", Id: "1"},
+				Relation:    "can_view",
+				UserFilters: []*openfgav1.UserTypeFilter{{Type: "user"}},
+			},
+			inputConfigMaxResults: 0, // infinite
+			expectedResults:       100 * 50,
+		},
+		`all_results_with_conditions`: {
+			inputModel: `model
+							schema 1.1
+						type user
+						type folder
+							relations
+								define viewer: [user]
+						type document
+							relations
+								define viewer: [user with condTrue]
+								define parent: [folder]
+								define can_view_conditional: viewer or viewer from parent
 
-	b.Run("allResults_without_conditions", func(b *testing.B) {
-		b.ResetTimer()
-		listUsersQuery := listusers.NewListUsersQuery(ds,
-			listusers.WithListUserMaxResults(uint32(numberOfUsersWithAccess)))
-		for i := 0; i < b.N; i++ {
-			resp, err := listUsersQuery.ListUsers(ctx, &openfgav1.ListUsersRequest{
-				StoreId:              storeID,
-				AuthorizationModelId: ts.GetAuthorizationModelID(),
-				Object:               &openfgav1.Object{Type: "document", Id: "1"},
-				Relation:             "can_view",
-				UserFilters:          []*openfgav1.ListUsersFilter{{Type: "user"}},
-			})
+						condition condTrue(param: bool) {
+							param == true
+						}`,
+			tupleGenerator: func() []*openfgav1.TupleKey {
+				var tuples []*openfgav1.TupleKey
+				for i := 0; i < 100; i++ {
+					for j := 0; j < 50; j++ {
+						// one document accessible by many users
+						user := fmt.Sprintf("user:%s", ulid.Make().String())
+						tuples = append(tuples, tuple.NewTupleKeyWithCondition("document:1", "viewer", user, "condTrue", nil))
+					}
+				}
+				return tuples
+			},
+			inputRequest: &openfgav1.ListUsersRequest{
+				Object:      &openfgav1.Object{Type: "document", Id: "1"},
+				Relation:    "can_view_conditional",
+				UserFilters: []*openfgav1.UserTypeFilter{{Type: "user"}},
+				Context:     testutils.MustNewStruct(b, map[string]interface{}{"param": true}),
+			},
+			inputConfigMaxResults: 0, // infinite
+			expectedResults:       100 * 50,
+		},
+		`exclusion_without_conditions`: {
+			inputModel: `model
+							schema 1.1
+						type user
+						type document
+							relations
+								define blocked: [user]
+								define public_but_not: [user:*] but not blocked`,
+			tupleGenerator: func() []*openfgav1.TupleKey {
+				return []*openfgav1.TupleKey{
+					tuple.NewTupleKey("document:1", "public_but_not", "user:*"),
+				}
+			},
+			inputRequest: &openfgav1.ListUsersRequest{
+				Object:      &openfgav1.Object{Type: "document", Id: "1"},
+				Relation:    "public_but_not",
+				UserFilters: []*openfgav1.UserTypeFilter{{Type: "user"}},
+			},
+			inputConfigMaxResults: 0, // infinite
+			expectedResults:       1,
+		},
+	}
+
+	for name, bm := range benchmarkScenarios {
+		b.Run(name, func(b *testing.B) {
+			ctx := context.Background()
+			storeID := ulid.Make().String()
+
+			// arrange: write model
+			model := testutils.MustTransformDSLToProtoWithID(bm.inputModel)
+			typeSystem, err := typesystem.NewAndValidate(context.Background(), model)
 			require.NoError(b, err)
-			require.Len(b, resp.GetUsers(), numberOfUsersWithAccess)
-		}
-		allResultsIterations = b.N
-	})
-
-	require.Greater(b, oneResultIterations, allResultsIterations)
-
-	b.Run("allResults_with_conditions", func(b *testing.B) {
-		listUsersQuery := listusers.NewListUsersQuery(ds,
-			listusers.WithListUserMaxResults(uint32(numberOfUsersWithAccess)))
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			resp, err := listUsersQuery.ListUsers(ctx, &openfgav1.ListUsersRequest{
-				StoreId:              storeID,
-				AuthorizationModelId: ts.GetAuthorizationModelID(),
-				Object:               &openfgav1.Object{Type: "document", Id: "2"},
-				Relation:             "can_view_conditional",
-				UserFilters:          []*openfgav1.ListUsersFilter{{Type: "user"}},
-				Context:              testutils.MustNewStruct(b, map[string]interface{}{"param": true}),
-			})
+			err = ds.WriteAuthorizationModel(context.Background(), storeID, model)
 			require.NoError(b, err)
-			require.Len(b, resp.GetUsers(), numberOfUsersWithAccess)
-		}
-	})
 
-	b.Run("exclusion_without_conditions", func(b *testing.B) {
-		listUsersQuery := listusers.NewListUsersQuery(ds,
-			listusers.WithListUserMaxResults(uint32(numberOfUsersWithAccess)))
-		b.ResetTimer()
-		for i := 0; i < b.N; i++ {
-			resp, err := listUsersQuery.ListUsers(ctx, &openfgav1.ListUsersRequest{
-				StoreId:              storeID,
-				AuthorizationModelId: ts.GetAuthorizationModelID(),
-				Object:               &openfgav1.Object{Type: "document", Id: "3"},
-				Relation:             "public_but_not",
-				UserFilters:          []*openfgav1.ListUsersFilter{{Type: "user"}},
-			})
-			require.NoError(b, err)
-			require.Len(b, resp.GetUsers(), 1)
-		}
-	})
+			// arrange: write tuples
+			tuples := bm.tupleGenerator()
+			for i := 0; i < len(tuples); {
+				var tuplesToWrite []*openfgav1.TupleKey
+				for j := 0; j < ds.MaxTuplesPerWrite(); j++ {
+					if i == len(tuples) {
+						break
+					}
+					tuplesToWrite = append(tuplesToWrite, tuples[i])
+					i++
+				}
+				err = ds.Write(context.Background(), storeID, nil, tuplesToWrite)
+				require.NoError(b, err)
+			}
+
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				bm.inputRequest.StoreId = storeID
+				ctx = typesystem.ContextWithTypesystem(ctx, typeSystem)
+				resp, err := listusers.NewListUsersQuery(ds,
+					listusers.WithListUserMaxResults(bm.inputConfigMaxResults)).
+					ListUsers(ctx, bm.inputRequest)
+				require.NoError(b, err)
+				require.NotNil(b, resp)
+				require.Len(b, resp.GetUsers(), bm.expectedResults)
+			}
+			if name == "all_results_without_conditions" {
+				allResultsIterations = b.N
+			} else if name == "one_result_without_conditions" {
+				oneResultIterations = b.N
+			}
+		})
+	}
+	b.Log(oneResultIterations)
+	b.Log(allResultsIterations)
+	// TODO uncomment
+	//require.Greater(b, oneResultIterations, allResultsIterations)
 }
