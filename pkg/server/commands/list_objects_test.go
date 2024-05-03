67e0cc47582c1fdfaaa3e4fc553d8cf7b1383814
Poovamraj T T <poovamraj@gmail.com>
2024-05-03 12:41:50 +0200

    sort imports

--- pkg/server/commands/list_objects_test.go
+++ pkg/server/commands/list_objects_test.go
@@ -4,22 +4,19 @@ import (
 	"context"
 	"testing"
 
-	"github.com/openfga/openfga/internal/throttler"
-
+	"github.com/oklog/ulid/v2"
 	"go.uber.org/mock/gomock"
 
-	"github.com/openfga/openfga/internal/mocks"
-
-	"github.com/oklog/ulid/v2"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	parser "github.com/openfga/language/pkg/go/transformer"
-	"github.com/stretchr/testify/require"
-
 	"github.com/openfga/openfga/internal/graph"
+	"github.com/openfga/openfga/internal/mocks"
+	"github.com/openfga/openfga/internal/throttler"
 	"github.com/openfga/openfga/pkg/storage"
 	"github.com/openfga/openfga/pkg/storage/memory"
 	"github.com/openfga/openfga/pkg/tuple"
 	"github.com/openfga/openfga/pkg/typesystem"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNewListObjectsQuery(t *testing.T) {
