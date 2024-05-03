833335d19276cbf03a5498b53e5bcdd59e8ebb83
Poovamraj T T <poovamraj@gmail.com>
2024-05-03 16:35:05 +0200

    Fix import order

--- pkg/server/commands/list_objects_test.go
+++ pkg/server/commands/list_objects_test.go
@@ -5,12 +5,11 @@ import (
 	"testing"
 
 	"github.com/oklog/ulid/v2"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	parser "github.com/openfga/language/pkg/go/transformer"
-	"github.com/stretchr/testify/require"
-
 	"github.com/openfga/openfga/internal/graph"
 	"github.com/openfga/openfga/internal/mocks"
 	"github.com/openfga/openfga/internal/throttler"
