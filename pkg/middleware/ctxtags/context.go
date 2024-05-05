bc9b805acb92f8c04c1d5e64bc18c03997e91889
Ilya Alexin <ilya.alexin@gmail.com>
2024-05-06 00:22:03 +0200

    deleted unsued code

--- pkg/middleware/ctxtags/context.go
+++ pkg/middleware/ctxtags/context.go
@@ -16,8 +16,6 @@ var (
 type Tags interface {
 	// Set sets the given key in the metadata tags.
 	Set(key string, value interface{}) Tags
-	// Has checks if the given key exists.
-	Has(key string) bool
 	// Values returns a map of key to values.
 	// Do not modify the underlying map, please use Set instead.
 	Values() map[string]interface{}
@@ -32,11 +30,6 @@ func (t *mapTags) Set(key string, value interface{}) Tags {
 	return t
 }
 
-func (t *mapTags) Has(key string) bool {
-	_, ok := t.values[key]
-	return ok
-}
-
 func (t *mapTags) Values() map[string]interface{} {
 	return t.values
 }
@@ -45,10 +38,6 @@ type noopTags struct{}
 
 func (t *noopTags) Set(key string, value interface{}) Tags {
 	return t
-}
-
-func (t *noopTags) Has(key string) bool {
-	return false
 }
 
 func (t *noopTags) Values() map[string]interface{} {
--- pkg/middleware/ctxtags/zap/zap.go
+++ pkg/middleware/ctxtags/zap/zap.go
@@ -13,28 +13,11 @@ type ctxMarker struct{}
 
 type ctxLogger struct {
 	logger *zap.Logger
-	fields []zapcore.Field
 }
 
 var (
 	ctxMarkerKey = &ctxMarker{}
-	nullLogger   = zap.NewNop()
 )
-
-// Extract takes the call-scoped Logger from grpc_zap middleware.
-//
-// It always returns a Logger that has all the grpc_ctxtags updated.
-func Extract(ctx context.Context) *zap.Logger {
-	l, ok := ctx.Value(ctxMarkerKey).(*ctxLogger)
-	if !ok || l == nil {
-		return nullLogger
-	}
-	// Add grpc_ctxtags tags metadata until now.
-	fields := TagsToFields(ctx)
-	// Add zap fields added until now.
-	fields = append(fields, l.fields...)
-	return l.logger.With(fields...)
-}
 
 // TagsToFields transforms the Tags on the supplied context into zap fields.
 func TagsToFields(ctx context.Context) []zapcore.Field {
