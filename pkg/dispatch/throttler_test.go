2cfe98a970edbbf2f0946ff8d5c4deb9dd06b5d6
Poovamraj T T <poovamraj@gmail.com>
2024-05-16 14:33:48 -0500

    Update throttler_test.go

--- pkg/dispatch/throttler_test.go
+++ pkg/dispatch/throttler_test.go
@@ -2,8 +2,9 @@ package dispatch
 
 import (
 	"context"
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestContextWithThrottlingThreshold(t *testing.T) {
