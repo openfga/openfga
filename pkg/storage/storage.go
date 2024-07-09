df82103c2252b916865e438ec89158fe6a5d5d80
Jim Anderson <jim.anderson@okta.com>
2024-07-08 21:45:00 -0500

    gdocs fix

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -154,7 +154,7 @@ type RelationshipTupleReader interface {
 	Read(ctx context.Context, store string, tupleKey *openfgav1.TupleKey, options ReadOptions) (TupleIterator, error)
 
 	// ReadPage functions similarly to Read but includes support for pagination. It takes
-	// mandatory pagination options. PageSize will always be greater than zero.
+	// mandatory ReadPageOptions options. PageSize will always be greater than zero.
 	// It returns a slice of tuples along with a continuation token. This token can be used for retrieving subsequent pages of data.
 	// There is NO guarantee on the order of the tuples in one page.
 	ReadPage(
