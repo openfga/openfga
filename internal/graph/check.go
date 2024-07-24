242ada58f4e989dcc76deec40cceaf6a505fa5ef
Yamil Asusta <yamil.asusta@okta.com>
2024-07-24 00:21:53 -0400

    traverse computedusersets serially

--- internal/graph/check.go
+++ internal/graph/check.go
@@ -974,23 +974,53 @@ func (c *LocalChecker) checkDirect(parentctx context.Context, req *ResolveCheckR
 }
 
 // checkComputedUserset evaluates the Check request with the rewritten relation (e.g. the computed userset relation).
-func (c *LocalChecker) checkComputedUserset(ctx context.Context, req *ResolveCheckRequest, rewrite *openfgav1.Userset_ComputedUserset) CheckHandlerFunc {
+func (c *LocalChecker) checkComputedUserset(ctx context.Context, req *ResolveCheckRequest, rewrite *openfgav1.Userset) CheckHandlerFunc {
 	_, span := tracer.Start(ctx, "checkComputedUserset")
 	defer span.End()
 
-	childRequest := clone(req)
+	typesys, _ := typesystem.TypesystemFromContext(ctx)
 
-	rewrittenTupleKey := tuple.NewTupleKey(
+	tk := tuple.NewTupleKey(
 		req.GetTupleKey().GetObject(),
-		rewrite.ComputedUserset.GetRelation(),
+		req.GetTupleKey().GetRelation(),
 		req.GetTupleKey().GetUser(),
 	)
+	seen := map[string]struct{}{}
 
-	childRequest.TupleKey = rewrittenTupleKey
-
-	return func(ctx context.Context) (*ResolveCheckResponse, error) {
-		return c.ResolveCheck(ctx, childRequest)
+	for {
+		tk = tuple.NewTupleKey(
+			tk.GetObject(),
+			rewrite.GetComputedUserset().GetRelation(),
+			tk.GetUser(),
+		)
+		key := tuple.TupleKeyToString(tk)
+		if _, cycleDetected := seen[key]; cycleDetected {
+			return func(ctx context.Context) (*ResolveCheckResponse, error) {
+				return &ResolveCheckResponse{
+					Allowed: false,
+					ResolutionMetadata: &ResolveCheckResponseMetadata{
+						CycleDetected: true,
+					},
+				}, nil
+			}
+		}
+		seen[key] = struct{}{}
+		rw, err := typesys.GetRelation(tuple.GetType(tk.GetObject()), tk.GetRelation())
+		if err != nil {
+			return func(ctx context.Context) (*ResolveCheckResponse, error) {
+				return nil, fmt.Errorf("relation '%s' undefined for object type '%s'", tk.GetRelation(), tuple.GetType(tk.GetObject()))
+			}
+		}
+		if _, isComputed := rw.GetRewrite().GetUserset().(*openfgav1.Userset_ComputedUserset); !isComputed {
+			break
+		}
+		rewrite = rw.GetRewrite()
 	}
+
+	childRequest := clone(req)
+	childRequest.TupleKey = tk
+
+	return c.checkRewrite(ctx, childRequest, rewrite)
 }
 
 // checkTTUSlowPath is the slow path for checkTTU where we cannot short-circuit TTU evaluation and
