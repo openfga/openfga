be9b54bce25495dd539be2c391cac955a1d9c576
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-08-01 15:43:38 -0700

    fix: proper nesting of spans in Check API if computed usersets

--- internal/graph/check.go
+++ internal/graph/check.go
@@ -1036,9 +1036,6 @@ func (c *LocalChecker) checkDirect(parentctx context.Context, req *ResolveCheckR
 
 // checkComputedUserset evaluates the Check request with the rewritten relation (e.g. the computed userset relation).
 func (c *LocalChecker) checkComputedUserset(ctx context.Context, req *ResolveCheckRequest, rewrite *openfgav1.Userset) CheckHandlerFunc {
-	_, span := tracer.Start(ctx, "checkComputedUserset")
-	defer span.End()
-
 	rewrittenTupleKey := tuple.NewTupleKey(
 		req.GetTupleKey().GetObject(),
 		rewrite.GetComputedUserset().GetRelation(),
@@ -1049,6 +1046,8 @@ func (c *LocalChecker) checkComputedUserset(ctx context.Context, req *ResolveChe
 	childRequest.TupleKey = rewrittenTupleKey
 
 	return func(ctx context.Context) (*ResolveCheckResponse, error) {
+		ctx, span := tracer.Start(ctx, "checkComputedUserset")
+		defer span.End()
 		// No dispatch here, as we don't want to increase resolution depth.
 		return c.ResolveCheck(ctx, childRequest)
 	}
