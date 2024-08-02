230b4d3bd5e867ada66d790ec37b4d907abe55d7
Maria Ines Parnisari <maria.inesparnisari@okta.com>
2024-08-01 19:00:53 -0700

    modify godoc of errors.go

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -2,7 +2,8 @@ package errors
 
 import "errors"
 
-// Idea for this package is to hold all errors that can be eventually mapped to a distinct HTTP status code,
-// and that can be extended with more details if need be.
+// Idea for this package is to hold sentinel errors that can be eventually mapped to a distinct HTTP status code,
+// If you need to add more details to the error so it will surface to the customer,
+// use error wrapping (https://github.com/tomarrell/wrapcheck#why)
 
 var ErrUnknown = errors.New("internal server error")
