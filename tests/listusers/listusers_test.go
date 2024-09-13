d17a3068b7655a5452a15b7ad331bdb6c721c0cf
Dan Cech <dcech@grafana.com>
2024-09-13 10:53:03 -0400

    extend list users test timeout

--- tests/listusers/listusers_test.go
+++ tests/listusers/listusers_test.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"net/http"
 	"testing"
+	"time"
 
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	parser "github.com/openfga/language/pkg/go/transformer"
@@ -49,6 +50,9 @@ func testRunAll(t *testing.T, engine string) {
 	cfg := config.MustDefaultConfig()
 	cfg.Log.Level = "error"
 	cfg.Datastore.Engine = engine
+	cfg.ListUsersDeadline = 0 // no deadline
+	// extend the timeout for the tests, coverage makes them slower
+	cfg.RequestTimeout = 10 * time.Second
 
 	tests.StartServer(t, cfg)
 
