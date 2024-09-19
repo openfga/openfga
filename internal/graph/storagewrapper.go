cd3468c3abf6501964f82e11e8348881509b99e6
José Padilla <jose.padilla@okta.com>
2024-09-19 17:47:15 -0400

    improvements and observability

--- internal/graph/storagewrapper.go
+++ internal/graph/storagewrapper.go
@@ -5,18 +5,48 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
 	"github.com/cespare/xxhash/v2"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 
+	"github.com/openfga/openfga/internal/build"
 	"github.com/openfga/openfga/internal/keys"
 	"github.com/openfga/openfga/pkg/storage"
 	"github.com/openfga/openfga/pkg/tuple"
 )
 
-var _ storage.OpenFGADatastore = (*cachedDatastore)(nil)
+var (
+	_ storage.OpenFGADatastore = (*cachedDatastore)(nil)
+
+	tuplesCacheTotalCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: build.ProjectName,
+		Name:      "tuples_cache_total_count",
+		Help:      "The total number of created cached iterator instances.",
+	})
+
+	tuplesCacheHitCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Namespace: build.ProjectName,
+		Name:      "tuples_cache_hit_count",
+		Help:      "The total number of cache hits from cached iterator instances.",
+	})
+
+	tuplesCacheSizeHistogram = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace:                       build.ProjectName,
+		Name:                            "tuples_cache_size",
+		Help:                            "The number of tuples cached.",
+		Buckets:                         []float64{1, 10, 100, 1000, 5000, 10000},
+		NativeHistogramBucketFactor:     1.1,
+		NativeHistogramMaxBucketNumber:  100,
+		NativeHistogramMinResetDuration: time.Hour,
+	})
+)
 
 type cachedDatastore struct {
 	storage.OpenFGADatastore
@@ -37,50 +67,87 @@ func NewCachedDatastore(inner storage.OpenFGADatastore, cache storage.InMemoryCa
 }
 
 func (c *cachedDatastore) ReadUsersetTuples(ctx context.Context, store string, filter storage.ReadUsersetTuplesFilter, options storage.ReadUsersetTuplesOptions) (storage.TupleIterator, error) {
-	iter, err := c.OpenFGADatastore.ReadUsersetTuples(ctx, store, filter, options)
-	if err != nil {
-		return nil, err
+	ctx, span := tracer.Start(ctx, "ReadStartingWithUser")
+	defer span.End()
+
+	iter := func(ctx context.Context) (storage.TupleIterator, error) {
+		return c.OpenFGADatastore.ReadUsersetTuples(ctx, store, filter, options)
 	}
 
-	cacheKey := fmt.Sprintf("%s/%s#%s", store, filter.Object, filter.Relation)
-	// todo(jpadilla): stable sort filter.AllowedUserTypeRestrictions
+	var b strings.Builder
+	b.WriteString(fmt.Sprintf("%s/%s#%s", store, filter.Object, filter.Relation))
+
+	var rb strings.Builder
+	var wb strings.Builder
+
 	for _, userset := range filter.AllowedUserTypeRestrictions {
 		if _, ok := userset.GetRelationOrWildcard().(*openfgav1.RelationReference_Relation); ok {
-			cacheKey += "/" + userset.GetType() + ":%#" + userset.GetRelation()
+			rb.WriteString(fmt.Sprintf("/%s:%%#%s", userset.GetType(), userset.GetRelation()))
 		}
 		if _, ok := userset.GetRelationOrWildcard().(*openfgav1.RelationReference_Wildcard); ok {
-			cacheKey += "/" + userset.GetType() + ":*"
+			wb.WriteString(fmt.Sprintf("/%s:*", userset.GetType()))
 		}
 	}
 
-	return c.newCachedIterator(iter, cacheKey), nil
+	if rb.Len() > 0 {
+		b.WriteString(rb.String())
+	}
+
+	if wb.Len() > 0 {
+		b.WriteString(wb.String())
+	}
+
+	return c.newCachedIterator(ctx, iter, b.String())
 }
 
 func (c *cachedDatastore) Read(ctx context.Context, store string, tupleKey *openfgav1.TupleKey, options storage.ReadOptions) (storage.TupleIterator, error) {
-	iter, err := c.OpenFGADatastore.Read(ctx, store, tupleKey, options)
-	if err != nil {
-		return nil, err
+	ctx, span := tracer.Start(ctx, "Read")
+	defer span.End()
+
+	iter := func(ctx context.Context) (storage.TupleIterator, error) {
+		return c.OpenFGADatastore.Read(ctx, store, tupleKey, options)
 	}
-	cacheKey := fmt.Sprintf("%s/%s", store, tuple.TupleKeyToString(tupleKey))
-	return c.newCachedIterator(iter, cacheKey), nil
+
+	var b strings.Builder
+	b.WriteString(fmt.Sprintf("%s/%s", store, tuple.TupleKeyToString(tupleKey)))
+	return c.newCachedIterator(ctx, iter, b.String())
 }
 
-func (c *cachedDatastore) newCachedIterator(iter storage.TupleIterator, key string) storage.TupleIterator {
+type iterFunc func(ctx context.Context) (storage.TupleIterator, error)
+
+func (c *cachedDatastore) newCachedIterator(ctx context.Context, dsIterFunc iterFunc, key string) (storage.TupleIterator, error) {
+	span := trace.SpanFromContext(ctx)
+
 	cacheKey := cacheKeyFor(key)
-	cachedResp := c.cache.Get(cacheKey)
-	isCached := cachedResp != nil && !cachedResp.Expired && cachedResp.Value != nil
-	if isCached {
-		return storage.NewStaticTupleIterator(cachedResp.Value.([]*openfgav1.Tuple))
+	if cacheKey == "" {
+		return dsIterFunc(ctx)
 	}
 
-	return &cachedIterator{
-		iter:         iter,
-		tks:          make([]*openfgav1.Tuple, 0, c.maxCacheSize),
+	cachedResp := c.cache.Get(cacheKey)
+	cachedIter := &cachedIterator{
+		iter:         nil,
+		tuples:       make([]*openfgav1.Tuple, c.maxCacheSize),
 		cacheKey:     cacheKey,
 		cache:        c.cache,
 		maxCacheSize: c.maxCacheSize,
+		isCached:     cachedResp != nil && !cachedResp.Expired && cachedResp.Value != nil,
 		ttl:          c.ttl,
 	}
+
+	if cachedIter.isCached {
+		cachedIter.iter = storage.NewStaticTupleIterator(cachedResp.Value.([]*openfgav1.Tuple))
+	} else {
+		dsIter, err := dsIterFunc(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		cachedIter.iter = dsIter
+	}
+
+	span.SetAttributes(attribute.Bool("is_iter_cached", cachedIter.isCached))
+
+	return cachedIter, nil
 }
 
 // Close closes the datastore and cleans up any residual resources.
@@ -90,10 +157,12 @@ func (c *cachedDatastore) Close() {
 }
 
 type cachedIterator struct {
+	pos          int
 	iter         storage.TupleIterator
-	tks          []*openfgav1.Tuple
+	tuples       []*openfgav1.Tuple
 	cacheKey     string
 	cache        storage.InMemoryCache[any]
+	isCached     bool
 	ttl          time.Duration
 	maxCacheSize int64
 	closeOnce    sync.Once
@@ -103,14 +172,12 @@ type cachedIterator struct {
 func (c *cachedIterator) Next(ctx context.Context) (*openfgav1.Tuple, error) {
 	tuple, err := c.iter.Next(ctx)
 	if err != nil {
-		if errors.Is(err, storage.ErrIteratorDone) {
-			c.flush()
-		}
 		return nil, err
 	}
 
-	if len(c.tks) < int(c.maxCacheSize) {
-		c.tks = append(c.tks, tuple)
+	if c.pos < int(c.maxCacheSize) {
+		c.tuples[c.pos] = tuple
+		c.pos++
 	}
 
 	return tuple, nil
@@ -118,24 +185,29 @@ func (c *cachedIterator) Next(ctx context.Context) (*openfgav1.Tuple, error) {
 
 // Stop see [Iterator.Stop].
 func (c *cachedIterator) Stop() {
+	// TODO: Determine if we need/should drain the iterator and cache in the background.
+	// There are concerns with synchronization and leaking goroutines
 	c.closeOnce.Do(func() {
-		// todo(jpadilla): figure out how to handle when at capacity
-		if len(c.tks) == int(c.maxCacheSize) {
-			c.iter.Stop()
+		defer c.iter.Stop()
+
+		tuplesCacheTotalCounter.Inc()
+		if c.isCached {
+			tuplesCacheHitCounter.Inc()
+		}
+
+		if c.pos >= int(c.maxCacheSize) {
 			return
 		}
 
-		go func() {
-			for {
-				_, err := c.Next(context.Background())
-				if err != nil {
+		for {
+			_, err := c.Next(context.Background())
+			if err != nil {
+				if errors.Is(err, storage.ErrIteratorDone) {
 					c.flush()
-					break
 				}
+				break
 			}
-
-			c.iter.Stop()
-		}()
+		}
 	})
 }
 
@@ -145,10 +217,14 @@ func (c *cachedIterator) Head(ctx context.Context) (*openfgav1.Tuple, error) {
 }
 
 func (c *cachedIterator) flush() {
-	if len(c.tks) > 0 {
-		c.cache.Set(c.cacheKey, c.tks, c.ttl)
-		c.tks = nil
+	if !c.isCached && c.pos > 0 && c.pos < int(c.maxCacheSize) {
+		tuples := c.tuples[:c.pos]
+		c.cache.Set(c.cacheKey, tuples, c.ttl)
+		tuplesCacheSizeHistogram.Observe(float64(len(tuples)))
 	}
+
+	c.tuples = make([]*openfgav1.Tuple, c.maxCacheSize)
+	c.pos = 0
 }
 
 func cacheKeyFor(s string) string {
--- internal/graph/storagewrapper_test.go
+++ internal/graph/storagewrapper_test.go
@@ -2,18 +2,22 @@ package graph
 
 import (
 	"context"
-	"sync"
+	"errors"
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/oklog/ulid/v2"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
 	"go.uber.org/mock/gomock"
+	"google.golang.org/protobuf/testing/protocmp"
+	"google.golang.org/protobuf/types/known/timestamppb"
 
 	"github.com/openfga/openfga/internal/mocks"
 	"github.com/openfga/openfga/pkg/storage"
+	"github.com/openfga/openfga/pkg/testutils"
 	"github.com/openfga/openfga/pkg/tuple"
 	"github.com/openfga/openfga/pkg/typesystem"
 )
@@ -24,7 +28,7 @@ func TestReadUsersetTuples(t *testing.T) {
 		goleak.VerifyNone(t)
 	})
 	mockController := gomock.NewController(t)
-	mockController.Finish()
+	defer mockController.Finish()
 
 	mockCache := mocks.NewMockInMemoryCache[any](mockController)
 	mockDatastore := mocks.NewMockOpenFGADatastore(mockController)
@@ -58,34 +62,68 @@ func TestReadUsersetTuples(t *testing.T) {
 		},
 	}
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	gomock.InOrder(
-		mockDatastore.EXPECT().
-			Write(ctx, storeID, nil, tks).Return(nil),
-		mockDatastore.EXPECT().
-			ReadUsersetTuples(ctx, storeID, filter, options).
-			Return(storage.NewStaticTupleIterator(tuples), nil),
-		mockCache.EXPECT().Get(gomock.Any()),
-		mockCache.EXPECT().Set(gomock.Any(), tuples, ttl).
-			Do(func(arg0, arg1, arg2 interface{}) {
-				defer wg.Done()
-			}),
-	)
-
+	mockDatastore.EXPECT().
+		Write(gomock.Any(), storeID, nil, tks).Return(nil)
 	err := ds.Write(ctx, storeID, nil, tks)
 	require.NoError(t, err)
 
-	iter, err := ds.ReadUsersetTuples(ctx, storeID, filter, options)
-	require.NoError(t, err)
+	t.Run("cache miss", func(t *testing.T) {
+		gomock.InOrder(
+			mockCache.EXPECT().Get(gomock.Any()),
+			mockDatastore.EXPECT().
+				ReadUsersetTuples(gomock.Any(), storeID, filter, options).
+				Return(storage.NewStaticTupleIterator(tuples), nil),
+			mockCache.EXPECT().Set(gomock.Any(), tuples, ttl),
+		)
 
-	tp, err := iter.Next(ctx)
-	require.NoError(t, err)
-	require.Equal(t, tuples[0], tp)
+		iter, err := ds.ReadUsersetTuples(ctx, storeID, filter, options)
+		require.NoError(t, err)
+		defer iter.Stop()
 
-	iter.Stop()
-	wg.Wait()
+		var actual []*openfgav1.Tuple
+
+		for {
+			tuple, err := iter.Next(ctx)
+			if err != nil {
+				if errors.Is(err, storage.ErrIteratorDone) {
+					break
+				}
+				require.Fail(t, "no error was expected")
+				break
+			}
+
+			actual = append(actual, tuple)
+		}
+
+		require.Equal(t, tuples, actual)
+	})
+
+	t.Run("cache hit", func(t *testing.T) {
+		gomock.InOrder(
+			mockCache.EXPECT().Get(gomock.Any()).Return(&storage.CachedResult[any]{Value: tuples}),
+		)
+
+		iter, err := ds.ReadUsersetTuples(ctx, storeID, filter, options)
+		require.NoError(t, err)
+		defer iter.Stop()
+
+		var actual []*openfgav1.Tuple
+
+		for {
+			tuple, err := iter.Next(ctx)
+			if err != nil {
+				if errors.Is(err, storage.ErrIteratorDone) {
+					break
+				}
+				require.Fail(t, "no error was expected")
+				break
+			}
+
+			actual = append(actual, tuple)
+		}
+
+		require.Equal(t, tuples, actual)
+	})
 }
 
 func TestRead(t *testing.T) {
@@ -93,8 +131,9 @@ func TestRead(t *testing.T) {
 	t.Cleanup(func() {
 		goleak.VerifyNone(t)
 	})
+
 	mockController := gomock.NewController(t)
-	mockController.Finish()
+	defer mockController.Finish()
 
 	mockCache := mocks.NewMockInMemoryCache[any](mockController)
 	mockDatastore := mocks.NewMockOpenFGADatastore(mockController)
@@ -119,32 +158,212 @@ func TestRead(t *testing.T) {
 
 	tk := tuple.NewTupleKey("license:1", "owner", "")
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-
-	gomock.InOrder(
-		mockDatastore.EXPECT().
-			Write(ctx, storeID, nil, tks).Return(nil),
-		mockDatastore.EXPECT().
-			Read(ctx, storeID, tk, storage.ReadOptions{}).
-			Return(storage.NewStaticTupleIterator(tuples), nil),
-		mockCache.EXPECT().Get(gomock.Any()),
-		mockCache.EXPECT().Set(gomock.Any(), tuples, ttl).
-			Do(func(arg0, arg1, arg2 interface{}) {
-				defer wg.Done()
-			}),
-	)
+	mockDatastore.EXPECT().
+		Write(gomock.Any(), storeID, nil, tks).Return(nil)
 
 	err := ds.Write(ctx, storeID, nil, tks)
 	require.NoError(t, err)
 
-	iter, err := ds.Read(ctx, storeID, tk, storage.ReadOptions{})
-	require.NoError(t, err)
+	t.Run("cache miss", func(t *testing.T) {
+		gomock.InOrder(
+			mockCache.EXPECT().Get(gomock.Any()),
+			mockDatastore.EXPECT().
+				Read(gomock.Any(), storeID, tk, storage.ReadOptions{}).
+				Return(storage.NewStaticTupleIterator(tuples), nil),
+			mockCache.EXPECT().Set(gomock.Any(), tuples, ttl),
+		)
 
-	tp, err := iter.Next(ctx)
-	require.NoError(t, err)
-	require.Equal(t, tuples[0], tp)
+		iter, err := ds.Read(ctx, storeID, tk, storage.ReadOptions{})
+		require.NoError(t, err)
+		defer iter.Stop()
 
-	iter.Stop()
-	wg.Wait()
+		var actual []*openfgav1.Tuple
+
+		for {
+			tuple, err := iter.Next(ctx)
+			if err != nil {
+				if errors.Is(err, storage.ErrIteratorDone) {
+					break
+				}
+				require.Fail(t, "no error was expected")
+				break
+			}
+
+			actual = append(actual, tuple)
+		}
+
+		require.Equal(t, tuples, actual)
+	})
+
+	t.Run("cache hit", func(t *testing.T) {
+		gomock.InOrder(
+			mockCache.EXPECT().Get(gomock.Any()).
+				Return(&storage.CachedResult[any]{Value: tuples}),
+		)
+
+		iter, err := ds.Read(ctx, storeID, tk, storage.ReadOptions{})
+		require.NoError(t, err)
+		defer iter.Stop()
+
+		var actual []*openfgav1.Tuple
+
+		for {
+			tuple, err := iter.Next(ctx)
+			if err != nil {
+				if errors.Is(err, storage.ErrIteratorDone) {
+					break
+				}
+				require.Fail(t, "no error was expected")
+				break
+			}
+
+			actual = append(actual, tuple)
+		}
+
+		require.Equal(t, tuples, actual)
+	})
+}
+
+func TestCachedIterator(t *testing.T) {
+	ctx := context.Background()
+
+	tuples := []*openfgav1.Tuple{
+		{
+			Key:       tuple.NewTupleKey("document:doc1", "viewer", "bill"),
+			Timestamp: timestamppb.New(time.Now()),
+		},
+		{
+			Key:       tuple.NewTupleKey("document:doc2", "editor", "bob"),
+			Timestamp: timestamppb.New(time.Now()),
+		},
+	}
+
+	cmpOpts := []cmp.Option{
+		testutils.TupleKeyCmpTransformer,
+		protocmp.Transform(),
+	}
+
+	t.Run("next", func(t *testing.T) {
+		maxCacheSize := 10
+		cacheKey := "cache-key"
+		ttl := 5 * time.Hour
+		cache := storage.NewInMemoryLRUCache([]storage.InMemoryLRUCacheOpt[any]{
+			storage.WithMaxCacheSize[any](int64(100)),
+		}...)
+
+		iter := &cachedIterator{
+			iter:         storage.NewStaticTupleIterator(tuples),
+			tuples:       make([]*openfgav1.Tuple, maxCacheSize),
+			cacheKey:     cacheKey,
+			cache:        cache,
+			maxCacheSize: int64(maxCacheSize),
+			ttl:          ttl,
+		}
+
+		var actual []*openfgav1.Tuple
+
+		for {
+			tk, err := iter.Next(ctx)
+			if err != nil {
+				if errors.Is(err, storage.ErrIteratorDone) {
+					break
+				}
+				require.Fail(t, "no error was expected")
+				break
+			}
+
+			actual = append(actual, tk)
+		}
+
+		require.Equal(t, tuples, actual)
+
+		cachedResults := cache.Get(cacheKey)
+		require.Nil(t, cachedResults)
+	})
+
+	t.Run("next capacity", func(t *testing.T) {
+		maxCacheSize := 2
+		cacheKey := "cache-key"
+		ttl := 5 * time.Hour
+		cache := storage.NewInMemoryLRUCache([]storage.InMemoryLRUCacheOpt[any]{
+			storage.WithMaxCacheSize[any](int64(100)),
+		}...)
+
+		iter := &cachedIterator{
+			iter:         storage.NewStaticTupleIterator(tuples),
+			tuples:       make([]*openfgav1.Tuple, maxCacheSize),
+			cacheKey:     cacheKey,
+			cache:        cache,
+			maxCacheSize: int64(maxCacheSize),
+			ttl:          ttl,
+		}
+
+		var actual []*openfgav1.Tuple
+
+		for {
+			tk, err := iter.Next(ctx)
+			if err != nil {
+				if errors.Is(err, storage.ErrIteratorDone) {
+					break
+				}
+				require.Fail(t, "no error was expected")
+				break
+			}
+
+			actual = append(actual, tk)
+		}
+
+		require.Equal(t, tuples, actual)
+
+		cachedResults := cache.Get(cacheKey)
+		require.Nil(t, cachedResults)
+	})
+
+	t.Run("stop", func(t *testing.T) {
+		maxCacheSize := 10
+		cacheKey := "cache-key"
+		ttl := 5 * time.Hour
+		cache := storage.NewInMemoryLRUCache([]storage.InMemoryLRUCacheOpt[any]{
+			storage.WithMaxCacheSize[any](int64(100)),
+		}...)
+
+		iter := &cachedIterator{
+			iter:         storage.NewStaticTupleIterator(tuples),
+			tuples:       make([]*openfgav1.Tuple, maxCacheSize),
+			cacheKey:     cacheKey,
+			cache:        cache,
+			maxCacheSize: int64(maxCacheSize),
+			ttl:          ttl,
+		}
+
+		iter.Stop()
+		cachedResults := cache.Get(cacheKey)
+		require.NotNil(t, cachedResults)
+
+		if diff := cmp.Diff(tuples, cachedResults.Value.([]*openfgav1.Tuple), cmpOpts...); diff != "" {
+			t.Fatalf("mismatch (-want +got):\n%s", diff)
+		}
+	})
+
+	t.Run("stop capacity", func(t *testing.T) {
+		maxCacheSize := 2
+		cacheKey := "cache-key"
+		ttl := 5 * time.Hour
+		cache := storage.NewInMemoryLRUCache([]storage.InMemoryLRUCacheOpt[any]{
+			storage.WithMaxCacheSize[any](int64(100)),
+		}...)
+
+		iter := &cachedIterator{
+			iter:         storage.NewStaticTupleIterator(tuples),
+			tuples:       make([]*openfgav1.Tuple, maxCacheSize),
+			cacheKey:     cacheKey,
+			cache:        cache,
+			maxCacheSize: int64(maxCacheSize),
+			ttl:          ttl,
+		}
+
+		iter.Stop()
+		cachedResults := cache.Get(cacheKey)
+		require.Nil(t, cachedResults)
+	})
 }
