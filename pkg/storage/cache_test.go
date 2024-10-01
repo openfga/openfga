80410489132c5608d829984359d1bcd4f3bac8e6
José Padilla <jose.padilla@okta.com>
2024-10-01 11:59:25 -0400

    fix: close cache once

---
+++ pkg/storage/cache_test.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
+)
+
+func TestInMemoryCache(t *testing.T) {
+	cache := NewInMemoryLRUCache[string]()
+	defer cache.Stop()
+
+	t.Run("set_and_get", func(t *testing.T) {
+		t.Cleanup(func() {
+			goleak.VerifyNone(t)
+		})
+		defer cache.Stop()
+		cache.Set("key", "value", 1*time.Second)
+		result := cache.Get("key")
+		require.Equal(t, "value", result.Value)
+		require.False(t, result.Expired)
+	})
+
+	t.Run("stop_multiple_times", func(t *testing.T) {
+		t.Cleanup(func() {
+			goleak.VerifyNone(t)
+		})
+
+		cache.Stop()
+		cache.Stop()
+	})
+}
