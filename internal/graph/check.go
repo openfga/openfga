cb48a64dcb36c7020f6210cf60a64e81312d83f6
Saket Nigam <113360927+sOnU1002@users.noreply.github.com>
2024-10-04 18:34:19 +0530

    Update check.go

--- internal/graph/check.go
+++ internal/graph/check.go
@@ -62,13 +62,7 @@ func WithUsersetBatchSize(usersetBatchSize uint32) LocalCheckerOption {
 	}
 }
 
-// WithMaxConcurrentReads see server.WithMaxConcurrentReadsForCheck.
-// TODO remove - unused.
-func WithMaxConcurrentReads(limit uint32) LocalCheckerOption {
-	return func(d *LocalChecker) {
-		d.maxConcurrentReads = limit
-	}
-}
+
 
 func WithLocalCheckerLogger(logger logger.Logger) LocalCheckerOption {
 	return func(d *LocalChecker) {
