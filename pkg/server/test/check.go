a202045086c580606dcc90635ac08923242be2bf
justin <justin.cohen@okta.com>
2024-10-11 21:39:25 -0400

    WIP add tuple generator and some other bits

--- pkg/server/test/check.go
+++ pkg/server/test/check.go
@@ -2,13 +2,15 @@ package test
 
 import (
 	"context"
+	"fmt"
 	"github.com/oklog/ulid/v2"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+	"github.com/openfga/openfga/internal/graph"
+	"github.com/openfga/openfga/pkg/server/commands"
 	"github.com/openfga/openfga/pkg/storage"
 	"github.com/openfga/openfga/pkg/testutils"
 	"github.com/openfga/openfga/pkg/typesystem"
 	"github.com/stretchr/testify/require"
-	"log"
 	"testing"
 )
 
@@ -16,6 +18,7 @@ func BenchmarkCheck(b *testing.B, ds storage.OpenFGADatastore) {
 	benchmarkScenarios := map[string]struct {
 		inputModel      string
 		tupleGenerator  func() []*openfgav1.TupleKey
+		checker         graph.CheckResolver
 		inputRequest    *openfgav1.CheckRequest
 		expectedResults any // not sure what goes here yet
 	}{
@@ -35,27 +38,33 @@ func BenchmarkCheck(b *testing.B, ds storage.OpenFGADatastore) {
 					relations
 						define member: [user]`,
 			tupleGenerator: func() []*openfgav1.TupleKey {
-				return []*openfgav1.TupleKey{} // TODO
+				var tuples []*openfgav1.TupleKey
+				for i := 0; i < 1000; i++ { // add user:anne to many teams
+					tuples = append(tuples, &openfgav1.TupleKey{
+						User:     "user:anne",
+						Relation: "member",
+						Object:   fmt.Sprintf("team:%d", i),
+					})
+				}
+				return tuples
 			},
+			checker:         graph.NewLocalChecker(),
 			inputRequest:    &openfgav1.CheckRequest{},
 			expectedResults: "hello",
 		},
 	}
 
-	//for name, bench := range benchmarkScenarios {
-	for _, bench := range benchmarkScenarios {
-		//ctx := context.Background()
+	for name, bm := range benchmarkScenarios {
+		ctx := context.Background()
 		storeID := ulid.Make().String()
 		// write model
-		model := testutils.MustTransformDSLToProtoWithID(bench.inputModel)
-		//typeSystem, err := typesystem.NewAndValidate(context.Background(), model)
-		_, err := typesystem.NewAndValidate(context.Background(), model)
+		model := testutils.MustTransformDSLToProtoWithID(bm.inputModel)
+		typeSystem, err := typesystem.NewAndValidate(context.Background(), model)
 		require.NoError(b, err)
 		err = ds.WriteAuthorizationModel(context.Background(), storeID, model)
 
-		log.Println("justinnnnnnnnnnnnnnn")
-		// create tuples
-		tuples := bench.tupleGenerator()
+		// create and write tuples
+		tuples := bm.tupleGenerator()
 		for i := 0; i < len(tuples); {
 			var tuplesToWrite []*openfgav1.TupleKey
 			for j := 0; j < ds.MaxTuplesPerWrite(); j++ {
@@ -68,6 +77,17 @@ func BenchmarkCheck(b *testing.B, ds storage.OpenFGADatastore) {
 			err := ds.Write(context.Background(), storeID, nil, tuplesToWrite)
 			require.NoError(b, err)
 		}
+		bm.inputRequest.StoreId = storeID
+		ctx = typesystem.ContextWithTypesystem(ctx, typeSystem)
+
+		// then give one team access to repo
+		// then give anne direct access to repo
+
 		// run check
+		b.Run(name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				checkQuery := commands.NewCheckCommand(ds, bm.checker, typeSystem)
+			}
+		})
 	}
 }
