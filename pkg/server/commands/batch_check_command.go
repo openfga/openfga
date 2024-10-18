3796100b818af761f35f9a6e8a7ae9e177d7977c
justin <justin.cohen@okta.com>
2024-10-18 10:31:12 -0400

    WIP on BatchCheckCommand

--- pkg/server/commands/batch_check_command.go
+++ pkg/server/commands/batch_check_command.go
@@ -2,20 +2,12 @@ package commands
 
 import (
 	"context"
-	"errors"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-
-	"github.com/openfga/openfga/internal/condition"
+	"github.com/openfga/openfga/internal/cachecontroller"
 	"github.com/openfga/openfga/internal/graph"
-	"github.com/openfga/openfga/internal/validation"
-	"github.com/openfga/openfga/pkg/middleware/validator"
-	serverErrors "github.com/openfga/openfga/pkg/server/errors"
 	"github.com/openfga/openfga/pkg/storage"
-	"github.com/openfga/openfga/pkg/storage/storagewrappers"
-	"github.com/openfga/openfga/pkg/tuple"
 	"github.com/openfga/openfga/pkg/typesystem"
+	"time"
 
 	"github.com/openfga/openfga/pkg/logger"
 )
@@ -26,11 +18,11 @@ import (
 //)
 
 type BatchCheckQuery struct {
-	logger        logger.Logger
-	checkResolver graph.CheckResolver
-	typesys       *typesystem.TypeSystem
-	datastore     storage.RelationshipTupleReader
-	//cacheController cachecontroller.CacheController
+	logger          logger.Logger
+	checkResolver   graph.CheckResolver
+	typesys         *typesystem.TypeSystem
+	datastore       storage.RelationshipTupleReader
+	cacheController cachecontroller.CacheController
 
 	//resolveNodeLimit    uint32
 	maxConcurrentChecks uint32
@@ -45,16 +37,19 @@ func WithBatchCheckCommandMaxConcurrentChecks(m uint32) BatchCheckQueryOption {
 	}
 }
 
+func WithCheckCommandCacheController(cc cachecontroller.CacheController) BatchCheckQueryOption {
+	return func(bq *BatchCheckQuery) {
+		bq.cacheController = cc
+	}
+
+}
+
 func NewBatchCheckCommand(datastore storage.RelationshipTupleReader, checkResolver graph.CheckResolver, typesys *typesystem.TypeSystem, opts ...BatchCheckQueryOption) *BatchCheckQuery {
 	cmd := &BatchCheckQuery{
-		logger:              logger.NewNoopLogger(),
-		datastore:           datastore,
-		checkResolver:       checkResolver,
-		typesys:             typesys,
-		maxConcurrentChecks: 50, // TODO put this where it belongs
-		//cacheController:    cachecontroller.NewNoopCacheController(),
-		//resolveNodeLimit:   defaultResolveNodeLimit,
-		//maxConcurrentReads: defaultMaxConcurrentReadsForCheck,
+		logger:        logger.NewNoopLogger(),
+		datastore:     datastore,
+		checkResolver: checkResolver,
+		typesys:       typesys,
 	}
 
 	for _, opt := range opts {
@@ -64,17 +59,17 @@ func NewBatchCheckCommand(datastore storage.RelationshipTupleReader, checkResolv
 }
 
 // Execute here needs new return types as well.
-func (c *BatchCheckQuery) Execute(ctx context.Context, req *openfgav1.BatchCheckRequest) (*graph.ResolveCheckResponse, *graph.ResolveCheckRequestMetadata, error) {
+func (bq *BatchCheckQuery) Execute(ctx context.Context, req *openfgav1.BatchCheckRequest) (*graph.ResolveCheckResponse, *graph.ResolveCheckRequestMetadata, error) {
 	err := req.Validate()
 	if err != nil {
 		return nil, nil, err
 	}
 
-	//cacheInvalidationTime := time.Time{}
+	cacheInvalidationTime := time.Time{}
 
-	//if req.GetConsistency() != openfgav1.ConsistencyPreference_HIGHER_CONSISTENCY {
-	//	cacheInvalidationTime = c.cacheController.DetermineInvalidation(ctx, req.GetStoreId())
-	//}
+	if req.GetConsistency() != openfgav1.ConsistencyPreference_HIGHER_CONSISTENCY {
+		cacheInvalidationTime = bq.cacheController.DetermineInvalidation(ctx, req.GetStoreId())
+	}
 
 	// for validation:
 	// validate the overall batch check request
@@ -87,87 +82,13 @@ func (c *BatchCheckQuery) Execute(ctx context.Context, req *openfgav1.BatchCheck
 	//checkCommand := NewCheckCommand(
 	//	)
 	resp, checkRequestMetadata, err := NewCheckCommand(
-		checkDatastore,                   // has to pass through
-		checkResolver,                    // has to pass through
-		typesys,                          // also
+		bq.datastore,
+		bq.checkResolver,
+		bq.typesys,
 		WithCheckCommandLogger(s.logger), // logger?
 		WithCheckCommandMaxConcurrentReads(s.maxConcurrentReadsForCheck), // these?
 		WithCheckCommandResolveNodeLimit(s.resolveNodeLimit),             // these?
 		WithCacheController(s.cacheController),                           // these?
 	).Execute(ctx, req)
 
-	// This will pass through direct to CheckCommand
-	resolveCheckRequest := graph.ResolveCheckRequest{
-		StoreID:              req.GetStoreId(),
-		AuthorizationModelID: c.typesys.GetAuthorizationModelID(), // the resolved model ID
-		TupleKey:             tuple.ConvertCheckRequestTupleKeyToTupleKey(req.GetTupleKey()),
-		ContextualTuples:     req.GetContextualTuples().GetTupleKeys(),
-		Context:              req.GetContext(),
-		VisitedPaths:         make(map[string]struct{}),
-		RequestMetadata:      graph.NewCheckRequestMetadata(c.resolveNodeLimit),
-		Consistency:          req.GetConsistency(),
-		// avoid having to read from cache consistently by propagating it
-		LastCacheInvalidationTime: cacheInvalidationTime,
-	}
-
-	ctx = buildCheckContext(ctx, c.typesys, c.datastore, c.maxConcurrentReads, resolveCheckRequest.GetContextualTuples())
-
-	resp, err := c.checkResolver.ResolveCheck(ctx, &resolveCheckRequest)
-	if err != nil {
-		return nil, nil, translateError(resolveCheckRequest.GetRequestMetadata(), err)
-	}
-	return resp, resolveCheckRequest.GetRequestMetadata(), nil
-}
-
-func validateCheckRequest(ctx context.Context, req *openfgav1.CheckRequest, typesys *typesystem.TypeSystem) error {
-	if !validator.RequestIsValidatedFromContext(ctx) {
-		if err := req.Validate(); err != nil {
-			return status.Error(codes.InvalidArgument, err.Error())
-		}
-	}
-
-	// The input tuple Key should be validated loosely.
-	if err := validation.ValidateUserObjectRelation(typesys, tuple.ConvertCheckRequestTupleKeyToTupleKey(req.GetTupleKey())); err != nil {
-		return serverErrors.ValidationError(err)
-	}
-
-	// But contextual tuples need to be validated more strictly, the same as an input to a Write Tuple request.
-	for _, ctxTuple := range req.GetContextualTuples().GetTupleKeys() {
-		if err := validation.ValidateTupleForWrite(typesys, ctxTuple); err != nil {
-			return serverErrors.HandleTupleValidateError(err)
-		}
-	}
-	return nil
-}
-
-func buildCheckContext(ctx context.Context, typesys *typesystem.TypeSystem, datastore storage.RelationshipTupleReader, maxconcurrentreads uint32, contextualTuples []*openfgav1.TupleKey) context.Context {
-	ctx = typesystem.ContextWithTypesystem(ctx, typesys)
-
-	// TODO the order is wrong, see https://github.com/openfga/openfga/issues/1394
-	ctx = storage.ContextWithRelationshipTupleReader(ctx,
-		storagewrappers.NewBoundedConcurrencyTupleReader(
-			storagewrappers.NewCombinedTupleReader(
-				datastore,
-				contextualTuples,
-			),
-			maxconcurrentreads,
-		),
-	)
-	return ctx
-}
-
-func translateError(reqMetadata *graph.ResolveCheckRequestMetadata, err error) error {
-	if errors.Is(err, graph.ErrResolutionDepthExceeded) {
-		return serverErrors.AuthorizationModelResolutionTooComplex
-	}
-
-	if errors.Is(err, condition.ErrEvaluationFailed) {
-		return serverErrors.ValidationError(err)
-	}
-
-	if errors.Is(err, context.DeadlineExceeded) && reqMetadata.WasThrottled.Load() {
-		return serverErrors.ThrottledTimeout
-	}
-
-	return serverErrors.HandleError("", err)
 }
