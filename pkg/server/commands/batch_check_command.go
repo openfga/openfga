856a2b103702e46b9ee182d20ea3ac062b12c9b9
justin <justin.cohen@okta.com>
2024-10-23 10:09:49 -0400

    clean up comments and TODOs

--- pkg/server/commands/batch_check_command.go
+++ pkg/server/commands/batch_check_command.go
@@ -7,7 +7,6 @@ import (
 
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 	"github.com/openfga/openfga/internal/cachecontroller"
-	"github.com/openfga/openfga/internal/concurrency"
 	"github.com/openfga/openfga/internal/graph"
 	"github.com/openfga/openfga/pkg/logger"
 	"github.com/openfga/openfga/pkg/storage"
@@ -35,7 +34,6 @@ type BatchCheckCommandParams struct {
 }
 
 type BatchCheckOutcome struct {
-	//CorrelationID string
 	CheckResponse *graph.ResolveCheckResponse
 	Duration      time.Duration
 	Err           error
@@ -43,27 +41,9 @@ type BatchCheckOutcome struct {
 
 type BatchCheckQueryOption func(*BatchCheckQuery)
 
-func WithBatchCheckCommandMaxConcurrentChecks(m uint32) BatchCheckQueryOption {
-	return func(c *BatchCheckQuery) {
-		c.maxConcurrentChecks = m
-	}
-}
-
 func WithBatchCheckCommandCacheController(cc cachecontroller.CacheController) BatchCheckQueryOption {
 	return func(bq *BatchCheckQuery) {
 		bq.cacheController = cc
-	}
-}
-
-func WithBatchCheckResolveNodeLimit(resolveLimit uint32) BatchCheckQueryOption {
-	return func(bq *BatchCheckQuery) {
-		bq.resolveNodeLimit = resolveLimit
-	}
-}
-
-func WithBatchCheckMaxConcurrentReadsPerCheck(maxReads uint32) BatchCheckQueryOption {
-	return func(bq *BatchCheckQuery) {
-		bq.maxConcurrentReadsPerCheck = maxReads
 	}
 }
 
@@ -88,7 +68,6 @@ func NewBatchCheckCommand(datastore storage.RelationshipTupleReader, checkResolv
 	return cmd
 }
 
-// Execute here needs new return types as well.
 func (bq *BatchCheckQuery) Execute(ctx context.Context, params *BatchCheckCommandParams) (map[string]*BatchCheckOutcome, error) {
 	// This check query will be run against every check in the batch
 	checkQuery := NewCheckCommand(
@@ -97,8 +76,6 @@ func (bq *BatchCheckQuery) Execute(ctx context.Context, params *BatchCheckComman
 		bq.typesys,
 		WithCheckCommandLogger(bq.logger),
 		WithCacheController(bq.cacheController),
-		//needs a default or it'll break
-		//WithCheckCommandMaxConcurrentReads(bq.maxConcurrentReadsPerCheck),
 	)
 
 	// the keys to this map are the correlation_id associated with each check
@@ -108,10 +85,13 @@ func (bq *BatchCheckQuery) Execute(ctx context.Context, params *BatchCheckComman
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	// Kick off all checks concurrently allowing contextPool to manage limits
-	pool := concurrency.NewPool(ctx, int(bq.maxConcurrentChecks))
+	// Kick off all checks concurrently, there is no limit here
+	// Concurrency limiting is left to check's ResolveNodeLimit and MaxConcurrentReads
+	var wg sync.WaitGroup
 	for _, check := range params.Checks {
-		pool.Go(func(ctx context.Context) error {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
 			checkParams := &CheckCommandParams{
 				StoreID:          params.StoreID,
 				TupleKey:         check.TupleKey,
@@ -131,13 +111,9 @@ func (bq *BatchCheckQuery) Execute(ctx context.Context, params *BatchCheckComman
 				Err:           err,
 			}
 			lock.Unlock()
-
-			return nil
-		})
+		}()
 	}
+	wg.Wait()
 
-	_ = pool.Wait()
-
-	// TODO will there ever be an actual error condition in this command?
 	return resultMap, nil
 }
