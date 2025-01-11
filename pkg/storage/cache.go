2ec3890901016d7772ca3e13bd9226832c9857cc
Joshua Jones <joshua.jones@okta.com>
2025-01-11 14:43:11 -0500

    move internal/keys/hasher symbols into pkg/storage/cache

--- pkg/storage/cache.go
+++ pkg/storage/cache.go
@@ -3,7 +3,10 @@
 package storage
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"sort"
 	"strconv"
 	"sync"
 	"time"
@@ -14,7 +17,7 @@ import (
 
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 
-	"github.com/openfga/openfga/internal/keys"
+	"github.com/openfga/openfga/pkg/tuple"
 )
 
 const (
@@ -144,6 +147,150 @@ func GetReadCacheKey(store, tuple string) string {
 	return fmt.Sprintf("%sr/%s/%s", iteratorCachePrefix, store, tuple)
 }
 
+// ErrUnexpectedStructValue is an error used to indicate that
+// an unexpected structpb.Value kind was encountered.
+var ErrUnexpectedStructValue = errors.New("unexpected structpb value encountered")
+
+// writeValue writes value v to the writer w. An error
+// is returned only when the underlying writer returns
+// an error or an unexpected value kind is encountered.
+func writeValue(w io.StringWriter, v *structpb.Value) (err error) {
+	switch val := v.GetKind().(type) {
+	case *structpb.Value_BoolValue:
+		_, err = w.WriteString(fmt.Sprintf("%v", val.BoolValue))
+	case *structpb.Value_NullValue:
+		_, err = w.WriteString("null")
+	case *structpb.Value_StringValue:
+		_, err = w.WriteString(val.StringValue)
+	case *structpb.Value_NumberValue:
+		_, err = w.WriteString(strconv.FormatFloat(val.NumberValue, 'f', -1, 64)) // -1 precision ensures we represent the 64-bit value with the maximum precision needed to represent it, see strconv#FormatFloat for more info.
+	case *structpb.Value_ListValue:
+		values := val.ListValue.GetValues()
+
+		for n, vv := range values {
+			if err = writeValue(w, vv); err != nil {
+				return
+			}
+
+			if n < len(values)-1 {
+				if _, err = w.WriteString(","); err != nil {
+					return
+				}
+			}
+		}
+	case *structpb.Value_StructValue:
+		err = writeStruct(w, val.StructValue)
+	default:
+		err = ErrUnexpectedStructValue
+	}
+	return
+}
+
+// keys accepts a map m and returns a slice of its keys.
+// When this project is updated to Go version 1.23 or greater,
+// `maps.Keys` should be preferred.
+func keys[T comparable, U any](m map[T]U) []T {
+	n := make([]T, len(m))
+	var i int
+	for k := range m {
+		n[i] = k
+	}
+	return n
+}
+
+// writeStruct writes Struct value s to writer w. When s is nil, a
+// nil error is returned. An error is returned only when the underlying
+// writer returns an error. The struct fields are written in the sorted
+// order of their names. A comma separates fields.
+func writeStruct(w io.StringWriter, s *structpb.Struct) (err error) {
+	if s == nil {
+		return
+	}
+
+	fields := s.GetFields()
+	keys := keys(fields)
+	sort.Strings(keys)
+
+	for _, key := range keys {
+		if _, err = w.WriteString(fmt.Sprintf("'%s:'", key)); err != nil {
+			return
+		}
+
+		if err = writeValue(w, fields[key]); err != nil {
+			return
+		}
+
+		if _, err = w.WriteString(","); err != nil {
+			return
+		}
+	}
+	return
+}
+
+// writeTuples writes the set of tuples to writer w in ascending sorted order.
+// The intention of this function is to write the tuples as a unique string.
+// Tuples are separated by commas, and when present, conditions are included
+// in the tuple string representation. writeTuples returns an error only when
+// the underlying writer returns an error.
+func writeTuples(w io.StringWriter, tuples ...*openfgav1.TupleKey) (err error) {
+	sortedTuples := make(tuple.TupleKeys, len(tuples))
+
+	// copy tuples slice to avoid mutating the original slice during sorting.
+	copy(sortedTuples, tuples)
+
+	// sort tulpes for a deterministic write
+	sort.Sort(sortedTuples)
+
+	// prefix to avoid overlap with previous strings written
+	_, err = w.WriteString("/")
+	if err != nil {
+		return
+	}
+
+	for n, tupleKey := range sortedTuples {
+		_, err = w.WriteString(tupleKey.GetObject() + "#" + tupleKey.GetRelation())
+		if err != nil {
+			return
+		}
+
+		cond := tupleKey.GetCondition()
+		if cond != nil {
+			// " with " is separated by spaces as those are invalid in relation names
+			// and we need to ensure this cache key is unique
+			// resultant cache key format is "object:object_id#relation with {condition} {context}@user:user_id"
+			_, err = w.WriteString(" with " + cond.GetName())
+			if err != nil {
+				return
+			}
+
+			// if the condition also has context, we need an additional separator
+			// which cannot be present in condition names
+			if cond.GetContext() != nil {
+				_, err = w.WriteString(" ")
+				if err != nil {
+					return
+				}
+			}
+
+			// now write context to hash. Is a noop if context is nil.
+			if err = writeStruct(w, cond.GetContext()); err != nil {
+				return
+			}
+		}
+
+		if _, err = w.WriteString("@" + tupleKey.GetUser()); err != nil {
+			return
+		}
+
+		if n < len(tuples)-1 {
+			if _, err = w.WriteString(","); err != nil {
+				return
+			}
+		}
+	}
+	return
+}
+
 // CheckCacheKeyParams is all the necessary pieces to create a unique-per-check cache key.
 type CheckCacheKeyParams struct {
 	StoreID              string
@@ -180,13 +327,13 @@ func GetCheckCacheKey(params *CheckCacheKeyParams) (string, error) {
 
 	// here, and for context below, avoid hashing if we don't need to
 	if len(params.ContextualTuples) > 0 {
-		if err = keys.WriteTuples(hasher, params.ContextualTuples...); err != nil {
+		if err = writeTuples(hasher, params.ContextualTuples...); err != nil {
 			return "", err
 		}
 	}
 
 	if params.Context != nil {
-		if err = keys.WriteStruct(hasher, params.Context); err != nil {
+		if err = writeStruct(hasher, params.Context); err != nil {
 			return "", err
 		}
 	}
