43f244f9b2002864aa9b5b74d3525c40d8c11171
justin <justin.cohen@okta.com>
2025-02-07 13:38:48 -0500

    change matrix tests to accept a ClientInterface rather than build it

--- tests/check/check_test.go
+++ tests/check/check_test.go
@@ -31,33 +31,78 @@ import (
 )
 
 func TestMatrixMemory(t *testing.T) {
-	testRunTestMatrix(t, "memory", true)
-	testRunTestMatrix(t, "memory", false)
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
+
+	memory := "memory"
+	clientWithExperimentals := buildClientInterface(t, memory, true)
+	testRunTestMatrix(t, memory, true, clientWithExperimentals)
+
+	clientWithoutExperimentals := buildClientInterface(t, memory, false)
+	testRunTestMatrix(t, memory, false, clientWithoutExperimentals)
 }
 
-func testRunTestMatrix(t *testing.T, engine string, experimental bool) {
-	t.Run("test_matrix_"+engine+"_experimental_"+strconv.FormatBool(experimental), func(t *testing.T) {
-		t.Cleanup(func() {
-			goleak.VerifyNone(t)
-		})
-		cfg := config.MustDefaultConfig()
-		if experimental {
-			cfg.Experimentals = append(cfg.Experimentals, "enable-check-optimizations")
-		}
-		cfg.Log.Level = "error"
-		cfg.Datastore.Engine = engine
-		cfg.ListUsersDeadline = 0   // no deadline
-		cfg.ListObjectsDeadline = 0 // no deadline
-		// extend the timeout for the tests, coverage makes them slower
-		cfg.RequestTimeout = 10 * time.Second
+func TestMatrixPostgres(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
 
-		cfg.CheckIteratorCache.Enabled = true
+	postgres := "postgres"
+	clientWithExperimentals := buildClientInterface(t, postgres, true)
+	testRunTestMatrix(t, postgres, true, clientWithExperimentals)
 
-		tests.StartServer(t, cfg)
+	clientWithoutExperimentals := buildClientInterface(t, postgres, false)
+	testRunTestMatrix(t, postgres, false, clientWithoutExperimentals)
+}
+func TestMatrixMysql(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
 
-		conn := testutils.CreateGrpcConnection(t, cfg.GRPC.Addr)
+	mysql := "mysql"
+	clientWithExperimentals := buildClientInterface(t, mysql, true)
+	testRunTestMatrix(t, mysql, true, clientWithExperimentals)
 
-		runTestMatrixSuite(t, openfgav1.NewOpenFGAServiceClient(conn))
+	clientWithoutExperimentals := buildClientInterface(t, mysql, false)
+	testRunTestMatrix(t, mysql, false, clientWithoutExperimentals)
+}
+func TestMatrixSqlite(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
+
+	sqlite := "sqlite"
+	clientWithExperimentals := buildClientInterface(t, sqlite, true)
+	testRunTestMatrix(t, sqlite, true, clientWithExperimentals)
+
+	clientWithoutExperimentals := buildClientInterface(t, sqlite, false)
+	testRunTestMatrix(t, sqlite, false, clientWithoutExperimentals)
+}
+
+func buildClientInterface(t *testing.T, engine string, experimentalsEnabled bool) ClientInterface {
+	cfg := config.MustDefaultConfig()
+	if experimentalsEnabled {
+		cfg.Experimentals = append(cfg.Experimentals, "enable-check-optimizations")
+	}
+	cfg.Log.Level = "error"
+	cfg.Datastore.Engine = engine
+	cfg.ListUsersDeadline = 0   // no deadline
+	cfg.ListObjectsDeadline = 0 // no deadline
+	// extend the timeout for the tests, coverage makes them slower
+	cfg.RequestTimeout = 10 * time.Second
+
+	cfg.CheckIteratorCache.Enabled = true
+
+	tests.StartServer(t, cfg)
+
+	conn := testutils.CreateGrpcConnection(t, cfg.GRPC.Addr)
+	return openfgav1.NewOpenFGAServiceClient(conn)
+}
+
+func testRunTestMatrix(t *testing.T, engine string, experimentalsEnabled bool, client ClientInterface) {
+	t.Run("test_matrix_"+engine+"_experimental_"+strconv.FormatBool(experimentalsEnabled), func(t *testing.T) {
+		runTestMatrixSuite(t, client)
 	})
 }
 
