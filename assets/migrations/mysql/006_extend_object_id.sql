b0235a4494cf46211a2748fe0f39d7868d47b7ed
Eric Leijonmarck <eric.leijonmarck@gmail.com>
2025-02-11 13:55:16 +0000

    change from 256 - 255 for one less byte used

--- assets/migrations/mysql/006_extend_object_id.sql
+++ assets/migrations/mysql/006_extend_object_id.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE tuple MODIFY COLUMN object_id VARCHAR(256);
+ALTER TABLE tuple MODIFY COLUMN object_id VARCHAR(255);
 
 -- +goose Down
 ALTER TABLE tuple MODIFY COLUMN object_id VARCHAR(128);
