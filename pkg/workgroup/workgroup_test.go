deb9207c35f0827ef3caba9a87c200c2110825d9
Joshua Jones <joshua.jones@okta.com>
2025-02-12 17:07:19 -0500

    fixes a bug in the bound workgroup and adds comments
    if `Push` is called after the bound workgroup has
    been closed, the result will be a panic. this change
    adds an internal context to the workgroup that is
    canceled on close of the workgroup before the workgoup's
    channel is closed.

--- pkg/workgroup/workgroup_test.go
+++ pkg/workgroup/workgroup_test.go
@@ -12,13 +12,15 @@ import (
 func TestWorkerPool(t *testing.T) {
 	var i atomic.Int32
 
+	ctx := context.Background()
+
 	p := Bound(2, func(j int32) {
 		i.Add(j)
 	})
 
-	p.Push(context.Background(), 1)
+	p.Push(ctx, 1)
 	p.Close()
-	p.Push(context.Background(), 2)
+	p.Push(ctx, 2)
 
 	require.Equal(t, int32(1), i.Load())
 }
@@ -26,20 +28,22 @@ func TestWorkerPool(t *testing.T) {
 func TestWorkerPoolWaitCancel(t *testing.T) {
 	var i atomic.Int32
 
+	ctx := context.Background()
+
 	p := Bound(2, func(j int32) {
 		i.Add(j)
 		time.Sleep(1 * time.Second)
 	})
 
-	p.Push(context.Background(), 1)
-	p.Push(context.Background(), 1)
+	p.Push(ctx, 1)
+	p.Push(ctx, 1)
 
 	go func() {
-		p.Push(context.Background(), 1)
-		p.Push(context.Background(), 1)
-		p.Push(context.Background(), 1)
-		p.Push(context.Background(), 1)
-		p.Push(context.Background(), 1)
+		p.Push(ctx, 1)
+		p.Push(ctx, 1)
+		p.Push(ctx, 1)
+		p.Push(ctx, 1)
+		p.Push(ctx, 1)
 	}()
 	p.Close()
 
