bed7fc0d39d1758060a1e7b6d5783a224bfcc0b1
Yamil Asusta <yamil.asusta@okta.com>
2025-03-11 14:05:36 -0400

    fix

---
+++ internal/graph/shadow_resolver_test.go
@@ -0,0 +1,60 @@
+package graph
+
+import (
+	"context"
+	"errors"
+	"github.com/openfga/openfga/internal/mocks"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"testing"
+)
+
+func TestShadowResolver_ResolveCheck(t *testing.T) {
+	t.Run("should_noop_on_error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		main := NewMockCheckResolver(ctrl)
+		checker := NewShadowChecker(main, NewMockCheckResolver(ctrl))
+		expectedErr := errors.New("test error")
+		main.EXPECT().ResolveCheck(gomock.Any(), gomock.Any()).Return(nil, expectedErr)
+		_, err := checker.ResolveCheck(context.Background(), &ResolveCheckRequest{})
+		assert.Equal(t, expectedErr, err)
+	})
+	t.Run("should_log_warn_on_error", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		main := NewMockCheckResolver(ctrl)
+		shadow := NewMockCheckResolver(ctrl)
+		logger := mocks.NewMockLogger(ctrl)
+		checker := NewShadowChecker(main, shadow, ShadowResolverWithLogger(logger))
+		main.EXPECT().ResolveCheck(gomock.Any(), gomock.Any()).Return(&ResolveCheckResponse{
+			Allowed: false,
+			ResolutionMetadata: ResolveCheckResponseMetadata{
+				DatastoreQueryCount: 5,
+			},
+		}, nil)
+		shadow.EXPECT().ResolveCheck(gomock.Any(), gomock.Any()).Return(nil, context.Canceled)
+		logger.EXPECT().WarnWithContext(gomock.Any(), "shadow check errored", gomock.Any())
+		res, err := checker.ResolveCheck(context.Background(), &ResolveCheckRequest{})
+		checker.wg.Wait()
+		assert.NoError(t, err)
+		assert.False(t, res.Allowed)
+	})
+	t.Run("should_log_on_difference", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		main := NewMockCheckResolver(ctrl)
+		shadow := NewMockCheckResolver(ctrl)
+		logger := mocks.NewMockLogger(ctrl)
+		checker := NewShadowChecker(main, shadow, ShadowResolverWithLogger(logger))
+		main.EXPECT().ResolveCheck(gomock.Any(), gomock.Any()).Return(&ResolveCheckResponse{
+			Allowed: false,
+			ResolutionMetadata: ResolveCheckResponseMetadata{
+				DatastoreQueryCount: 5,
+			},
+		}, nil)
+		shadow.EXPECT().ResolveCheck(gomock.Any(), gomock.Any()).Return(&ResolveCheckResponse{Allowed: true}, nil)
+		logger.EXPECT().InfoWithContext(gomock.Any(), "shadow check difference", gomock.Any())
+		res, err := checker.ResolveCheck(context.Background(), &ResolveCheckRequest{})
+		checker.wg.Wait()
+		assert.NoError(t, err)
+		assert.False(t, res.Allowed)
+	})
+}
