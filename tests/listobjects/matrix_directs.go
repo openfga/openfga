afae15aacfc4a108a00daf21a0a782f068b94702
Justin Cohen <justincoh@gmail.com>
2025-03-25 16:48:45 -0400

    test: listobjects nested algebra with multiple types (#2334)

--- tests/listobjects/matrix_directs.go
+++ tests/listobjects/matrix_directs.go
@@ -156,4 +156,79 @@ var directs = []matrixTest{
 			},
 		},
 	},
+	{
+		Name: "directs_nested_algebraic_expressions",
+		Tuples: []*openfgav1.TupleKey{
+			// Will exclude due to "but not computed_3_times" in the resolution path
+			{Object: "directs:nested_alg_1", Relation: "direct", User: "user:nested_alg_1"},
+			{Object: "directs:nested_alg_1", Relation: "other_rel", User: "user:nested_alg_1"},
+
+			// Will exclude due to "but not computed_comb" in the resolution path
+			{Object: "directs:nested_alg_2", Relation: "direct_comb", User: "user:*"},
+			{Object: "directs:nested_alg_2", Relation: "other_rel", User: "user:*", Condition: xCond},
+
+			// Should return for both types
+			{Object: "directs:nested_alg_3", Relation: "direct_mult_types", User: "user:nested_alg_1"},
+			{Object: "directs:nested_alg_3", Relation: "direct_mult_types", User: "employee:*"},
+			{Object: "directs:nested_alg_3", Relation: "other_rel", User: "user:*", Condition: xCond},
+			{Object: "directs:nested_alg_3", Relation: "other_rel", User: "employee:nested_alg_1"},
+
+			// Will exclude due to lack of other_rel
+			{Object: "directs:nested_alg_4", Relation: "direct", User: "user:nested_alg_1"},
+		},
+		ListObjectAssertions: []*listobjectstest.Assertion{
+			{
+				Request: &openfgav1.ListObjectsRequest{
+					User:     "user:nested_alg_1",
+					Type:     "directs",
+					Relation: "alg_combined",
+				},
+				Expectation: []string{"directs:nested_alg_3"},
+				Context:     validConditionContext,
+			},
+			{
+				Request: &openfgav1.ListObjectsRequest{
+					User:     "employee:nested_alg_1",
+					Type:     "directs",
+					Relation: "alg_combined",
+				},
+				Expectation: []string{"directs:nested_alg_3"},
+			},
+		},
+	},
+	{
+		Name: "directs_alg_combined_oneline",
+		Tuples: []*openfgav1.TupleKey{
+			// excluded, only satisfies left side of AND
+			{Object: "directs:alg_combined_oneline_1", Relation: "direct", User: "user:alg_combined_oneline_1"},
+
+			// satisfies both sides of AND relation for this user
+			{Object: "directs:alg_combined_oneline_2", Relation: "direct_mult_types", User: "user:alg_combined_oneline_1"},
+			{Object: "directs:alg_combined_oneline_2", Relation: "direct_comb", User: "user:alg_combined_oneline_1"},
+			{Object: "directs:alg_combined_oneline_2", Relation: "other_rel", User: "user:alg_combined_oneline_1"},
+
+			// excluded for this employee, these relations are both on one side of the AND
+			{Object: "directs:alg_combined_oneline_2", Relation: "other_rel", User: "employee:alg_combined_oneline_1"},
+			{Object: "directs:alg_combined_oneline_2", Relation: "direct_mult_types", User: "employee:alg_combined_oneline_1"},
+		},
+		ListObjectAssertions: []*listobjectstest.Assertion{
+			{
+				Request: &openfgav1.ListObjectsRequest{
+					User:     "user:alg_combined_oneline_1",
+					Type:     "directs",
+					Relation: "alg_combined_oneline",
+				},
+				Expectation: []string{"directs:alg_combined_oneline_2"},
+				Context:     validConditionContext,
+			},
+			{
+				Request: &openfgav1.ListObjectsRequest{
+					User:     "employee:alg_combined_oneline_1",
+					Type:     "directs",
+					Relation: "alg_combined_oneline",
+				},
+				Expectation: nil,
+			},
+		},
+	},
 }
