5482bbd52fda6724a4b6cb37022f1e4567f7d780
Rafael Emerick <rafael.rezo@gmail.com>
2025-04-21 22:51:33 -0300

    FIX: Use VARCHAR instead of VARBINARY for condition_name to avoid implicit conversion error in MSSQL adapter tests

--- assets/migrations/mssql/005_add_conditions_to_tuples.sql
+++ assets/migrations/mssql/005_add_conditions_to_tuples.sql
@@ -1,6 +1,6 @@
 -- +goose Up
-ALTER TABLE tuple ADD condition_name VARCHAR(256), condition_context VARBINARY(MAX);
-ALTER TABLE changelog ADD condition_name VARCHAR(256), condition_context VARBINARY(MAX);
+ALTER TABLE tuple ADD condition_name VARCHAR(256), condition_context VARCHAR(256);
+ALTER TABLE changelog ADD condition_name VARCHAR(256), condition_context VARCHAR(256);
 
 -- +goose Down
 ALTER TABLE tuple DROP COLUMN condition_name, condition_context;
