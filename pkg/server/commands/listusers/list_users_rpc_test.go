5fb2171d4436870dba53e6a44b742e4e48ad1657
Michael Gobaco <michael.gobaco@okta.com>
2025-05-13 09:39:49 -0700

    Add Panic Handling to ListUsers expandExclusion

--- pkg/server/commands/listusers/list_users_rpc_test.go
+++ pkg/server/commands/listusers/list_users_rpc_test.go
@@ -4073,6 +4073,7 @@ func TestListUsersExclusionPanicExpandDirect(t *testing.T) {
 		},
 		{
 			name: "non_stratifiable_exclusion_containing_cycle_1_panic_expand_direct",
+
 			req: &openfgav1.ListUsersRequest{
 				Object:   &openfgav1.Object{Type: "document", Id: "1"},
 				Relation: "viewer",
@@ -4106,6 +4107,75 @@ func TestListUsersExclusionPanicExpandDirect(t *testing.T) {
 	tests.runListUsersTestCases(t)
 }
 
+func TestListUsersExclusionPanicExpandRewrite(t *testing.T) {
+	t.Cleanup(func() {
+		goleak.VerifyNone(t)
+	})
+	tests := ListUsersTests{
+		{
+			name: "exclusion_panic_expand_rewrite_base",
+			req: &openfgav1.ListUsersRequest{
+				Object:   &openfgav1.Object{Type: "document", Id: "1"},
+				Relation: "viewer",
+				UserFilters: []*openfgav1.UserTypeFilter{
+					{
+						Type: "user",
+					},
+				},
+			},
+			model: `
+				model
+					schema 1.1
+				type user
+				type document
+					relations
+						define blocked: [user]
+						define viewer: [user] but not blocked`,
+
+			tuples: []*openfgav1.TupleKey{
+				tuple.NewTupleKey("document:1", "viewer", "user:blocked_user"),
+				tuple.NewTupleKey("document:1", "blocked", "user:blocked_user"),
+				tuple.NewTupleKey("document:1", "viewer", "user:will"),
+				tuple.NewTupleKey("document:1", "blocked", "user:another_blocked_user"),
+			},
+			expectedUsers:     []string{},
+			expectedErrorMsg:  ErrPanic.Error(),
+			newListUsersQuery: NewListUsersQueryPanicListUsersExpandRewriteBase,
+		},
+		{
+			name: "exclusion_panic_expand_rewrite_subtract",
+			req: &openfgav1.ListUsersRequest{
+				Object:   &openfgav1.Object{Type: "document", Id: "1"},
+				Relation: "viewer",
+				UserFilters: []*openfgav1.UserTypeFilter{
+					{
+						Type: "user",
+					},
+				},
+			},
+			model: `
+				model
+					schema 1.1
+				type user
+				type document
+					relations
+						define blocked: [user]
+						define viewer: [user] but not blocked`,
+
+			tuples: []*openfgav1.TupleKey{
+				tuple.NewTupleKey("document:1", "viewer", "user:blocked_user"),
+				tuple.NewTupleKey("document:1", "blocked", "user:blocked_user"),
+				tuple.NewTupleKey("document:1", "viewer", "user:will"),
+				tuple.NewTupleKey("document:1", "blocked", "user:another_blocked_user"),
+			},
+			expectedUsers:     []string{},
+			expectedErrorMsg:  ErrPanic.Error(),
+			newListUsersQuery: NewListUsersQueryPanicListUsersExpandRewriteSubtract,
+		},
+	}
+	tests.runListUsersTestCases(t)
+}
+
 func NewListUsersQueryPanicExpandDirect(ds storage.RelationshipTupleReader, contextualTuples []*openfgav1.TupleKey, opts ...ListUsersQueryOption) *listUsersQuery {
 	l := &listUsersQuery{
 		logger:                  logger.NewNoopLogger(),
@@ -4116,6 +4186,58 @@ func NewListUsersQueryPanicExpandDirect(ds storage.RelationshipTupleReader, cont
 		maxConcurrentReads:      serverconfig.DefaultMaxConcurrentReadsForListUsers,
 		wasThrottled:            new(atomic.Bool),
 		expandDirectDispatch: func(ctx context.Context, listUsersQuery *listUsersQuery, req *internalListUsersRequest, userObjectType, userObjectID, userRelation string, resp expandResponse, foundUsersChan chan<- foundUser, hasCycle *atomic.Bool) expandResponse {
+			panic(ErrPanic)
+		},
+		expandExclusionExpandRewriteBase:     expandExclusionExpandRewrite,
+		expandExclusionExpandRewriteSubtract: expandExclusionExpandRewrite,
+	}
+
+	for _, opt := range opts {
+		opt(l)
+	}
+
+	l.datastore = storagewrappers.NewRequestStorageWrapper(ds, contextualTuples, l.maxConcurrentReads)
+
+	return l
+}
+
+func NewListUsersQueryPanicListUsersExpandRewriteBase(ds storage.RelationshipTupleReader, contextualTuples []*openfgav1.TupleKey, opts ...ListUsersQueryOption) *listUsersQuery {
+	l := &listUsersQuery{
+		logger:                  logger.NewNoopLogger(),
+		resolveNodeBreadthLimit: serverconfig.DefaultResolveNodeBreadthLimit,
+		resolveNodeLimit:        serverconfig.DefaultResolveNodeLimit,
+		deadline:                serverconfig.DefaultListUsersDeadline,
+		maxResults:              serverconfig.DefaultListUsersMaxResults,
+		maxConcurrentReads:      serverconfig.DefaultMaxConcurrentReadsForListUsers,
+		wasThrottled:            new(atomic.Bool),
+		expandDirectDispatch:    expandDirectDispatch,
+		expandExclusionExpandRewriteBase: func(ctx context.Context, l *listUsersQuery, req *internalListUsersRequest, userset *openfgav1.Userset, baseFoundUsersCh chan foundUser) expandResponse {
+			panic(ErrPanic)
+		},
+		expandExclusionExpandRewriteSubtract: expandExclusionExpandRewrite,
+	}
+
+	for _, opt := range opts {
+		opt(l)
+	}
+
+	l.datastore = storagewrappers.NewRequestStorageWrapper(ds, contextualTuples, l.maxConcurrentReads)
+
+	return l
+}
+
+func NewListUsersQueryPanicListUsersExpandRewriteSubtract(ds storage.RelationshipTupleReader, contextualTuples []*openfgav1.TupleKey, opts ...ListUsersQueryOption) *listUsersQuery {
+	l := &listUsersQuery{
+		logger:                           logger.NewNoopLogger(),
+		resolveNodeBreadthLimit:          serverconfig.DefaultResolveNodeBreadthLimit,
+		resolveNodeLimit:                 serverconfig.DefaultResolveNodeLimit,
+		deadline:                         serverconfig.DefaultListUsersDeadline,
+		maxResults:                       serverconfig.DefaultListUsersMaxResults,
+		maxConcurrentReads:               serverconfig.DefaultMaxConcurrentReadsForListUsers,
+		wasThrottled:                     new(atomic.Bool),
+		expandDirectDispatch:             expandDirectDispatch,
+		expandExclusionExpandRewriteBase: expandExclusionExpandRewrite,
+		expandExclusionExpandRewriteSubtract: func(ctx context.Context, l *listUsersQuery, req *internalListUsersRequest, userset *openfgav1.Userset, baseFoundUsersCh chan foundUser) expandResponse {
 			panic(ErrPanic)
 		},
 	}
