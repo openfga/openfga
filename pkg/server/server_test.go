d197897d2e7c4fd3f80f4e29d80cf3b2dcc16795
Yamil Asusta <yamil.asusta@okta.com>
2025-05-18 15:41:23 -0400

    fix: refactor handlesqlerror and minor fixes

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -2089,7 +2089,7 @@ func TestServer_ThrottleUntilDeadline(t *testing.T) {
 
 		require.NoError(t, err)
 		require.NotNil(t, resp)
-		require.Len(t, resp.GetUsers(), 1)
+		require.LessOrEqual(t, len(resp.GetUsers()), 1) // race condition of context cancellation
 	})
 
 	t.Run("list_objects_return_no_error_and_partial_results", func(t *testing.T) {
@@ -2103,7 +2103,7 @@ func TestServer_ThrottleUntilDeadline(t *testing.T) {
 
 		require.NoError(t, err)
 		require.NotNil(t, resp)
-		require.Len(t, resp.GetObjects(), 1)
+		require.LessOrEqual(t, len(resp.GetObjects()), 1) // race condition of context cancellation
 	})
 }
 
