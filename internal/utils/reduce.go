4de797ec5389b354b7892466b30ec6aee5807830
justin <justin.cohen@okta.com>
2025-05-27 13:24:42 -0400

    docstrings for new util methods

--- internal/utils/reduce.go
+++ internal/utils/reduce.go
@@ -1,5 +1,16 @@
 package utils
 
+// Reduce accepts a generic slice, an initializer value, and a function.
+// It iterates over the slice applying the function f to the current accumulated value and each element in the slice,
+// reducing the slice to a single value.
+//
+// Example reducing to a sum:
+//
+//	Reduce([]int{1, 2, 3}, 0, func(accumulator int, currentValue int) int {
+//		return accumulator + currentValue
+//	})
+//
+// returns 6
 func Reduce[S ~[]E, E any, A any](s S, initializer A, f func(A, E) A) A {
 	i := initializer
 	for _, item := range s {
