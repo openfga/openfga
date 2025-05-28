8b21a1f5a339ac3625fbadfcccfb1b675cce7466
justin <justin.cohen@okta.com>
2025-05-28 16:02:11 -0400

    working on readTuplesAndExecuteV2, directEdge complete
    # Conflicts:
    #	pkg/server/commands/reverseexpand/reverse_expand.go

--- pkg/server/commands/reverseexpand/reverse_expand.go
+++ pkg/server/commands/reverseexpand/reverse_expand.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"slices"
 	weightedGraph "github.com/openfga/language/pkg/go/graph"
 	"strings"
 	"sync"
@@ -45,6 +46,9 @@ type ReverseExpandRequest struct {
 	Consistency      openfgav1.ConsistencyPreference
 
 	edge *graph.RelationshipEdge
+
+	weightedEdge *weightedGraph.WeightedAuthorizationModelEdge
+
 	// TODO: might need another piece here
 	weightedEdgeTypeRel string
 }
@@ -323,7 +327,7 @@ func (c *ReverseExpandQuery) execute(
 	targetObjRef := typesystem.DirectRelationReference(req.ObjectType, req.Relation)
 
 	wg := c.typesystem.GetWeightedGraph()
-	globalWg = *wg
+	globalWg = *wg // for debugging convenience
 	if wg != nil {
 		targetTypeRel := req.weightedEdgeTypeRel
 		// This is true on the first call of reverse expand
@@ -351,8 +355,10 @@ func (c *ReverseExpandQuery) execute(
 			resultChan,
 			sourceUserObj,
 		)
+		return nil
 	}
 
+	//return nil
 	g := graph.New(c.typesystem)
 
 	edges, err := g.GetPrunedRelationshipEdges(targetObjRef, sourceUserRef)
@@ -460,6 +466,29 @@ func (c *ReverseExpandQuery) reverseExpandDirect(
 	return err
 }
 
+func (c *ReverseExpandQuery) reverseExpandDirectV2(
+	ctx context.Context,
+	req *ReverseExpandRequest,
+	resultChan chan<- *ReverseExpandResult,
+	intersectionOrExclusionInPreviousEdges bool,
+	resolutionMetadata *ResolutionMetadata,
+) error {
+	ctx, span := tracer.Start(ctx, "reverseExpandDirect", trace.WithAttributes(
+		//attribute.String("edge", req.edge.String()),
+		attribute.String("source.user", req.User.String()),
+	))
+	var err error
+	defer func() {
+		if err != nil {
+			telemetry.TraceError(span, err)
+		}
+		span.End()
+	}()
+
+	err = c.readTuplesAndExecuteV2(ctx, req, resultChan, intersectionOrExclusionInPreviousEdges, resolutionMetadata)
+	return err
+}
+
 func (c *ReverseExpandQuery) shouldCheckPublicAssignable(targetReference *openfgav1.RelationReference, userRef IsUserRef) (bool, error) {
 	_, userIsUserset := userRef.(*UserRefObjectRelation)
 	if userIsUserset {
@@ -473,14 +502,72 @@ func (c *ReverseExpandQuery) shouldCheckPublicAssignable(targetReference *openfg
 	return publiclyAssignable, nil
 }
 
+func (c *ReverseExpandQuery) buildQueryFiltersV2(
+	ctx context.Context,
+	req *ReverseExpandRequest,
+) ([]*openfgav1.ObjectRelation, string, error) {
+	var userFilter []*openfgav1.ObjectRelation
+	var relationFilter string
+	// type assertion to determine whether this is internal graph or weighted graph?
+	// OR do we use a different key, req.edge vs req.weightedEdge?
+	// or are there just completely different code paths
+	switch req.weightedEdge.GetEdgeType() {
+	case weightedGraph.DirectEdge:
+		// the .From() for a direct edge will have a type#rel e.g. directs-employee#other_rel
+		// TODO: might want a helper to get the type off it
+		from := req.weightedEdge.GetFrom().GetLabel()
+		rel := from[strings.Index(from, "#")+1:] // directs-employee#other_rel -> other_rel
+		relationFilter = rel
+
+		targetUserObjectType := req.weightedEdge.GetTo().GetLabel() // "employee"
+
+		publiclyAssignable := slices.Contains(req.weightedEdge.GetWildcards(), targetUserObjectType)
+
+		if publiclyAssignable {
+			// e.g. 'user:*'
+			userFilter = append(userFilter, &openfgav1.ObjectRelation{
+				Object: tuple.TypedPublicWildcard(targetUserObjectType),
+			})
+		}
+
+		// e.g. 'user:bob'
+		if val, ok := req.User.(*UserRefObject); ok {
+			userFilter = append(userFilter, &openfgav1.ObjectRelation{
+				Object: tuple.BuildObject(val.Object.GetType(), val.Object.GetId()),
+			})
+		}
+
+		// e.g. 'group:eng#member'
+		if val, ok := req.User.(*UserRefObjectRelation); ok {
+			userFilter = append(userFilter, val.ObjectRelation)
+		}
+	case weightedGraph.TTUEdge:
+		relationFilter = req.edge.TuplesetRelation
+		// a TTU edge can only have a userset as a source node
+		// e.g. 'group:eng#member'
+		if val, ok := req.User.(*UserRefObjectRelation); ok {
+			userFilter = append(userFilter, &openfgav1.ObjectRelation{
+				Object: val.ObjectRelation.GetObject(),
+			})
+			fmt.Printf("TTU EDGE GetObject(): %s", val.ObjectRelation.GetObject())
+		} else {
+			panic("unexpected source for reverse expansion of tuple to userset")
+		}
+	// TODO: are there any other cases?
+	default:
+		panic("unsupported edge type")
+	}
+
+	return userFilter, relationFilter, nil
+}
+
 func (c *ReverseExpandQuery) buildQueryFilters(
 	ctx context.Context,
 	req *ReverseExpandRequest,
 ) ([]*openfgav1.ObjectRelation, string, error) {
 	var userFilter []*openfgav1.ObjectRelation
 	var relationFilter string
-	// type assertion to determine whether this is internal graph or weighted graph
-	// OR do we use a different key, req.edge vs req.weightedEdge
+
 	switch req.edge.Type {
 	case graph.DirectEdge:
 		// the .From() for a direct edge will have a type#rel e.g. directs-employee#other_rel
@@ -548,6 +635,124 @@ func (c *ReverseExpandQuery) readTuplesAndExecute(
 	var relationFilter string
 
 	userFilter, relationFilter, err := c.buildQueryFilters(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	// find all tuples of the form req.edge.TargetReference.Type:...#relationFilter@userFilter
+	iter, err := c.datastore.ReadStartingWithUser(ctx, req.StoreID, storage.ReadStartingWithUserFilter{
+		ObjectType: req.edge.TargetReference.GetType(), //directs-employee
+		Relation:   relationFilter,                     // other-rel
+		UserFilter: userFilter,                         // .Object = employee#alg_combined_1
+	}, storage.ReadStartingWithUserOptions{
+		Consistency: storage.ConsistencyOptions{
+			Preference: req.Consistency,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	// filter out invalid tuples yielded by the database iterator
+	filteredIter := storage.NewFilteredTupleKeyIterator(
+		storage.NewTupleKeyIteratorFromTupleIterator(iter),
+		validation.FilterInvalidTuples(c.typesystem),
+	)
+	defer filteredIter.Stop()
+
+	pool := concurrency.NewPool(ctx, int(c.resolveNodeBreadthLimit))
+
+	var errs error
+
+LoopOnIterator:
+	for {
+		tk, err := filteredIter.Next(ctx)
+		if err != nil {
+			if errors.Is(err, storage.ErrIteratorDone) {
+				break
+			}
+			errs = errors.Join(errs, err)
+			break LoopOnIterator
+		}
+
+		condEvalResult, err := eval.EvaluateTupleCondition(ctx, tk, c.typesystem, req.Context)
+		if err != nil {
+			errs = errors.Join(errs, err)
+			continue
+		}
+
+		if !condEvalResult.ConditionMet {
+			if len(condEvalResult.MissingParameters) > 0 {
+				errs = errors.Join(errs, condition.NewEvaluationError(
+					tk.GetCondition().GetName(),
+					fmt.Errorf("tuple '%s' is missing context parameters '%v'",
+						tuple.TupleKeyToString(tk),
+						condEvalResult.MissingParameters),
+				))
+			}
+
+			continue
+		}
+
+		foundObject := tk.GetObject()
+		var newRelation string
+
+		switch req.edge.Type {
+		case graph.DirectEdge:
+			newRelation = tk.GetRelation()
+		case graph.TupleToUsersetEdge:
+			newRelation = req.edge.TargetReference.GetRelation()
+		default:
+			panic("unsupported edge type")
+		}
+
+		pool.Go(func(ctx context.Context) error {
+			return c.dispatch(ctx, &ReverseExpandRequest{
+				StoreID:    req.StoreID,
+				ObjectType: req.ObjectType,
+				Relation:   req.Relation,
+				User: &UserRefObjectRelation{
+					ObjectRelation: &openfgav1.ObjectRelation{
+						Object:   foundObject,
+						Relation: newRelation,
+					},
+					Condition: tk.GetCondition(),
+				},
+				ContextualTuples: req.ContextualTuples,
+				Context:          req.Context,
+				edge:             req.edge,
+				Consistency:      req.Consistency,
+			}, resultChan, intersectionOrExclusionInPreviousEdges, resolutionMetadata)
+		})
+	}
+
+	errs = errors.Join(errs, pool.Wait())
+	if errs != nil {
+		telemetry.TraceError(span, errs)
+		return errs
+	}
+
+	return nil
+}
+
+func (c *ReverseExpandQuery) readTuplesAndExecuteV2(
+	ctx context.Context,
+	req *ReverseExpandRequest,
+	resultChan chan<- *ReverseExpandResult,
+	intersectionOrExclusionInPreviousEdges bool,
+	resolutionMetadata *ResolutionMetadata,
+) error {
+	if ctx.Err() != nil {
+		return ctx.Err()
+	}
+
+	ctx, span := tracer.Start(ctx, "readTuplesAndExecute")
+	defer span.End()
+
+	var userFilter []*openfgav1.ObjectRelation
+	var relationFilter string
+
+	userFilter, relationFilter, err := c.buildQueryFiltersV2(ctx, req)
 	if err != nil {
 		return err
 	}
@@ -791,12 +996,13 @@ func (c *ReverseExpandQuery) LoopOverWeightedEdges(
 		}
 		switch edge.GetEdgeType() {
 		case weightedGraph.DirectEdge:
-			fmt.Printf("JUSTIN DIRECT EDGE")
+			fmt.Println("JUSTIN DIRECT EDGE")
+			r.weightedEdge = edge
 			pool.Go(func(ctx context.Context) error {
-				return c.reverseExpandDirect(ctx, r, resultChan, needsCheck, resolutionMetadata)
+				return c.reverseExpandDirectV2(ctx, r, resultChan, needsCheck, resolutionMetadata)
 			})
 		case weightedGraph.ComputedEdge:
-			fmt.Printf("JUSTIN COMPUTED EDGE")
+			fmt.Println("JUSTIN COMPUTED EDGE")
 			// follow the computed_userset edge, no new goroutine needed since it's not I/O intensive
 			to := edge.GetTo().GetUniqueLabel()
 
@@ -889,4 +1095,9 @@ func filter[S ~[]E, E any](s S, f func(E) bool) []E {
 		}
 	}
 	return filteredItems
+}
+
+// expects a "type#rel"
+func getTypeFromLabel(label string) string {
+
 }
