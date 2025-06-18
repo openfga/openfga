b581691a8d07053323dda141b011ad73b11fc52d
Yamil Asusta <hello@yamilasusta.com>
2025-06-18 14:28:59 -0400

    clean: Remove fanin code (#2514)

--- internal/iterator/fan_in_test.go
+++ internal/iterator/fan_in_test.go
@@ -13,64 +13,15 @@ import (
 	"github.com/openfga/openfga/pkg/storage"
 )
 
-func makeIterChan(ctrl *gomock.Controller, id string, next bool) chan *Msg {
+func makeIterChan(ctrl *gomock.Controller, id string) chan *Msg {
 	iterChan := make(chan *Msg, 1)
 	iter := mocks.NewMockIterator[string](ctrl)
-	if next {
-		iter.EXPECT().Next(gomock.Any()).MaxTimes(1).Return(id, nil)
-		iter.EXPECT().Next(gomock.Any()).MaxTimes(1).Return("", storage.ErrIteratorDone)
-	}
+	iter.EXPECT().Next(gomock.Any()).MaxTimes(1).Return(id, nil)
+	iter.EXPECT().Next(gomock.Any()).MaxTimes(1).Return("", storage.ErrIteratorDone)
 	iter.EXPECT().Stop().MinTimes(1)
 	iterChan <- &Msg{Iter: iter}
 	close(iterChan)
 	return iterChan
-}
-
-func TestFanIn(t *testing.T) {
-	t.Cleanup(func() {
-		goleak.VerifyNone(t)
-	})
-	ctx := context.Background()
-	ctrl := gomock.NewController(t)
-	t.Cleanup(ctrl.Finish)
-	fanin := NewFanIn(ctx, 5)
-
-	fanin.Add(makeIterChan(ctrl, "1", true))
-	fanin.Add(makeIterChan(ctrl, "2", true))
-	fanin.Add(makeIterChan(ctrl, "3", true))
-	fanin.Add(makeIterChan(ctrl, "4", true))
-	fanin.Add(makeIterChan(ctrl, "5", true))
-	fanin.Add(makeIterChan(ctrl, "6", true))
-	fanin.Done()
-	iterChan1 := makeIterChan(ctrl, "8", false)
-	accepted := fanin.Add(iterChan1)
-	require.False(t, accepted)
-	Drain(iterChan1)
-	iterChan2 := makeIterChan(ctrl, "9", false)
-	accepted = fanin.Add(iterChan2)
-	require.False(t, accepted)
-	Drain(iterChan2)
-
-	out := fanin.Out()
-	iterations := 0
-	for msg := range out {
-		id, err := msg.Iter.Next(ctx)
-		require.NoError(t, err)
-		i, err := strconv.Atoi(id)
-		require.NoError(t, err)
-		require.Positive(t, i)
-		require.LessOrEqual(t, i, 7)
-		_, err = msg.Iter.Next(ctx)
-		require.Equal(t, storage.ErrIteratorDone, err)
-		msg.Iter.Stop()
-		iterations++
-		if iterations > 2 {
-			break
-		}
-	}
-	fanin.Stop()
-	fanin.wg.Wait()
-	Drain(fanin.Out()).Wait()
 }
 
 func TestFanInIteratorChannels(t *testing.T) {
@@ -83,15 +34,15 @@ func TestFanInIteratorChannels(t *testing.T) {
 
 	chans := make([]<-chan *Msg, 0, 9)
 	chans = append(chans,
-		makeIterChan(ctrl, "1", true),
-		makeIterChan(ctrl, "2", true),
-		makeIterChan(ctrl, "3", true),
-		makeIterChan(ctrl, "4", true),
-		makeIterChan(ctrl, "5", true),
-		makeIterChan(ctrl, "6", true),
-		makeIterChan(ctrl, "7", true),
-		makeIterChan(ctrl, "8", true),
-		makeIterChan(ctrl, "9", true))
+		makeIterChan(ctrl, "1"),
+		makeIterChan(ctrl, "2"),
+		makeIterChan(ctrl, "3"),
+		makeIterChan(ctrl, "4"),
+		makeIterChan(ctrl, "5"),
+		makeIterChan(ctrl, "6"),
+		makeIterChan(ctrl, "7"),
+		makeIterChan(ctrl, "8"),
+		makeIterChan(ctrl, "9"))
 
 	out := FanInIteratorChannels(ctx, chans)
 
@@ -113,11 +64,11 @@ func TestFanInIteratorChannels(t *testing.T) {
 	cancel() // Stop would still be called in all entries even tho its been cancelled
 	chans = make([]<-chan *Msg, 0, 5)
 	chans = append(chans,
-		makeIterChan(ctrl, "1", true),
-		makeIterChan(ctrl, "2", true),
-		makeIterChan(ctrl, "3", true),
-		makeIterChan(ctrl, "4", true),
-		makeIterChan(ctrl, "5", true),
+		makeIterChan(ctrl, "1"),
+		makeIterChan(ctrl, "2"),
+		makeIterChan(ctrl, "3"),
+		makeIterChan(ctrl, "4"),
+		makeIterChan(ctrl, "5"),
 	)
 	out = FanInIteratorChannels(cancellable, chans)
 	iterations = 0
