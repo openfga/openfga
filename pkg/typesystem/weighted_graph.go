b767c393b4d0fc3ae02d1e14fabf2b7b3e810175
Adrian Tam <adrian.tam@okta.com>
2025-06-20 14:40:42 -0400

    update based on code review feedback

--- pkg/typesystem/weighted_graph.go
+++ pkg/typesystem/weighted_graph.go
@@ -95,23 +95,22 @@ func (t *TypeSystem) GetEdgesForIntersection(intersectionUniqueLabel string, sou
 					directEdgesAreLowest = true
 					lowestWeight = weight
 				}
-			} else { // non-direct edges
-				if lowestWeight == 0 && edgeNum != 0 {
-					// this means that all the direct edges are not connected.
-					// In reality, should not happen because the caller should have trimmed
-					// the parent node already.
-					return IntersectionEdges{}, nil
-				}
-				if weight, ok := edge.GetWeight(sourceType); ok {
-					// in the case of the first edge, we need to initialize as the least weight
-					// for other edges to compare against it.
-					// For non-first edges, only replace if it has the lowest weight.
-					if weight < lowestWeight || edgeNum == 0 {
-						lowestEdge = edge
-						lowestWeight = weight
-						directEdgesAreLowest = false
-					}
-				}
+				continue
+			}
+			// non-direct edges
+			if lowestWeight == 0 && edgeNum != 0 {
+				// this means that all the direct edges are not connected.
+				// In reality, should not happen because the caller should have trimmed
+				// the parent node already.
+				return IntersectionEdges{}, nil
+			}
+			if weight, ok := edge.GetWeight(sourceType); ok && (weight < lowestWeight || edgeNum == 0) {
+				// in the case of the first edge, we need to initialize as the least weight
+				// for other edges to compare against it.
+				// For non-first edges, only replace if it has the lowest weight.
+				lowestEdge = edge
+				lowestWeight = weight
+				directEdgesAreLowest = false
 			}
 		}
 	}
