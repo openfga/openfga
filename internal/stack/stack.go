e6038fd62466a6a289dbb4c85affbc2ff04e6916
Justin Cohen <justincoh@gmail.com>
2025-07-10 10:05:22 -0400

    refactor: use a linked list in ReverseExpand instead of copying slices (#2542)

---
+++ internal/stack/stack.go
@@ -0,0 +1,22 @@
+package stack
+
+// Stack is an implementation of a stack based on a linked list.
+//
+// *Important*: Each push() or pop() operation creates and returns a pointer to a new stack entirely to
+// ensure thread safety.
+type Stack[T any] struct {
+	value T
+	next  *Stack[T]
+}
+
+func Push[T any](stack *Stack[T], value T) *Stack[T] {
+	return &Stack[T]{value: value, next: stack}
+}
+
+func Pop[T any](stack *Stack[T]) (T, *Stack[T]) {
+	return stack.value, stack.next
+}
+
+func Peek[T any](stack *Stack[T]) T {
+	return stack.value
+}
