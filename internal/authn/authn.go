d01f5b8696035019eec98edec2a497702685a761
Riju Mondal <mailrijumondal@gmail.com>
2025-08-16 17:05:38 +0530

    chore: improve test coverage

--- internal/authn/authn.go
+++ internal/authn/authn.go
@@ -21,7 +21,7 @@ type Authenticator interface {
 	// Authenticate returns a nil error and the AuthClaims info (if available) if the subject is authenticated or a
 	// non-nil error with an appropriate error cause otherwise.
 	Authenticate(requestContext context.Context) (*authclaims.AuthClaims, error)
-	// Legacy Close Method
+	// Close Cleans up the authenticator.
 	Close()
 }
 
