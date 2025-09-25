f8cad5d820d8056f6184820f32789dda8696ee88
Joshua Jones <joshua.jones@okta.com>
2025-09-25 12:58:58 -0400

    build pipeline from entire graph

--- pkg/server/commands/reverseexpand/reverse_expand_weighted.go
+++ pkg/server/commands/reverseexpand/reverse_expand_weighted.go
@@ -1552,12 +1552,6 @@ func (p *Path) resolve(ctx context.Context, source *Node, coord *coordinator) {
 		p.traversal.pipeline = make(map[*Node]*Worker)
 	}
 
-	_, ok := source.GetWeight(p.target.GetLabel())
-	if !ok {
-		// println(source.GetLabel())
-		return
-	}
-
 	if _, ok := p.traversal.pipeline[source]; ok {
 		return
 	}
@@ -1565,7 +1559,7 @@ func (p *Path) resolve(ctx context.Context, source *Node, coord *coordinator) {
 
 	edges, ok := p.traversal.graph.GetEdgesFromNode(source)
 	if !ok {
-		panic("given node not a member of graph")
+		return
 	}
 
 	for _, edge := range edges {
@@ -1582,17 +1576,6 @@ func (p *Path) resolve(ctx context.Context, source *Node, coord *coordinator) {
 			p.resolve(ctx, edge.GetTo(), coord)
 			p.traversal.pipeline[tupleset].Listen(edge, p.traversal.pipeline[edge.GetTo()].Subscribe(ctx, tupleset))
 			continue
-		}
-
-		if edge.GetEdgeType() == EdgeTypeDirect {
-			if edge.GetTo().GetNodeType() == NodeTypeSpecificType {
-				if _, ok := p.traversal.pipeline[edge.GetTo()]; !ok {
-					p.traversal.pipeline[edge.GetTo()] = NewWorker(p.traversal.backend, edge.GetTo(), coord)
-					p.traversal.pipeline[edge.GetTo()].Listen(edge, emptyGroupSequence)
-				}
-				p.traversal.pipeline[source].Listen(edge, p.traversal.pipeline[edge.GetTo()].Subscribe(ctx, source))
-				continue
-			}
 		}
 
 		p.resolve(ctx, edge.GetTo(), coord)
