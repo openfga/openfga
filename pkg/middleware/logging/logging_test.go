cedf586c0b7122d42a3e61487c04776d561eec24
Alex Mammay <alexmammay@gmail.com>
2025-09-24 20:03:00 -0400

    clean up linter errors

--- pkg/middleware/logging/logging_test.go
+++ pkg/middleware/logging/logging_test.go
@@ -5,12 +5,10 @@ import (
 	"context"
 	"encoding/json"
 	"net"
+	"sync"
 	"testing"
 
 	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
-	openfgav1 "github.com/openfga/api/proto/openfga/v1"
-	"github.com/openfga/openfga/pkg/logger"
-	"github.com/openfga/openfga/pkg/middleware/requestid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
@@ -18,11 +16,16 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/test/bufconn"
+
+	openfgav1 "github.com/openfga/api/proto/openfga/v1"
+
+	"github.com/openfga/openfga/pkg/logger"
+	"github.com/openfga/openfga/pkg/middleware/requestid"
 )
 
 type outputCapture struct {
 	Level           string          `json:"level"`
-	Ts              float64         `json:"ts"`
+	TS              float64         `json:"ts"`
 	Msg             string          `json:"msg"`
 	GrpcService     string          `json:"grpc_service"`
 	GrpcMethod      string          `json:"grpc_method"`
@@ -32,12 +35,11 @@ type outputCapture struct {
 	RawResponse     json.RawMessage `json:"raw_response"`
 	QueryDurationMs string          `json:"query_duration_ms"`
 	PeerAddress     string          `json:"peer.address"`
-	RequestId       string          `json:"request_id"`
+	RequestID       string          `json:"request_id"`
 	GrpcCode        int             `json:"grpc_code"`
 }
 
 func TestNewLoggingInterceptor_concrete(t *testing.T) {
-
 	gotBuffer := new(bytes.Buffer)
 
 	core := zapcore.NewCore(
@@ -53,13 +55,17 @@ func TestNewLoggingInterceptor_concrete(t *testing.T) {
 
 	listner := bufconn.Listen(1024 * 1024)
 	srv := grpc.NewServer(serverOpts...)
-	t.Cleanup(srv.Stop)
 
 	openfgav1.RegisterOpenFGAServiceServer(srv, &fgaServer{})
 
+	wg := sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		err := srv.Serve(listner)
-		require.NoError(t, err)
+		if err != nil {
+			t.Errorf("failed to serve: %v", err)
+		}
 	}()
 
 	dialer := func(context.Context, string) (net.Conn, error) {
@@ -83,7 +89,7 @@ func TestNewLoggingInterceptor_concrete(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, "info", output.Level)
-	assert.NotEmpty(t, output.Ts)
+	assert.NotEmpty(t, output.TS)
 	assert.Equal(t, "grpc_req_complete", output.Msg)
 	assert.Equal(t, "openfga.v1.OpenFGAService", output.GrpcService)
 	assert.Equal(t, "Check", output.GrpcMethod)
@@ -93,8 +99,11 @@ func TestNewLoggingInterceptor_concrete(t *testing.T) {
 	assert.NotEmpty(t, output.RawResponse)
 	assert.NotEmpty(t, output.QueryDurationMs)
 	assert.NotEmpty(t, output.PeerAddress)
-	assert.NotEmpty(t, output.RequestId)
+	assert.NotEmpty(t, output.RequestID)
 	assert.Equal(t, 0, output.GrpcCode)
+
+	srv.Stop()
+	wg.Wait()
 }
 
 type fgaServer struct {
