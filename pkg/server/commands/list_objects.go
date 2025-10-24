14584b0914fdc15dc6f2f2b602b17e916ba5ed83
Joshua Jones <joshua.jones@okta.com>
2025-10-24 09:41:41 -0400

    respect requested consistency value in pipeline

--- pkg/server/commands/list_objects.go
+++ pkg/server/commands/list_objects.go
@@ -530,6 +530,7 @@ func (q *ListObjectsQuery) Execute(
 			TypeSystem: typesys,
 			Context:    req.GetContext(),
 			Graph:      wgraph,
+			Preference: req.GetConsistency(),
 		}
 
 		pipeline := reverseexpand.NewPipeline(backend)
@@ -707,6 +708,7 @@ func (q *ListObjectsQuery) ExecuteStreamed(ctx context.Context, req *openfgav1.S
 			TypeSystem: typesys,
 			Context:    req.GetContext(),
 			Graph:      wgraph,
+			Preference: req.GetConsistency(),
 		}
 
 		pipeline := reverseexpand.NewPipeline(backend)
