3a869f40324d3d7290ab78dc2d89353233b4135b
Jeremy Loy <jeremy.loy@headspace.com>
2025-10-29 17:22:01 -0400

    no longer drop the v1 indexes

--- assets/migrations/postgres/007_add_reverse_lookup_index_v2.sql
+++ assets/migrations/postgres/007_add_reverse_lookup_index_v2.sql
@@ -1,9 +1,6 @@
 -- +goose Up
 -- recreate the reverse lookup index with relation last, as it is an optional parameter
 CREATE INDEX idx_reverse_lookup_user_v2 on tuple (store, object_type, _user, relation);
-DROP INDEX IF EXISTS idx_reverse_lookup_user;
 
 -- +goose Down
--- recreate migration 3
-CREATE INDEX idx_reverse_lookup_user on tuple (store, object_type, relation, _user);
 DROP INDEX IF EXISTS idx_reverse_lookup_user_v2;
