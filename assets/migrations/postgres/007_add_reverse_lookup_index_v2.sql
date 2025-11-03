39e46a458e94b8d75eb44c18b3967d9a75c1dc0e
Jeremy Loy <20881794+JeremyLoy@users.noreply.github.com>
2025-11-03 18:20:12 -0500

    change postgres migration to be concurrent

--- assets/migrations/postgres/007_add_reverse_lookup_index_v2.sql
+++ assets/migrations/postgres/007_add_reverse_lookup_index_v2.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- recreate the reverse lookup index with relation last, as it is an optional parameter
-CREATE INDEX idx_reverse_lookup_user_v2 on tuple (store, object_type, _user, relation);
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_reverse_lookup_user_v2 ON tuple (store, object_type, _user, relation);
 
 -- +goose Down
-DROP INDEX IF EXISTS idx_reverse_lookup_user_v2;
+DROP INDEX CONCURRENTLY IF EXISTS idx_reverse_lookup_user_v2;
