ef6362a1a049587a2c440be864b3121bc68b9b7b
Yissell Garma <yissell.garma@okta.com>
2025-11-05 13:10:46 -0500

    added test

--- internal/check/strategies/weight2_test.go
+++ internal/check/strategies/weight2_test.go
@@ -2,6 +2,7 @@ package strategies
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/oklog/ulid/v2"
@@ -356,6 +357,115 @@ func TestWeight2Userset(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, res)
 		require.False(t, res.GetAllowed())
+	})
+
+	t.Run("combined_with_match_and_condition", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		storeID := ulid.Make().String()
+
+		readStartingWithUserMembers := []*openfgav1.Tuple{
+			{
+				Key: tuple.NewTupleKey("group:2", "members", "user:1"),
+			},
+			{
+				Key: tuple.NewTupleKey("group:3", "members", "user:1"),
+			},
+		}
+		readStartingWithUserPublic := []*openfgav1.Tuple{
+			{
+				Key: tuple.NewTupleKey("group:1", "public_restricted", "user:*"),
+			},
+			{
+				Key: tuple.NewTupleKey("group:2", "public_restricted", "user:*"),
+			},
+		}
+		contextStruct := testutils.MustNewStruct(t, map[string]interface{}{"tcond": "restricted"})
+		readStartingWithUserRestricted := []*openfgav1.Tuple{
+			{
+				Key: &openfgav1.TupleKey{
+					Object:   "group:1",
+					Relation: "restricted",
+					User:     "user:1",
+					Condition: &openfgav1.RelationshipCondition{
+						Name:    "xcond",
+						Context: contextStruct,
+					},
+				},
+			},
+			{
+				Key: &openfgav1.TupleKey{
+					Object:   "group:3",
+					Relation: "restricted",
+					User:     "user:1",
+					Condition: &openfgav1.RelationshipCondition{
+						Name:    "xcond",
+						Context: contextStruct,
+					},
+				},
+			},
+		}
+
+		mockDatastore := mocks.NewMockOpenFGADatastore(ctrl)
+		mockDatastore.EXPECT().ReadStartingWithUser(gomock.Any(), storeID, gomock.Any(), gomock.Any()).
+			MaxTimes(3). // Allow any number of calls
+			DoAndReturn(func(ctx context.Context, sID string, filter storage.ReadStartingWithUserFilter, opts storage.ReadStartingWithUserOptions) (storage.TupleIterator, error) {
+
+				// Manually check the relation and return the right data
+				switch filter.Relation {
+				case "members":
+					return storage.NewStaticTupleIterator(readStartingWithUserMembers), nil
+				case "public_restricted":
+					return storage.NewStaticTupleIterator(readStartingWithUserPublic), nil
+				case "restricted":
+					return storage.NewStaticTupleIterator(readStartingWithUserRestricted), nil
+				default:
+					return nil, fmt.Errorf("unexpected relation %s", filter.Relation)
+				}
+			})
+
+		ctx := context.Background()
+		model := testutils.MustTransformDSLToProtoWithID(`
+			model
+				schema 1.1
+			type user
+			type employee
+			type group
+				relations
+					define members: [user]
+					define public_restricted: [user:*] but not restricted
+					define restricted: [user with xcond]
+					define all: (members and public_restricted) or guest
+					define guest: [employee]
+			type document
+				relations
+					define viewer: [user, group#all]
+			condition xcond(tcond: string) {
+  				tcond == 'restricted'
+			}`)
+
+		mg, err := check.NewAuthorizationModelGraph(model)
+		require.NoError(t, err)
+
+		edges, ok := mg.GetEdgesFromNodeId("document#viewer")
+		require.True(t, ok)
+
+		iter := storage.NewStaticTupleKeyIterator([]*openfgav1.TupleKey{{
+			User:     "group:2#all",
+			Relation: "viewer",
+			Object:   "document:1",
+		}})
+
+		strategy := NewWeight2(mg, mockDatastore)
+		res, err := strategy.Userset(ctx, &check.Request{
+			StoreID:              storeID,
+			AuthorizationModelID: mg.GetModelID(),
+			TupleKey:             tuple.NewTupleKey("document:1", "viewer", "user:1"),
+		}, edges[1], iter)
+		require.NoError(t, err)
+		require.NotNil(t, res)
+		require.True(t, res.GetAllowed())
 	})
 }
 
