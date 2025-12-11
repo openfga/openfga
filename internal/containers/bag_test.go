14b86efc515b865bae13a6204649899db54d64a4
Joshua Jones <joshua.jones@okta.com>
2025-12-11 17:21:56 -0500

    add test for bag

---
+++ internal/containers/bag_test.go
@@ -0,0 +1,36 @@
+package containers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBag(t *testing.T) {
+	var b Bag[int]
+
+	for i := range 1000 {
+		b.Add(i + 1)
+	}
+
+	values := b.Seq()
+
+	var count int
+
+	cmp := 1000
+	for i := range values {
+		require.Equal(t, cmp, i)
+		cmp--
+		count++
+	}
+	require.Equal(t, count, 1000)
+}
+
+func BenchmarkBag(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		var b Bag[int]
+		for i := range 1000 {
+			b.Add(i)
+		}
+	}
+}
