ff2040ab015783a72cafa0c0f515baf9f1f46d2f
Andrés Aguiar <andres.aguiar@okta.com>
2026-03-20 11:10:43 -0300

    AuthZen v1.0 Implementation (#2875)
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Rokibul Hasan <mdrokibulhasan@appscode.com>
    Signed-off-by: Vihang Mehta <vihang@gimletlabs.ai>
    Co-authored-by: Karl Persson <kalle.persson92@gmail.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Maria Ines Parnisari <maria.inesparnisari@okta.com>
    Co-authored-by: Rokibul Hasan <mdrokibulhasan18@gmail.com>
    Co-authored-by: José Padilla <jose.padilla@okta.com>
    Co-authored-by: Adrian Tam <adrian.tam@okta.com>
    Co-authored-by: Vihang Mehta <vihang@gimletlabs.ai>
    Co-authored-by: Joshua Jones <joshua.jones.software@gmail.com>
    Co-authored-by: Justin Cohen <justincoh@gmail.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -19,6 +19,7 @@ import (
 	"go.uber.org/zap"
 	"golang.org/x/sync/singleflight"
 
+	authzenv1 "github.com/openfga/api/proto/authzen/v1"
 	openfgav1 "github.com/openfga/api/proto/openfga/v1"
 
 	"github.com/openfga/openfga/internal/authz"
@@ -165,6 +166,7 @@ var (
 // a GRPC and HTTP server.
 type Server struct {
 	openfgav1.UnimplementedOpenFGAServiceServer
+	authzenv1.UnimplementedAuthZenServiceServer
 
 	logger                           logger.Logger
 	datastore                        storage.OpenFGADatastore
