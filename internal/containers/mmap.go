d91d9c82c8ac414778102248affd8815100418f7
Archy <6429715+archy-rock3t-cloud@users.noreply.github.com>
2026-03-20 19:40:44 +0100

    docs: fix typos in comments (#2972)
    Signed-off-by: Artem Muterko <artem@sopho.tech>
    Co-authored-by: Adrian Tam <adrian.tam@okta.com>

--- internal/containers/mmap.go
+++ internal/containers/mmap.go
@@ -3,7 +3,7 @@ package containers
 import "sync"
 
 // mmap is a struct that protects a standard library map with a mutex. All functions of mmap are
-// thead-safe map operations.
+// thread-safe map operations.
 //
 // Mmap exists as an alternative to sync.Map as a datastructure that does not necessarily require
 // an allocation for each of its elements. Sync.Map always requires an allocation for each element
@@ -13,7 +13,7 @@ type AtomicMap[K comparable, V any] struct {
 	m  map[K]V
 }
 
-// LoadOrStore is a function that atomically checks for the existence of a givne key within the map
+// LoadOrStore is a function that atomically checks for the existence of a given key within the map
 // and adds the given value if the key was not found. The existing value is returned in the case
 // that the key was found in the map, or the new value is returned in the case that the key was not
 // found within the map. The returned boolean is true when the given key was found within the map.
