5b53c8846e558153226ce290c80f6050185743f8
Asish Kumar <officialasishkumar@gmail.com>
2026-04-06 00:55:57 +0530

    feat: add warning log when cache TTL jitter percentage is capped
    Log a warning when WithCacheTTLJitterPercentage receives a value above
    100 and caps it, to help surface misconfiguration.
    
    Signed-off-by: Asish Kumar <officialasishkumar@gmail.com>

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -562,6 +562,8 @@ func WithListObjectsIteratorCacheTTL(ttl time.Duration) OpenFGAServiceV1Option {
 func WithCacheTTLJitterPercentage(pct uint32) OpenFGAServiceV1Option {
 	return func(s *Server) {
 		if pct > 100 {
+			s.logger.Warn("cache TTL jitter percentage exceeded 100, capping to 100",
+				zap.Uint32("requested", pct))
 			pct = 100
 		}
 		s.cacheSettings.CacheTTLJitterPercentage = pct
